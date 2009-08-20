105fbd0adaca6e5dd645027e79ae88c56455dd08
Joshua Wehner <joshua.wehner@gmail.com>
2009-08-20 13:32:36 -0400

    Adding templated plugins, initializers, and submodules

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,10 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'thoughtbot-shoulda', :lib => 'shoulda/rails', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'money'
+  config.gem 'facets'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
---
+++ lib/tasks/scenarios.rake
@@ -0,0 +1,5 @@
+namespace :scenarios do
+  task :populate do
+    puts "i like boots!"
+  end
+end
