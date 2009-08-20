dc51751d6bd3d4d105a5c290ad23f982299614a5
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-20 18:10:14 -0400

    adding hoptoad config

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,3 @@
-# HoptoadNotifier.configure do |config|
-#   config.api_key = '1234567890abcdef'
-# end
+HoptoadNotifier.configure do |config|
+  config.api_key = 'e343d87f3613e217edf16597f119ecc7'
+end
