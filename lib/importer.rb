5c2c4c5b31c553a25c77d24c9a20dea29f40a7af
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-25 16:46:22 -0400

    added country model, changed importer to import only country data

--- lib/importer.rb
+++ lib/importer.rb
@@ -7,34 +7,39 @@ class Importer
   
   FILES = {
     #fields: COUNTRY_ID	COUNTRY_NAME	COUNTRY_REGION	COUNTRY_NETWORK_TYPE	GC_COUNTRY_MANAGER
-    :country   =>  'TR01_COUNTRY.TXT',
+    :country   =>  {:file => 'TR01_COUNTRY.TXT', :fields => [:code, :name, :region, :network_type, :manager]}#,
     # fields: ID	TYPE	TYPE_PROPERTY
-    :organization_type => 'TR02_ORGANIZATION_TYPE.TXT',
+    #:organization_type => 'TR02_ORGANIZATION_TYPE.TXT',
     # fields: PRINCIPLE_ID	PRINCIPLE_NAME
-    :principle => 'TR05_PRINCIPLE.TXT',
+    #:principle => 'TR05_PRINCIPLE.TXT',
     # fields: INTEREST_ID	INTEREST_NAME
-    :interest  => 'TR06_INTEREST.TXT',
+    #:interest  => 'TR06_INTEREST.TXT',
     # fields: ROLE_ID ROLE_NAME
-    :role      => 'TR07_ROLE.TXT',
+    #:role      => 'TR07_ROLE.TXT',
     # fields: ROLE_ID ROLE_NAME
     # 0, Unknown - invalid id for mysql
-    :language  => 'TR10_LANGUAGE.TXT'
+    #:language  => 'TR10_LANGUAGE.TXT'
   }
   
   # Imports all the data in files located in options[:folder]
   def run(options={:folder => File.join(RAILS_ROOT, 'lib/un7 tables')})
     @data_folder = options[:folder]
-    FILES.each{|key, value| import key, value}
+    FILES.each{|key, options| import key, options}
   end
 
   # Imports the data for a single file
-  def import(name, file_name)
-    file = File.join(@data_folder, file_name)
+  def import(name, options)
+    file = File.join(@data_folder, options[:file])
+    model = eval(name.to_s.modulize)
+    # read the file
     FasterCSV.foreach(file, :col_sep => "\t",
                             :headers => :first_row) do |row|
-      record = row
-      puts "#{name}: #{record}"
-      # TODO create an object of the correct type and save
+      # create an object of the correct type and save
+      o = model.new
+      options[:fields].each_with_index {|field,i| o.send("#{field}=", row[i])}
+      o.save
+
+      puts "#{name}: #{row}"
     end
   end
 end
