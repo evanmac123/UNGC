6866eed089a044b3db873dee7417b3d7ed7df719
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-26 17:50:23 -0400

    contact belongs to organization and country

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
+  belongs_to :organization
+  belongs_to :country
 end
--- test/unit/contact_test.rb
+++ test/unit/contact_test.rb
@@ -2,4 +2,6 @@ require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
   should_validate_presence_of :first_name, :last_name
+  should_belong_to :organization
+  should_belong_to :country
 end
