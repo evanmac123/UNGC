fff35b0e5dd6d45700fb6bb060406e2fd2ea59c4
Joshua Wehner <joshua@unspace.ca>
2009-08-28 12:26:23 -0400

    Removing fixtures

--- db/example_data.rb
+++ db/example_data.rb
@@ -1,2 +1,82 @@
 module FixtureReplacement
+  attributes_for :case_story do |a|
+    
+	end
+
+  attributes_for :communication_on_progress do |a|
+    
+	end
+
+  attributes_for :contact do |a|
+    
+	end
+
+  attributes_for :cop_score do |a|
+    
+	end
+
+  attributes_for :country do |a|
+    
+	end
+
+  attributes_for :exchange do |a|
+    
+	end
+
+  attributes_for :interest do |a|
+    
+	end
+
+  attributes_for :language do |a|
+    
+	end
+
+  attributes_for :listing_status do |a|
+    
+	end
+
+  attributes_for :logo_approval do |a|
+    
+	end
+
+  attributes_for :logo_comment do |a|
+    
+	end
+
+  attributes_for :logo_file do |a|
+    
+	end
+
+  attributes_for :logo_publication do |a|
+    
+	end
+
+  attributes_for :logo_request do |a|
+    
+	end
+
+  attributes_for :organization_type do |a|
+    
+	end
+
+  attributes_for :organization do |a|
+    
+	end
+
+  attributes_for :principle do |a|
+    
+	end
+
+  attributes_for :removal_reason do |a|
+    
+	end
+
+  attributes_for :role do |a|
+    
+	end
+
+  attributes_for :sector do |a|
+    
+	end
+
 end
