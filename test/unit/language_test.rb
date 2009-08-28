d9784765616b78bc3823febe1e3fa204ce5f8872
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-28 15:34:39 -0400

    trying to commit a broken build for cc

--- test/unit/language_test.rb
+++ test/unit/language_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
 class LanguageTest < ActiveSupport::TestCase
-  should_validate_presence_of :name
+  should_validate_presence_of :namezz
   should_have_and_belong_to_many :communication_on_progresses
 end
