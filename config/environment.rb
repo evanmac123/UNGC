eaba34e29b5b8f760b4130a08e18629ad254b1f9
Joshua Wehner <joshua@unspace.ca>
2009-08-28 15:59:33 -0400

    Copying the existing layout, stylesheets and images; adjusting some navigation

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'thoughtbot-shoulda', :lib => 'shoulda/rails', :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'money'
-  config.gem 'facets'
+  config.gem 'liquid'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
