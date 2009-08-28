ade82d88b69ce550782a4302955a9e59eef63da2
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-28 17:06:48 -0400

    added relationships to logo request

--- app/models/logo_request.rb
+++ app/models/logo_request.rb
@@ -1,5 +1,7 @@
 class LogoRequest < ActiveRecord::Base
   validates_presence_of :organization_id, :requested_on, :publication_id
   belongs_to :organization
+  belongs_to :contact
+  belongs_to :publication, :class_name => "LogoPublication"
   has_many :logo_comments
 end
