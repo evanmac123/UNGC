7faa5425d0177ead40645f81d6653fd4f0cb7159
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-28 15:53:32 -0400

    corrected email address

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,7 +1,7 @@
 # Project-specific configuration for CruiseControl.rb
 Project.configure do |project|
   # Send email notifications about broken and fixed builds to email1@your.site, email2@your.site (default: send to nobody)
-  project.email_notifier.emails = ['ungs@unspace.ca']
+  project.email_notifier.emails = ['ungc@unspace.ca']
   # Set email 'from' field to john@doe.com:
   project.email_notifier.from = 'cc@unspace.ca'
 end
