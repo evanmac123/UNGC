859b40c90bf56c63b6b55acf914390513a24ca87
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-31 11:29:54 -0400

    organization has many contacts

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ActiveRecord::Base
   validates_presence_of :name
+  has_many :contacts
   belongs_to :sector
   belongs_to :organization_type
   belongs_to :country
+
+  accepts_nested_attributes_for :contacts
 end
