e886fb5b765155283720dabf2c48330146e9425c
Joshua Wehner <joshua@unspace.ca>
2009-09-01 12:42:40 -0400

    Revert "Contents as nav? (Won't work, though, some pages appear in multiple nav areas.)"
    This reverts commit 1be044f4e0596c3b4af11e4c5f296e60ef764c52.

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,27 +1,3 @@
-# == Schema Information
-# Schema version: 20090831192138
-#
-# Table name: contents
-#
-#  id         :integer(4)      not null, primary key
-#  path       :string(255)
-#  crumbs     :text
-#  content    :text
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class Content < ActiveRecord::Base
   serialize :path
-  has_many :children, :order => "position ASC", :class_name => 'Content', :foreign_key => :parent_id
-  
-  def self.sections
-    find :all, :conditions => "parent_id is null", :include => :children
-  end
-  
-  def parent
-    return nil unless parent_id
-    self.class.find :first, :conditions => ["id = ?", parent_id]
-  end
-  
 end
