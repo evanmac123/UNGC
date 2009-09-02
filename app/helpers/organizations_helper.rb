c332b8f93a6cd1daf973b867165067b4250e499e
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-09-02 17:24:59 -0400

    using can_* methods

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,8 +1,8 @@
 module OrganizationsHelper
   def organization_actions(organization)
     actions = []
-    actions << link_to('Approve', approve_organization_path(@organization), :method => :post) if organization.pending?
-    actions << link_to('Reject', reject_organization_path(@organization), :method => :post) if organization.pending?
+    actions << link_to('Approve', approve_organization_path(@organization), :method => :post) if organization.can_approve?
+    actions << link_to('Reject', reject_organization_path(@organization), :method => :post) if organization.can_reject?
     actions << link_to('Edit', edit_organization_path(@organization))
     actions << link_to('Back', organizations_path)
     actions.join(" | ")
