c9aeff1abc7f9fa2a7bdd1557fffb8bc3dbdc8c9
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-09-02 13:54:57 -0400

    Added more fields to organization form

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,6 @@ class Country < ActiveRecord::Base
   validates_presence_of :name, :code
   has_and_belongs_to_many :case_stories
   has_and_belongs_to_many :communication_on_progresses
+  
+  default_scope :order => 'name'
 end
--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,4 +1,7 @@
 class Sector < ActiveRecord::Base
   validates_presence_of :name
   acts_as_tree
+  
+  default_scope :order => 'name'
+  named_scope :top_level, :conditions => 'parent_id IS NULL'
 end
