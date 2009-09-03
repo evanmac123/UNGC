d0fe2913f9bac0537a38fd9cdfe37692f84fde6f
Joshua Wehner <joshua@unspace.ca>
2009-09-03 11:17:08 -0400

    Custom toolbar, settings for RTE

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -10,7 +10,28 @@ var Editor = {
 		var contentDiv = '#rightcontent div.copy';
 		Editor.originalContents = $(contentDiv).html();
 		$(contentDiv).html('');
-		Editor.editor = CKEDITOR.appendTo('rightcontent');
+		Editor.editor = CKEDITOR.appendTo('rightcontent', {
+			toolbar: [
+			    ['Source','-','Save','NewPage','Preview','-','Templates'],
+			    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+			    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+			    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+			    '/',
+			    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+			    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+			    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+			    ['Link','Unlink','Anchor'],
+			    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+			    '/',
+			    ['Styles','Format','Font','FontSize'],
+			    ['TextColor','BGColor'],
+			    ['Maximize', 'ShowBlocks','-','About']
+			],
+			width: 600,
+			dialog_magnetDistance: 5,
+			resize_minWidth: 300,
+			resize_maxWidth: 600
+		});
 		Editor.editor.setData(contents);
 	}
 }
