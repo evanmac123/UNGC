fc264310ab856a97b8f1f4e97a90a34b77f9b5a2
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-09-03 18:37:48 -0400

    added helper method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   belongs_to :organization
   belongs_to :country
+
+  def name
+    [first_name, last_name].join(' ')
+  end
 end
