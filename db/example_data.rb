06f15a49a183ef5ebb26641a71b1307008a7e0c4
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-09-04 14:26:48 -0400

    added more default values

--- db/example_data.rb
+++ db/example_data.rb
@@ -1,10 +1,8 @@
 module FixtureReplacement
   attributes_for :case_story do |a|
-    
 	end
 
   attributes_for :communication_on_progress do |a|
-    
 	end
 
   attributes_for :contact do |c|
@@ -13,47 +11,38 @@ module FixtureReplacement
 	end
 
   attributes_for :cop_score do |a|
-    
 	end
 
   attributes_for :country do |a|
-    
 	end
 
   attributes_for :exchange do |a|
-    
 	end
 
   attributes_for :interest do |a|
-    
 	end
 
   attributes_for :language do |a|
-    
 	end
 
   attributes_for :listing_status do |a|
-    
 	end
 
   attributes_for :logo_approval do |a|
-    
 	end
 
   attributes_for :logo_comment do |a|
-    
 	end
 
   attributes_for :logo_file do |a|
-    
 	end
 
-  attributes_for :logo_publication do |a|
-    
+  attributes_for :logo_publication do |p|
+    p.name = String.random
 	end
 
-  attributes_for :logo_request do |a|
-    
+  attributes_for :logo_request do |r|
+    r.purpose = String.random
 	end
 
   attributes_for :navigation do |n|
@@ -62,7 +51,6 @@ module FixtureReplacement
   end
 
   attributes_for :organization_type do |a|
-    
 	end
 
   attributes_for :organization do |o|
@@ -70,19 +58,14 @@ module FixtureReplacement
 	end
 
   attributes_for :principle do |a|
-    
 	end
 
   attributes_for :removal_reason do |a|
-    
 	end
 
   attributes_for :role do |a|
-    
 	end
 
   attributes_for :sector do |a|
-    
 	end
-
 end
