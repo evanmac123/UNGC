03146be35ebcf5848ade99f1891239f4e63037ae
Joshua Wehner <joshua@unspace.ca>
2009-09-08 16:38:08 -0400

    Fix bad migration

--- db/migrate/20090908161304_populate_content_versions.rb
+++ db/migrate/20090908161304_populate_content_versions.rb
@@ -2,7 +2,9 @@ class PopulateContentVersions < ActiveRecord::Migration
   def self.up
     Content.find_each do |c|
       ContentVersion.create c.attributes.merge({
-        :number => 1,
+        :number   => 1,
+        :content  => c.content,
+        :path     => c.path,
         :approved => true
       })
     end
