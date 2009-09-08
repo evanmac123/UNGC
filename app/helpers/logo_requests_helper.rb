747e53f259edbe3308d709939ae4039bcbe763bc
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-09-08 15:33:15 -0400

    assigning logos for a specific request

--- app/helpers/logo_requests_helper.rb
+++ app/helpers/logo_requests_helper.rb
@@ -4,4 +4,9 @@ module LogoRequestsHelper
     actions << link_to('Back', @organization)
     actions.join(" | ")
   end
+  
+  def logo_file_image_tag(logo_file)
+    # TODO we'll no longer use demo.unglobalcompact.org
+    image_tag "http://demo.unglobalcompact.org/admin/images/gc_logos/#{logo_file.thumbnail}"
+  end
 end
