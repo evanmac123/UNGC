4cb11b5ff1e163acb93c759ae03b57415c5f88ac
Joshua Wehner <joshua@unspace.ca>
2009-09-08 16:52:36 -0400

    Fixes the weirdness

--- db/migrate/20090908161304_populate_content_versions.rb
+++ db/migrate/20090908161304_populate_content_versions.rb
@@ -1,7 +1,7 @@
 class PopulateContentVersions < ActiveRecord::Migration
   def self.up
     Content.find_each do |c|
-      ContentVersion.create c.attributes.merge({
+      c.versions.create c.attributes.merge({
         :number   => 1,
         :approved => true
       })
