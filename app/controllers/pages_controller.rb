798e14ad490762f9b308cdfe1e9cba836b3ed0e5
Joshua Wehner <joshua@unspace.ca>
2009-09-17 11:55:42 -0400

    ERB isn't being very friendly, either

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,9 @@ class PagesController < ApplicationController
   # layout :determine_layout
   
   def view
-    render :template => template.filename, :layout => determine_layout
+    render :inline => "<%= 'hello, ' * 3 + 'again %>"
+    # render :inline => @current_version.content
+    # render :template => template.filename, :layout => determine_layout
   end
 
   def decorate
