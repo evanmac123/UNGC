49f43fbc245b20be47ee83995d7114428052e0ee
Joshua Wehner <joshua@unspace.ca>
2009-09-28 16:04:51 -0400

    Move "per_page_select" to pages_helper - it's used on several pages

--- app/helpers/cops_helper.rb
+++ app/helpers/cops_helper.rb
@@ -19,16 +19,4 @@ module CopsHelper
       :page_links => false, 
       :params => params
   end
-  
-  def per_page_select
-    # ['Number of results per page', 10, 25, 50, 100]
-    options = [
-      ['Number of results per page', ''],
-      ['10' , url_for(params.merge(:page => 1, :per_page => 10))],
-      ['25' , url_for(params.merge(:page => 1, :per_page => 25))],
-      ['50' , url_for(params.merge(:page => 1, :per_page => 50))],
-      ['100', url_for(params.merge(:page => 1, :per_page => 100))]
-    ]
-    select_tag :per_page, options_for_select(options), :class => 'autolink'
-  end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,21 @@
 module PagesHelper
 
+  def per_page_select
+    # ['Number of results per page', 10, 25, 50, 100]
+    options = [
+      ['Number of results per page', ''],
+      ['10' , url_for(params.merge(:page => 1, :per_page => 10))],
+      ['25' , url_for(params.merge(:page => 1, :per_page => 25))],
+      ['50' , url_for(params.merge(:page => 1, :per_page => 50))],
+      ['100', url_for(params.merge(:page => 1, :per_page => 100))]
+    ]
+    select_tag :per_page, options_for_select(options), :class => 'autolink'
+  end
+
+  def yyyy_mm_dd(date)
+    date ? date.strftime('%Y/%m/%d') : '&nbsp;'
+  end
+
   def versioned_edit_path
     if @current_version.approved?
       edit_content_path(:id => @page)
