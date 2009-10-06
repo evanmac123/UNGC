00661c208b77eeb7804b963b10dd68d9d8073762
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-10-06 15:17:25 -0400

    contacts with no login don't run uniqueness validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -41,7 +41,7 @@ class Contact < ActiveRecord::Base
   TYPE_NETWORK = :network
 
   validates_presence_of :first_name, :last_name
-  validates_uniqueness_of :login, :allows_blank => true
+  validates_uniqueness_of :login, :allow_nil => true
   validates_presence_of :password, :unless => Proc.new { |contact| contact.login.blank? }
   belongs_to :organization
   belongs_to :country
