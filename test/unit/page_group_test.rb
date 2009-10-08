0e75af0c6f9af712588737962cfb867e714e0da9
Joshua Wehner <joshua@unspace.ca>
2009-10-08 11:32:22 -0400

    Some simple tests for page_group

--- test/unit/page_group_test.rb
+++ test/unit/page_group_test.rb
@@ -1,8 +1,44 @@
 require 'test_helper'
 
 class PageGroupTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "given two page groups" do
+    setup do
+      @group1 = create_page_group
+      @group2 = create_page_group
+      @hidden = create_page_group :display_in_navigation => false
+    end
+
+    should "fail to show @hidden in navigation" do
+      assert_same_elements [@group1, @group2], PageGroup.for_navigation
+    end
+    
+    context "and some pages are assigned" do
+      setup do
+        @group1_pages, @group2_pages, @hidden_pages = [], [], []
+        @group1_pages << create_page(:section => @group1)
+        @group1_pages << create_page(:section => @group1)
+        @group1_pages << create_page(:section => @group1)
+        @group2_pages << create_page(:section => @group2)
+        @group2_pages << create_page(:section => @group2)
+        @hidden_pages << create_page(:section => @hidden, :display_in_navigation => true)
+        create_page :parent =>  @group1_pages.first
+      end
+
+      should "find children for group1" do
+        assert_same_elements @group1_pages, @group1.children
+      end
+
+      should "find children for group2" do
+        assert_same_elements @group2_pages, @group2.children
+      end
+      
+      should "forward group#path to first child" do
+        assert_equal @group1_pages.first.path, @group1.path
+      end
+    end
+    
   end
+  
+  
 end
