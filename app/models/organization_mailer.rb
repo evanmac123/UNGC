b1a75c73abbd37de406a8288472ae5a4772cc716
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-10-08 15:32:31 -0400

    added mailers for organization workflow

--- app/models/organization_mailer.rb
+++ app/models/organization_mailer.rb
@@ -6,4 +6,28 @@ class OrganizationMailer < ActionMailer::Base
     recipients organization.contacts.contact_points.collect(&:email)
     body :organization => organization
   end
+  
+  def in_review(organization)
+    from EMAIL_SENDER
+    subject "Your Global Compact Participation Request has been updated"
+    content_type "text/html"
+    recipients organization.contacts.contact_points.collect(&:email)
+    body :organization => organization
+  end
+  
+  def approved(organization)
+    from EMAIL_SENDER
+    subject "Your Global Compact Participation Request has been accepted"
+    content_type "text/html"
+    recipients organization.contacts.contact_points.collect(&:email)
+    body :organization => organization
+  end
+
+  def rejected(organization)
+    from EMAIL_SENDER
+    subject "Global Compact Participation Request Status"
+    content_type "text/html"
+    recipients organization.contacts.contact_points.collect(&:email)
+    body :organization => organization
+  end
 end
