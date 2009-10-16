0752529dd938fb83762f3a1e0a30b9bbdc68c333
Joshua Wehner <joshua@unspace.ca>
2009-10-16 15:23:09 -0400

    Fixes bug where malformed data interacts badly with Rails etag generator (for gentle page caching when using send_file).
    See https://rails.lighthouseapp.com/projects/8994/tickets/2628-ruby-19-and-activesupport for more details. We're basing this on the patch there. (Which is nice and should have been accepted.)

---
+++ config/initializers/core_ext.rb
@@ -0,0 +1,12 @@
+class String
+  def blank?
+    # old: self !~ /\S/ as of Rails 2.3.3
+    # This fails if the string contains invalidly coded characters, which our 
+    # data sometimes does - this version takes valid_encoding into account
+    if (!self.respond_to?(:valid_encoding?) or self.valid_encoding?)
+      self !~ /\S/
+    else
+      self.strip.size == 0
+    end
+  end
+end
