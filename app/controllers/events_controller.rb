2f348bee074f259d368e5976d454775cc1f4c258
Joshua Wehner <joshua@unspace.ca>
2009-10-21 11:11:01 -0400

    Links to event detail pages work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,19 @@
 class EventsController < ApplicationController
+  helper :navigation, :pages
+  before_filter :determine_navigation
+  before_filter :find_event
+
   def show
   end
 
+  private
+    def find_event
+      @event = Event.find_by_id(params[:permalink].to_i)
+      redirect_to(:permalink => @event.to_param) unless @event.to_param == params[:permalink]
+      redirect_to(:back) unless @event
+    end
+
+    def default_navigation
+      '/NewsAndEvents/Upcoming_Events.html'
+    end
 end
