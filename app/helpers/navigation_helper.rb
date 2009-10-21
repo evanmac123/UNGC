33a0c4263f916b468c02cf5cb10da109f2333c4f
Joshua Wehner <joshua@unspace.ca>
2009-10-21 11:05:31 -0400

    Bug: Pages lacking display_in_navigation were still showing up in top-nav, wasn't using visible_children scope

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -71,7 +71,7 @@ module NavigationHelper
   def top_nav_bar(section_children_content='')
     PageGroup.for_navigation.each do |section|
       section_link = content_tag :a, section.name, :href => section.link_to_first_child
-      children = section.children.map do |child|
+      children = section.visible_children.map do |child|
         child_link = content_tag :a, child.title, :href => child.path
         content_tag :li, child_link
       end
