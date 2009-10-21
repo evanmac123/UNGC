e8d6fcb0008fdcad680fcb4a4daf871830691a10
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-10-21 15:26:45 -0400

    Added habtm to country

--- test/unit/country_test.rb
+++ test/unit/country_test.rb
@@ -4,4 +4,5 @@ class CountryTest < ActiveSupport::TestCase
   should_validate_presence_of :name, :code
   should_have_and_belong_to_many :case_stories
   should_have_and_belong_to_many :communication_on_progresses
+  should_have_and_belong_to_many :local_networks
 end
