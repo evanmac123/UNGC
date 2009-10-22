e61f7c760609b6d8b73b4d14dd0a94053cdace83
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-10-22 14:39:02 -0400

    Implemented all visible_to scopes

--- app/models/case_story.rb
+++ app/models/case_story.rb
@@ -74,8 +74,10 @@ class CaseStory < ActiveRecord::Base
   named_scope :visible_to, lambda { |user|
     if user.user_type == Contact::TYPE_ORGANIZATION
       { :conditions => ['organization_id=?', user.organization_id] }
+    elsif user.user_type == Contact::TYPE_NETWORK
+      { :conditions => ["organizations.country_id in (?)", user.local_network.country_ids],
+        :include    => :organization }
     else
-      # TODO implement for network
       {}
     end
   }
