f5d63232cff500816123589dcb4d615465207bcf
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-10-26 17:01:43 -0400

    There's no from_network method :(

--- lib/visible_to.rb
+++ lib/visible_to.rb
@@ -4,7 +4,7 @@ module VisibleTo
       named_scope :visible_to, lambda { |user|
         if user.from_organization?
           { :conditions => ['organization_id=?', user.organization_id] }
-        elsif user.from_network
+        elsif user.from_network?
           { :conditions => ["organizations.country_id in (?)", user.local_network.country_ids],
             :include    => :organization }
         else
