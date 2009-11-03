819cad76c88479c5dfbf5f307a16096a3a4c00b6
Joshua Wehner <joshua@unspace.ca>
2009-11-03 11:56:12 -0500

    Basic import for Case Stories

--- app/models/searchable.rb
+++ app/models/searchable.rb
@@ -53,10 +53,22 @@ class Searchable < ActiveRecord::Base
     Page.approved.each { |page| index_page page }
   end
   
+  def self.get_text_from_pdf(system_path_to_file)
+    if File.exists?(system_path_to_file)
+      `pdf2txt.py #{system_path_to_file}`
+    else
+      '' # TODO: or raise?
+    end
+  end
+  
+  def self.get_text_from_word(system_path_to_file)
+    puts "I don't know this yet" # TODO
+  end
+  
   def self.index_pdf(system_path_to_file, public_path_to_file)
     title = 'PDF File'
     url   = public_path_to_file
-    content = `pdf2txt.py #{system_path_to_file}`
+    content = get_text_from_pdf(system_path_to_file)
     import 'PDF', :url => url, :title => title, :content => content
   end
 
@@ -81,11 +93,32 @@ class Searchable < ActiveRecord::Base
       headline.country.try(:name) + 
       helper.strip_tags(headline.description)
     url = helper.instance_eval { headline_path(headline) }
-    import 'Event', :url => url, :title => title, :content => content
+    import 'Headline', :url => url, :title => title, :content => content
   end
   
   def self.index_headlines
     Headline.approved.each { |h| index_headline h }
   end
 
+  def self.index_case_story(case_story)
+    helper = SearchableHelper.new
+    title = case_story.title
+    if case_story.attachment_content_type == 'application/pdf'
+      file_content = get_text_from_pdf(case_story.attachment.path)
+    elsif case_story.attachment_content_type =~ /Word/
+      file_content = get_text_from_word(case_story.attachment.path)
+    end
+    content = <<-EOF
+      #{case_story.description}
+      #{case_story.countries.map(&:name).join(' ')}
+      #{file_content}
+    EOF
+    url = "/case_story/#{case_story.to_param}"
+    import 'CaseStory', :url => url, :title => title, :content => content
+  end
+
+  def self.index_case_stories
+    CaseStory.approved.each { |c| index_case_story c }
+  end
+  
 end
