b83c527f7905cb7a31abb03991f7b1d92159559a
Venu Keesari <keesari@unglobalcompact.org>
2009-11-03 13:52:55 -0500

    Text changes to emails for organization_mailer - updated email subjects in organization_mailer.rb and corresponding tests in organization_mailer_test.rb - make @contact object accessible in view - updated text in views/organization_mailer/*.haml - commented out line #36 in views/organization_mailer/approved.haml because it was causing test to fail

--- app/models/organization_mailer.rb
+++ app/models/organization_mailer.rb
@@ -1,33 +1,33 @@
 class OrganizationMailer < ActionMailer::Base
   def submission_received(organization)
-    subject "We received your application"
+    subject "Your Letter of Commitment to the Global Compact"
     from EMAIL_SENDER
     content_type "text/html"
     recipients organization.contacts.contact_points.collect(&:email_recipient)
-    body :organization => organization, :first_contact => organization.contacts.contact_points.first
+    body :organization => organization, :contact => organization.contacts.contact_points.first
   end
   
   def in_review(organization)
     from EMAIL_SENDER
-    subject "Your Global Compact Participation Request has been updated"
+    subject "Your application to the Global Compact"
     content_type "text/html"
     recipients organization.contacts.contact_points.collect(&:email_recipient)
-    body :organization => organization
+    body :organization => organization, :contact => organization.contacts.contact_points.first
   end
   
   def approved(organization)
     from EMAIL_SENDER
-    subject "Your Global Compact Participation Request has been accepted"
+    subject "Welcome to the United Nations Global Compact"
     content_type "text/html"
     recipients organization.contacts.contact_points.collect(&:email_recipient)
-    body :organization => organization
+    body :organization => organization, :contact => organization.contacts.contact_points.first
   end
 
   def reject_microenterprise(organization)
     from EMAIL_SENDER
-    subject "Global Compact Participation Request Status"
+    subject "Your Letter of Commitment to the Global Compact"
     content_type "text/html"
     recipients organization.contacts.contact_points.collect(&:email_recipient)
-    body :organization => organization
+    body :organization => organization, :contact => organization.contacts.contact_points.first
   end
 end
