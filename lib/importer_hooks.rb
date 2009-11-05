3b617fd1386b153e64641cb4105c6e6efaf2e860
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-11-05 15:05:15 -0500

    Refactored Importer, moved code to ImporterHook and ImporterMapping. Moved CopAttribute.add_records to a importer hook.

---
+++ lib/importer_hooks.rb
@@ -0,0 +1,326 @@
+module ImporterHooks
+  # This module contains code that is executed after some files have been imported
+
+  # runs after the organization import, pledge amount only exists
+  # in the TEMP table/file
+  def post_organization
+    import_from_temp_table
+    convert_organizations_to_local_networks
+  end
+  
+  def post_contact
+    assign_roles
+    assign_network_managers
+    assign_local_network_id
+  end
+  
+  def post_communication_on_progress
+    import_cop_xml
+  end
+  
+  def post_principle
+    populate_principle_areas
+    add_cop_questions if CopQuestion.count == 0
+  end
+  
+  # Imports fields from R01_ORGANIZATION_TEMP.csv:
+  #   pledge_amount & id
+  def import_from_temp_table
+    file = File.join(@data_folder, "R01_ORGANIZATION_TEMP.csv")
+    CSV.foreach(file, :headers => :first_row) do |row|
+      # get organization by name
+      if o = Organization.find_by_name(row["ORG_NAME"])
+        # import pledge
+        pledge = row["PLEDGE_AMOUNT"]
+        o.pledge_amount = pledge.to_i if pledge.to_i > 0
+        # import id in TMP table
+        old_tmp_id = row["ID"].to_i
+        o.old_tmp_id = old_tmp_id if old_tmp_id.to_i > 0
+        # save the record
+        o.save
+      end
+    end
+  end
+  
+  def convert_organizations_to_local_networks
+    get_local_network_organizations.each do |organization|
+      name = organization.name.gsub(/GC Network - /, '')
+      next if skip_extraction?(name)
+      local_net = LocalNetwork.new( :name => name,
+                                    :url  => organization.url )
+      # 0-No network, 1-Network in Development, 2-Established
+      if organization.country.network_type
+        local_net.state = ['none', 'emerging', 'established'][organization.country.network_type]
+      end
+      # we now save the local network and assign the country
+      local_net.save
+      attach_countries_to_network(local_net, organization)
+      # we no longer need the organization, we could "organization.delete" now
+    end
+  end
+  
+  def get_local_network_organizations
+    OrganizationType.for_filter(:gc_networks).first.organizations.find(:all, :conditions => ["local_network = ? AND name LIKE ?", true, 'GC Network - %'])
+  end
+  
+  def assign_roles
+    file = File.join(@data_folder, "R12_XREF_R10_TR07.csv")
+    # "ROLE_ID","CONTACT_ID","R01_ORG_NAME"
+    CSV.foreach(file, :headers => :first_row) do |row|
+      role_id = row['ROLE_ID']
+      contact_id = row['CONTACT_ID']
+      contact = Contact.find_by_old_id contact_id
+      role = Role.find_by_old_id role_id
+      if role && contact
+        contact.roles << role
+      else
+        log "** [error] Could not assign role: #{row.inspect}"
+      end
+    end
+  end
+  
+  def assign_network_managers
+    #fields: COUNTRY_ID	COUNTRY_NAME	COUNTRY_REGION	COUNTRY_NETWORK_TYPE	GC_COUNTRY_MANAGER
+    file = File.join(@data_folder, CONFIG[:country][:file])
+    CSV.foreach(file, :headers => :first_row) do |row|
+      manager = row['GC_COUNTRY_MANAGER']
+      unless manager.blank?
+        # get country, contact and network
+        country = Country.find_by_code(row['COUNTRY_ID'])
+        contact = Contact.first(:conditions => {:first_name => manager.split.first,
+                                                :last_name  => manager.split.last})
+        begin
+          network = country.local_network.first
+        rescue
+          network = nil
+        end
+        if country && contact && network
+          # get network and assign manager
+          network.manager_id = contact.id
+          network.save
+        else
+          log "** [minor error] Could not find contact: #{manager}" unless contact
+          log "** [minor error] Could not find country: #{row['COUNTRY_ID']}" unless country
+          log "** [minor error] Could not find network for: #{row['COUNTRY_ID']}" unless network
+        end
+      end
+    end
+  end
+  
+  def assign_local_network_id
+    return unless LocalNetwork.count > 0
+    get_local_network_organizations.each do |organization|
+      puts "Working with #{organization.name} ##{organization.id}"
+      if organization && contacts = organization.contacts and contacts.any?
+        network = organization.country.local_network
+        contacts.each do |contact|
+          puts "  working with #{contact.name} ##{contact.id}"
+          contact.update_attribute :local_network_id, network.id
+          puts "  DONE!"
+        end
+      end
+    end
+  end
+  
+  def attach_countries_to_network(local_net, organization)
+    organization.country.update_attribute :local_network_id, local_net.id
+    countries = []
+    if local_net.name =~ /Gulf States/
+      countries = Country.find(:all, :conditions => ["name IN (?)", gulf_states])
+    elsif local_net.name =~ /Nordic/
+      countries = Country.find(:all, :conditions => ["name IN (?)", nordic_countries])
+    end
+    countries.each do |country|
+      country.update_attribute :local_network_id, local_net.id
+    end
+  end
+  
+  def import_cop_xml
+    require 'hpricot'
+    require 'facets'
+    # puts "*** Importing from cop_xml data..."
+    real_files = Dir[DEFAULTS[:path_to_cop_xml]]
+    converter = Iconv.new("UTF-8", "iso8859-1") 
+    real_files.each do |f|
+      short = (f - '.xml').split('/').last
+      # puts "Working with file #{short}:"
+      if cop = CommunicationOnProgress.find_by_identifier(short)
+        description = converter.iconv (Hpricot(open(f))/'description').inner_html
+        cop.update_attribute :description, description.strip
+      else
+        # puts "  *** Error: Can't find the COP"
+      end
+      # puts "\n"
+    end
+    # puts "*** Done!"
+  end
+    
+  def nordic_countries
+    ['Denmark', 'Finland', 'Sweden', 'Norway', 'Iceland']
+  end
+
+  def gulf_states
+    # Saudi Arabia seems mis-spelled?
+    ['Bahrain', 'Kuwait', 'Oman', 'Qatar', 'Saudi Arabia', 'Saudia Arabia', 'United Arab Emirates']
+  end
+
+  def skip_extraction?(name)
+    skip = nordic_countries + gulf_states
+    skip.include?(name)
+  end
+  
+  # Adds the default questions to be used in the COP form
+  def add_cop_questions
+    [ [:human_rights, false, "Does the COP explain how your company determined that human rights are not relevant to its business or the communities in which it operates?", nil, 1,
+        [""]
+      ],
+      [:human_rights, false, "Does the COP make explicit reference to planned policies and/or activities related to human rights?", nil, 2,
+        [""]
+      ],
+      [:human_rights, true, "Commitment and policy: Does the COP make an explicit commitment or mention a policy document on human rights?", nil, 1, 
+        ["Reflection on the relevance ('materiality') of human rights for your company", "Public commitment to respect and support human rights",
+          "Reference to the Universal Declaration of Human Rights", "Formal human rights policy (e.g. in code of conduct)"]
+      ],
+      [:human_rights, true, "Implementation: Does the COP explain how human rights issues are managed and/or what activities your company is undertaking?", nil, 2,
+        ["Allocation of responsibilities and accountabilities", "Human rights risk and/or impact assessment",
+          "Grievance mechanism", "Internal and external communication", "Training for employees",
+          "Participation in human rights initiatives / collective action", "Inclusion of human rights issues in contracts with business partners",
+          "Supplier audits", "Monitoring and evaluation", "Other"]
+      ],
+      [:human_rights, true, "Outcomes: Does the COP contain information on outcomes of your human right policies and activities?", nil, 3,
+        ["Qualitative outcomes", "Quantitative outcomes using defined indicators", "Expected outcomes/targets"]
+      ],
+
+      [:labour, false, "Does the COP explain how your company determined labour issues (freedom of association and right to collective bargaining; forced and compulsory labour; child labour; non-discrimination) are not relevant for its business or the communities in which it operates?", nil, 1,
+        [""]
+      ],
+      [:labour, false, "Does the COP make explicit reference to planned policies and/or activities related to labour issues?", nil, 2,
+        [""]
+      ],
+      [:labour, true, "Commitment and policy: Does the COP make an explicit commitment or mention a policy document on the labour principles?", nil, 1, 
+        ["Reflection on the relevance ('materiality') of the labour principles for your company",
+          "Public commitment to uphold freedom of association and the right to collective bargaining",
+          "Public commitment to eliminate forced and compulsory labour",
+          "Public commitment to eliminate child labour",
+          "Public commitment to eliminate discrimination in respect of employment and occupation",
+          "Reference to the International Labour Organization (ILO) Core Conventions",
+          "Formal policy that addresses the labour principles (e.g. in code of conduct)"
+        ]
+      ],
+      [:labour, true, "Implementation: Does the COP explain how the labour principles are managed and/or what activities your company is undertaking?", nil, 2, 
+        ["Allocation of responsibilities and accountabilities",
+          "Internal and external communication",
+          "Training for employees",
+          "Participation in labour initiatives / collective action",
+          "Inclusion of labour issues in contracts with business partners",
+          "Supplier audits",
+          "Monitoring and evaluation",
+          "Other"
+        ]
+      ],
+      [:labour, true, "Outcomes: Does the COP contain information on outcomes of your labour policies and activities?", nil, 3,
+        ["Qualitative outcomes (e.g. operations identified as having significant risk for labour incidents)",
+          "Quantitative outcomes using defined indicators (e.g. percentage of employees covered by collective bargaining agreements; )",
+          "Expected outcomes/targets"
+        ]
+      ],
+      
+      [:environment, false, "Does the COP explain how your company determined environmental issues are not relevant for its business or the communities in which it operates?", nil, 1,
+        [""]
+      ],
+      [:environment, false, "Does the COP make explicit reference to planned policies and/or activities related to labour issues?", nil, 2,
+        [""]
+      ],
+      [:environment, true, "Commitment and policy: Does the COP make an explicit commitment or mention a policy document on the environmental principles?", nil, 1,
+        ["Reflection on the relevance ('materiality') of the labour principles for your company",
+          "Public commitment to support a precautionary approach to environmental challenges",
+          "Public commitment to undertake initiatives to promote greater environmental responsibility",
+          "Public commitment to encourage the development and diffusion of environmentally friendly technologies",
+          "Reference to the Rio Declaration on Environment and Development",
+          "Formal environmental policy"
+        ]
+      ],
+      [:environment, true, "Implementation: Does the COP explain how environmental issues are managed and/or what activities your company is undertaking?", nil, 2,
+        ["Environmental risk and/or impact assessment",
+          "Environmental management system",
+          "Allocation of responsibilities and accountabilities",
+          "Awareness raising",
+          "Eco-efficiency programs",
+          "Life cycle assessment",
+          "Participation in environmental initiatives",
+          "Inclusion of environmental issues in contracts with business partners",
+          "Monitoring and evaluation",
+          "Other"
+        ]
+      ],
+      [:environment, true, "Outcomes: Does the COP contain information on outcomes of your environmental policies and activities?", nil, 3,
+        ["Qualitative outcomes",
+          "Quantitative outcomes using defined indicators",
+          "Expected outcomes/targets"
+        ]
+      ],
+      [:environment, true, "Does your COP provide information about activities and/or outcomes related to your company's participation in Caring for Climate?", Initiative.find_by_name("Caring For Climate").try(:id), 4,
+        ["Activities aimed at improving the energy efficiency of products, services and processes",
+          "Engagement in public policy",
+          "Working collaboratively with peers and along the value-chain",
+          "Data on carbon footprint",
+          "Expected outcomes such as CO2 emission targets"
+        ]
+      ],
+      
+      [:anti_corruption, false, "Does the COP explain how your company determined that corruption is not relevant to its business or the communities in which it operates?", nil, 1,
+        [""]
+      ],
+      [:anti_corruption, false, "Does the COP make explicit reference to planned policies and/or activities related to corruption?", nil, 2,
+        [""]
+      ],
+      [:anti_corruption, true, "Commitment and policy: Does the COP make an explicit commitment or mention a policy document on the environmental principles?", nil, 1,
+        ["Reflection on the relevance ('materiality') of corruption for your company",
+          "Publicly stated commitment to zero-tolerance of corruption",
+          "Commitment to be compliant with all laws relevant to corruption",
+          "Formal anti-corruption policy (e.g. in code of conduct)",
+          "Statement of support for international and regional legal frameworks, such as the UN Convention Against Corruption"
+        ]
+      ],
+      [:anti_corruption, true, "Implementation: Does the COP explain how environmental issues are managed and/or what activities your company is undertaking?", nil, 2,
+        ["Identification of corruption risks within your company's business",
+          "Implementation of an anti-corruption program, such as standards and procedures, allocation of responsibilities, or sanctions",
+          "Support by the organization's leadership to anti-corruption",
+          "Communication of and training on the anti-corruption commitment to all employees",
+          "Internal checks-and-balances to ensure consistency with anti-corruption commitment",
+          "Whistle blowing and other communication channels for reporting concerns or seeking advice and follow up mechanism",
+          "Communications and actions taken to encourage business partners to implement anti-corruption commitments",
+          "Participation in voluntary anti-corruption initiatives / collective action",
+          "Monitoring and improvement processes",
+          "Other"
+        ]
+      ],
+      [:anti_corruption, true, "Outcomes: Does the COP contain information on outcomes of your anti-corruption policies and activities?", nil, 3,
+        ["Qualitative outcomes (e.g. public legal cases regarding corruption; actions taken in response to incidents of corruption)",
+          "Quantitative outcomes using defined indicators (e.g. percentage and total number of business units analyzed for risks related to corruption)",
+          "Expected outcomes/targets"
+        ]
+      ],
+    ].each do |record|
+      question = CopQuestion.create(:principle_area_id => PrincipleArea.send(record.first).id,
+                                    :area_selected     => record.second,
+                                    :text              => record.third,
+                                    :initiative_id     => record.fourth,
+                                    :position          => record.fifth)
+      record.sixth.each_with_index do |attribute, i|
+        question.cop_attributes.create(:text => attribute, :position => i)
+      end
+    end
+  end
+  
+  def populate_principle_areas
+    [
+      'Human Rights',
+      'Labour',
+      'Environment',
+      'Anti-Corruption'
+    ].each do |string|
+      p = PrincipleArea.find_by_name(string) or PrincipleArea.create(:name => string)
+    end
+  end
+end
---
+++ lib/importer_mapping.rb
@@ -0,0 +1,135 @@
+module ImporterMapping
+  FILES = [:country, :organization_type, :sector, :exchange, :listing_status, :language, :removal_reason,
+            :cop_score, :principle, :interest, :role, :organization, :contact, :communication_on_progress,
+            :logo_publication, :logo_request, :logo_file, :logo_comment, :case_story, :initiative, :signing,
+            :bulletin_subscriber]
+  CONFIG = {
+    #fields: ID	NAME
+    :initiative => {
+      :file => 'TR13_INITIATIVES.csv',
+      :fields => [ :old_id, :name ]
+    },
+    #fields: ID	INITIATIVE_ID	ORG_ID	DATE_ADDED
+    :signing => {
+      :file => 'R16_XREF_R01_TR13.csv',
+      :fields => [:old_id, :initiative_id, :organization_id, :added_on]
+    },
+    #fields: COUNTRY_ID	COUNTRY_NAME	COUNTRY_REGION	COUNTRY_NETWORK_TYPE	GC_COUNTRY_MANAGER
+    :country => {:file => 'TR01_COUNTRY.csv', :fields => [:code, :name, :region, :network_type, nil]},
+    # fields: ID	TYPE	TYPE_PROPERTY
+    :organization_type => {:file => 'TR02_ORGANIZATION_TYPE.csv', :fields => [nil, :name, :type_property]},
+    #fields: SECTOR_NAME	SECTOR_ID	ICB_NUMBER	SUPER_SECTOR
+    :sector => {:file => 'TR03_SECTOR.csv', :fields => [:name, :old_id, :icb_number, :parent_id]},
+    #fields: COP_SCORE	COP_SCORE_DESCRIPTION
+    :cop_score => {:file => 'TR04_COP_SCORE.csv', :fields => [:old_id, :description]},
+    # fields: PRINCIPLE_ID	PRINCIPLE_NAME
+    :principle => {:file => 'TR05_PRINCIPLE.csv', :fields => [:old_id, :name]},
+    # fields: INTEREST_ID	INTEREST_NAME
+    :interest  => {:file => 'TR06_INTEREST.csv', :fields => [:old_id, :name]},
+    # fields: ROLE_ID ROLE_NAME
+    :role      => {:file => 'TR07_ROLE.csv', :fields => [:old_id, :name]},
+    #fields LIST_EXCHANGE_CODE	LIST_EXCHANGE_NAME	LIST_SECONDARY_CODE	LIST_TERTIARY_CODE	TR01_COUNTRY_ID
+    :exchange => {:file => 'TR08_EXCHANGE.csv', :fields => [:code, :name, :secondary_code, :terciary_code, :country_id]},
+    # fields: Listing_ID	Listing_Name
+    :listing_status => {:file => 'TR09_LISTING_STATUS.csv', :fields => [:old_id, :name]},
+    # fields: LANGUAGE_ID	LANGUAGE_NAME
+    :language => {:file => 'TR10_LANGUAGE.csv', :fields => [:old_id, :name]},
+    # fields: REASON_ID	REASON_DESCRIPTION
+    :removal_reason => {:file => 'TR11_REASON_FOR_REMOVAL.csv', :fields => [:old_id, :description]},
+    # fields: ID	NAME	PARENT_ID	DISPLAY_ORDER
+    :logo_publication => {:file => 'TR14_LOGO_PUBLICATIONS.csv', :fields => [:old_id, :name, :parent_id, :display_order]},
+    # fields: ID	DATE_REQUESTED	DATE_STATUS	PUBLICATION_ID	ORG_ID	CONTACT_ID	REVIEWER_ID	REPLIED_TO
+    #             PURPOSE	REQUEST_STATUS	POLICY_ACCEPTED	POLICY_ACCEPTED_DATE
+    :logo_request => {:file   => 'TR15_LOGO_REQUESTS.csv',
+                      :fields => [:old_id, :requested_on, :status_changed_on, :publication_id, :organization_id,
+                                    :contact_id, :reviewer_id, :replied_to, :purpose, :status, :accepted, :accepted_on]},
+    # fields: ID	LOGO_NAME	DESCRIPTION	THUMBNAIL	LOGOFILE
+    :logo_file => {:file => 'TR18_LOGO_FILES.csv', :fields => [:old_id, :name, :description, :thumbnail, :file]},
+    # fields: ID	COMMENT_DATE	LOGO_REQUEST_ID	CONTACT_ID	TEXT
+    :logo_comment => {:file => 'TR17_LOGO_COMMENTS.csv', :fields => [:old_id, :added_on, :logo_request_id, :contact_id, :body]},
+
+    #fields: ID	TR02_TYPE	ORG_NAME	SECTOR_ID ORG_NETWORK_BIT	ORG_PARTICIPANT_BIT	ORG_NB_EMPLOY	ORG_URL
+    #        ORG_BHR_URL	ORG_DATE_CREATE	ORG_DATE_MODIFICATION	ORG_DATE_JOINING	ORG_DATE_DELISTED	ORG_ACTIVE
+    #        ORG_ISGLOBAL500	TR01_COUNTRY_ID	ORG_STATUS_COP	ORG_COP_ALERT	ORG_UN_SPEC	ORG_PART_MAIL
+    #        ORG_30DAYS_TO_COP	ORG_90DAYS_TO_COP	ORG_TODAY_COPDUE	ORG_TODAY_INACT	ORG_30DAYS_TO_INACT
+    #        ORG_90DAYS_TO_INACT	ORG_MEMBER_ONEYEAR	ORG_LISTED_STATUS	ORG_LIST_CODE	ORG_LIST_EXCHANGE_CODE
+    #        ORG_PARENT_NAME	ORG_PARENT_LIST_CODE	TR11_REASON	ORG_LAST_MODIFIED_BY	ORG_JOIN_LETTER
+    :organization => {:file   => 'R01_ORGANIZATION.csv',
+                      :fields => {
+                          "ID"                     => :old_id,
+                          "TR02_TYPE"              => :organization_type_id,
+                          "ORG_NAME"               => :name,
+                          "SECTOR_ID"              => :sector_id,
+                          "ORG_NETWORK_BIT"        => :local_network,
+                          "ORG_PARTICIPANT_BIT"    => :participant,
+                          "ORG_NB_EMPLOY"          => :employees,
+                          "ORG_URL"                => :url,
+                          "ORG_BHR_URL"            => nil,
+                          "ORG_DATE_CREATE"        => :added_on,
+                          "ORG_DATE_MODIFICATION"  => :modified_on,
+                          "ORG_DATE_JOINING"       => :joined_on,
+                          "ORG_DATE_DELISTED"      => :delisted_on,
+                          "ORG_ACTIVE"             => :active,
+                          "ORG_ISGLOBAL500"        => :is_ft_500,
+                          "TR01_COUNTRY_ID"        => :country_id,
+                          "ORG_STATUS_COP"         => :cop_status,
+                          "ORG_COP_ALERT"          => nil,
+                          "ORG_UN_SPEC"            => nil,
+                          "ORG_PART_MAIL"          => nil,
+                          "ORG_30DAYS_TO_COP"      => nil,
+                          "ORG_90DAYS_TO_COP"      => nil,
+                          "ORG_TODAY_COPDUE"       => :cop_due_on,
+                          "ORG_TODAY_INACT"        => :inactive_on,
+                          "ORG_30DAYS_TO_INACT"    => nil,
+                          "ORG_90DAYS_TO_INACT"    => nil,
+                          "ORG_MEMBER_ONEYEAR"     => :one_year_member_on,
+                          "ORG_LISTED_STATUS"      => :listing_status_id,
+                          "ORG_LIST_CODE"          => :stock_symbol,
+                          "ORG_LIST_EXCHANGE_CODE" => :exchange_id,
+                          "ORG_PARENT_NAME"        => nil,
+                          "ORG_PARENT_LIST_CODE"   => nil,
+                          "TR11_REASON"            => :removal_reason_id,
+                          "ORG_LAST_MODIFIED_BY"   => :last_modified_by_id,
+                          "ORG_JOIN_LETTER"        => nil
+                        }.values
+                      
+                      },
+    # fields: CONTACT_ID	CONTACT_FNAME	CONTACT_MNAME	CONTACT_LNAME	CONTACT_PREFIX	CONTACT_JOB_TITLE	CONTACT_EMAIL
+    #         CONTACT_PHONE	CONTACT_MOBILE	CONTACT_FAX	R01_ORG_NAME	CONTACT_ADRESS	CONTACT_CITY	CONTACT_STATE
+    #         CONTACT_CODE_POSTAL	TR01_COUNTRY_ID	CONTACT_IS_CEO	CONTACT_IS_CONTACT_POINT	CONTACT_IS_NEWSLETTER
+    #         CONTACT_IS_ADVISORY_COUNCIL	CONTACT_LOGIN	CONTACT_PWD	CONTACT_ADDRESS2
+    :contact => {:file   => 'R10_CONTACT.csv',
+                 :fields => [:old_id, :first_name, :middle_name, :last_name, :prefix, :job_title, :email,
+                              :phone, :mobile, :fax, :organization_id, :address, :city, :state,
+                              :postal_code, :country_id, :ceo, :contact_point, :newsletter, 
+                              :advisory_council, :login, :password, :address_more]},
+    # fields: COP_ID	R01_ORG_NAME	COP_TITLE	COP_DOCUMENT_RELATED	COP_CONTACT_EMAIL	COP_PERIOD_START_YEAR	
+    #         COP_FACILITATOR	COP_CONTACT_TITLE	COP_PERIOD_START_MONTH	COP_PERIOD_END_MONTH	COP_PERIOD_LINK1
+    #         COP_PERIOD_LINK2	COP_PERIOD_LINK3	COP_DATE_CREATE	COP_DATE_MODIFICATION	COP_CONTACT_PERSON
+    #         COP_PERIOD_END_YEAR	COP_DOCUMENT_STATUS	COP_CEO_COMMIT_LETTER	COP_DESCRIPTION_ACTIONS	COP_USE_INDICATORS
+    #         TR04_SCORE	COP_USE_GRI	COP_HAS_CERTIFICATION	COP_NOTABLE_PROGRAM
+    :communication_on_progress => {:file   => 'R02_COM_ON_PROCESS.csv',
+                                   :fields => [:identifier, :organization_id, :title, :related_document, :email, :start_year,
+                                               :facilitator, :job_title, :start_month, :end_month, :url1,
+                                               :url2, :url3, :added_on, :modified_on, :contact_name,
+                                               :end_year, :status, :include_ceo_letter, :include_actions, :use_indicators,
+                                               :cop_score_id, :use_gri, :has_certification]},
+    # fields: CASE_ID	R01_ORG_NAME	CASE_TITLE	CASE_STORY_TYPE	CASE_CATEGORY	CASE_DATE	CASE_DESCRIPTION
+    #         CASE_URL_LINK1	CASE_URL_LINK2	CASE_URL_LINK3	CASE_AUTHOR	CASE_AUTHOR_INSTITUTION
+    #         CASE_AUTHOR_EMAIL	CASE_AUTHOR2	CASE_AUTHOR2_INSTITUTION	CASE_AUTHOR2_EMAIL	CASE_REVIEWER
+    #         CASE_REVIEWER_INSTITUTION	CASE_REVIEWER_EMAIL	CASE_REVIEWER2	CASE_REVIEWER2_INSTITUTION
+    #         CASE_REVIEWER2_EMAIL	CASE_FILE	CASE_CONTACT1_NAME	CASE_CONTACT1_EMAIL	CASE_CONTACT2_NAME
+    #         CASE_CONTACT2_EMAIL	CASE_STATUS	CASE_DOC_EXT
+    :case_story => {:file   => 'R07_CASE_STORY.csv',
+                    :fields => [:identifier, :organization_id, :title, nil, :category, :case_date, :description,
+                                :url1, :url2, :url3, :author1, :author1_institution,
+                                :author1_email, :author2, :author2_institution, :author2_email, :reviewer1,
+                                :reviewer1_institution, :reviewer1_email, :reviewer2, :reviewer2_institution,
+                                :reviewer2_email, :uploaded, :contact1, :contact1_email, :contact2,
+                                :contact2_email, :status, :extension]},
+                                
+    #fields: "ID","FIRST_NAME","LAST_NAME","ORG_NAME","EMAIL"
+    :bulletin_subscriber => {:file  => 'TR20_GCB_SUBSCRIBERS.csv',
+                             :fields => [nil, :first_name, :last_name, :organization_name, :email]}
+  }
+end
