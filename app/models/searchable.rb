dbd5b8d23ebf350ee97befea52f3eabee22369a6
Joshua Wehner <joshua@unspace.ca>
2009-11-10 13:06:19 -0500

    Tweaks to search results display: weight by title, better excepted text, highlight matching text

--- app/models/searchable.rb
+++ app/models/searchable.rb
@@ -44,6 +44,7 @@ class Searchable < ActiveRecord::Base
     has document_type, :facet => true
     has url, last_indexed_at
     set_property :delta => true # TODO: Switch this to :delayed once we have DJ working
+    set_property :field_weights => {"title" => 100}
   end
   
   class << self 
