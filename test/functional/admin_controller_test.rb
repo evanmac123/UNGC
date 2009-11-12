5cf41575b067505cbeca33d40691d29c9724c548
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-11-12 11:17:18 -0500

    Added some data for ungc dashboard test

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -4,10 +4,13 @@ class AdminControllerTest < ActionController::TestCase
   
   context "given a UNGC staff member" do
     setup do
+      create_organization_and_user
+      add_organization_data(@organization, @organization_user)
       login_as create_staff_user
     end
 
     should "get the dashboard page" do
+      # now get the dashboard
       get :dashboard
       assert_response :success
       assert_template 'admin/dashboard_ungc.html.haml'
@@ -18,12 +21,7 @@ class AdminControllerTest < ActionController::TestCase
     setup do
       user = create_organization_and_user
       @organization.update_attribute :state, 'approved'
-      # add some content to the organization
-      create_logo_publication
-      create_communication_on_progress(:organization_id => @organization.id)
-      create_case_story(:organization_id => @organization.id)
-      create_logo_request(:organization_id => @organization.id,
-                          :contact_id      => user.id)
+      add_organization_data(@organization, user)
       login_as user
     end
 
@@ -33,4 +31,14 @@ class AdminControllerTest < ActionController::TestCase
       assert_template 'admin/dashboard_organization.html.haml'
     end
   end
+  
+  private
+    def add_organization_data(organization, user)
+      # add some content to the organization
+      create_logo_publication
+      create_communication_on_progress(:organization_id => organization.id)
+      create_case_story(:organization_id => organization.id)
+      create_logo_request(:organization_id => organization.id,
+                          :contact_id      => user.id)
+    end
 end
