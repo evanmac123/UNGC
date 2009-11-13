8d7f0756f49c64fa168e81f2e303484ea3787e58
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-11-13 12:32:49 -0500

    Added code to send COP reminder emails

--- lib/cop_reminder.rb
+++ lib/cop_reminder.rb
@@ -1,12 +1,25 @@
 class CopReminder
   def notify_all
-    notify_cop_due_in_90.days
+    notify_cop_due_in_90_days
     notify_cop_due_in_30_days
     notify_cop_due_today
   end
   
-  def notify_cop_due_in_90.days
-    organizations = with_cop_due_on(90.days.from_now.to_date)
-    organizations.each {|org| Mailer.deliver_cop_due_in_90(org) }
+  def notify_cop_due_in_90_days
+    notify_cop_due_on 90.days.from_now.to_date, :deliver_cop_due_in_90_days
   end
+  
+  def notify_cop_due_in_30_days
+    notify_cop_due_on 30.days.from_now.to_date, :deliver_cop_due_in_30_days
+  end
+  
+  def notify_cop_due_today
+    notify_cop_due_on Date.today, :deliver_cop_due_today
+  end
+  
+  private
+    def notify_cop_due_on(date, mailer)
+      organizations = Organization.with_cop_due_on(date)
+      organizations.each {|org| CopMailer.send(mailer, org) }
+    end
 end
