7f565e1adc076373768cab63bd4dd28bce3ed891
Venu Keesari <keesari@unglobalcompact.org>
2009-11-19 16:36:07 -0500

    Add Foundation emails to organization mailer

--- app/models/organization_mailer.rb
+++ app/models/organization_mailer.rb
@@ -30,4 +30,21 @@ class OrganizationMailer < ActionMailer::Base
     recipients organization.contacts.contact_points.collect(&:email_recipient)
     body :organization => organization, :contact => organization.contacts.contact_points.first
   end
+  
+  def foundation_invoice(organization)
+    from 'Foundation for the Global Compact <foundation@unglobalcompact.org>'
+    subject "Your pledge to The Foundation for the Global Compact"
+    content_type "text/html"
+    recipients organization.contacts.contact_points.collect(&:email_recipient)
+    body :organization => organization, :contact => organization.contacts.contact_points.first
+  end
+
+  def foundation_reminder(organization)
+    from 'Foundation for the Global Compact <foundation@unglobalcompact.org>'
+    subject "A message from The Foundation for the Global Compact"
+    content_type "text/html"
+    recipients organization.contacts.contact_points.collect(&:email_recipient)
+    body :organization => organization, :contact => organization.contacts.contact_points.first
+  end
+  
 end
