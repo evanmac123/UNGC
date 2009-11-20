60cbe8a150f616de2490d951d88733c5a530c123
Venu Keesari <keesari@unglobalcompact.org>
2009-11-20 12:08:32 -0500

    Converted Reports csv output to tab delimited - replaced render_csv method with render_xls - changed date format to use _ - did not change xls MIME type

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,7 +2,7 @@ class Admin::ReportsController < AdminController
 
   def participant_breakdown
     @report = ParticipantBreakdownReport.new
-    render_formatter(filename: "participant_breakdown_#{date_as_filename}.csv")
+    render_formatter(filename: "participant_breakdown_#{date_as_filename}.xls")
   end
   
   def approved_logo_requests
@@ -11,22 +11,22 @@ class Admin::ReportsController < AdminController
     
     @report = ApprovedLogoRequestsReport.new(:month => @month,
                                              :year  => @year)
-    render_formatter(filename: "approved_logo_requests_#{date_as_filename}.csv")
+    render_formatter(filename: "approved_logo_requests_#{date_as_filename}.xls")
   end
   
   def listed_companies
     @report = ListedCompaniesReport.new
-    render_formatter(filename: "listed_companies_#{date_as_filename}.csv")
+    render_formatter(filename: "listed_companies_#{date_as_filename}.xls")
   end
 
   def bulletin_subscribers
     @report = BulletinSubscribersReport.new
-    render_formatter(filename: "bulletin_subscribers_#{date_as_filename}.csv")
+    render_formatter(filename: "bulletin_subscribers_#{date_as_filename}.xls")
   end
   
   def companies_without_contacts
     @report = SimpleOrganizationReport.new
-    render_formatter(filename: "companies_without_contract_#{date_as_filename}.csv")
+    render_formatter(filename: "companies_without_contacts_#{date_as_filename}.xls")
   end
   
   def networks
@@ -37,7 +37,7 @@ class Admin::ReportsController < AdminController
     
     @report = NetworksReport.new(:region  => @region,
                                  :country => @country)
-    render_formatter(filename: "networks_report_#{date_as_filename}.csv")
+    render_formatter(filename: "networks_report_#{date_as_filename}.xls")
   end
   
   def foundation_pledges
@@ -46,20 +46,20 @@ class Admin::ReportsController < AdminController
     
     @report = FoundationPledgeReport.new(:month => @month,
                                          :year  => @year)
-    render_formatter(filename: "foundation_pledges_#{date_as_filename}.csv")
+    render_formatter(filename: "foundation_pledges_#{date_as_filename}.xls")
   end
   
   private
     def render_formatter(options={})
       respond_to do |format|
         format.html
-        format.csv  { send_data @report.render_csv, :type     => 'application/ms-excel',
+        format.csv  { send_data @report.render_xls, :type     => 'application/ms-excel',
                                                     :filename => options[:filename] }
       end
     end
 
     def date_as_filename
-      [Date.today.month, Date.today.day, Date.today.year].join('.')
+      [Date.today.year, Date.today.month, Date.today.day].join('_')
     end
 
 end
