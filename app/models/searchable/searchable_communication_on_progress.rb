2621eb445a11a76e7a3d503ff4367568c2452b9b
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-11-23 11:45:39 -0500

    Indexing COPs - including files

---
+++ app/models/searchable/searchable_communication_on_progress.rb
@@ -0,0 +1,27 @@
+module Searchable::SearchableCommunicationOnProgress
+  def index_communication_on_progress(cop)
+    title   = cop.title
+    url     = with_helper { cop_detail_path(:organization => cop.organization, :cop => cop) }
+    # import cop files
+    file_content = []
+    for file in cop.cop_files
+      if file.attachment_content_type =~ /^application\/.*pdf$/
+        file_content << get_text_from_pdf(file.attachment.path)
+      elsif file.attachment_content_type =~ /doc/
+        file_content << get_text_from_word(file.attachment.path)
+      end
+    end
+    content = <<-EOF
+      #{cop.description}
+      #{cop.principles.map(&:name).join(' ')}
+      #{cop.countries.map(&:name).join(' ')}
+    EOF
+    content = "#{content.force_encoding('UTF-8')} #{file_content.join(' ')}"
+    
+    import 'CommunicationOnProgress', url: url, title: title, content: content, object: cop
+  end
+  
+  def index_communications_on_progress
+    CommunicationOnProgress.approved.each { |cop| index_communication_on_progress cop }
+  end
+end
