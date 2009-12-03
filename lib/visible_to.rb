2dcbb70d2ad639784d2873ec08894d1afe624c6b
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-12-03 11:43:30 -0500

    Use user_type for checking scope

--- lib/visible_to.rb
+++ lib/visible_to.rb
@@ -2,9 +2,9 @@ module VisibleTo
   def self.included(klass)
     klass.class_eval do
       named_scope :visible_to, lambda { |user|
-        if user.from_organization?
+        if user.user_type == Contact::TYPE_ORGANIZATION
           { :conditions => ['organization_id=?', user.organization_id] }
-        elsif user.from_network?
+        elsif user.user_type == Contact::TYPE_NETWORK
           { :conditions => ["organizations.country_id in (?)", user.local_network.country_ids],
             :include    => :organization }
         else
