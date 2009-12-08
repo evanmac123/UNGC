b4f0e3504f6eaadd8ed35abf210fdaff38d7109f
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-12-08 15:35:41 -0500

    Added div for additional partnership-related questions

--- public/javascripts/cop_form.js
+++ public/javascripts/cop_form.js
@@ -1,44 +1,3 @@
-// COP form
-$("#communication_on_progress_references_human_rights").change(function() {
-  if ($('#communication_on_progress_references_human_rights:checked').is(':checked')) {
-	$('#human_rights_addressed').show();
-	$('#human_rights_not_addressed').hide();
-  } else {
-	$('#human_rights_addressed').hide();
-	$('#human_rights_not_addressed').show();
-  }
-})
-
-$("#communication_on_progress_references_labour").change(function() {
-  if ($('#communication_on_progress_references_labour:checked').is(':checked')) {
-	$('#labour_addressed').show();
-	$('#labour_not_addressed').hide();
-  } else {
-	$('#labour_addressed').hide();
-	$('#labour_not_addressed').show();
-  }
-})
-
-$("#communication_on_progress_references_environment").change(function() {
-  if ($('#communication_on_progress_references_environment:checked').is(':checked')) {
-	$('#environment_addressed').show();
-	$('#environment_not_addressed').hide();
-  } else {
-	$('#environment_addressed').hide();
-	$('#environment_not_addressed').show();
-  }
-})
-
-$("#communication_on_progress_references_anti_corruption").change(function() {
-  if ($('#communication_on_progress_references_anti_corruption:checked').is(':checked')) {
-	$('#anti_corruption_addressed').show();
-	$('#anti_corruption_not_addressed').hide();
-  } else {
-	$('#anti_corruption_addressed').hide();
-	$('#anti_corruption_not_addressed').show();
-  }
-})
-
 // Format
 $("input[name='communication_on_progress[format]']").change(function() {
   if ($("#communication_on_progress_format_grace_letter").is(':checked')) {
@@ -160,5 +119,14 @@ $("input[class='score']").change(function() {
     $("#anti_corruption_additional_questions").show();
   } else {
     $("#anti_corruption_additional_questions").hide();
+  }
+})
+
+// partnership project
+$("input[name='communication_on_progress[mentions_partnership_project]']").change(function() {
+  if ($("#communication_on_progress_mentions_partnership_project_true").is(':checked')) {
+	$("#partnership_additional_questions").show();
+  } else {
+	$("#partnership_additional_questions").hide();
   }
 })
