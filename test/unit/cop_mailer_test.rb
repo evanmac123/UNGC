1a8465c053f9f4fbbfa22f92034cbed47cbcb346
Venu Keesari <keesari@unglobalcompact.org>
2009-12-09 16:57:06 -0500

    Text changes to COP mailers - changed subject in model and test

--- test/unit/cop_mailer_test.rb
+++ test/unit/cop_mailer_test.rb
@@ -10,21 +10,21 @@ class CopMailerTest < ActionMailer::TestCase
     should "be able to send 90 days reminder" do
       response = CopMailer.deliver_cop_due_in_90_days(@organization)
       assert_equal "text/html", response.content_type
-      assert_equal "COP due in 90 days", response.subject
+      assert_equal "Your UN Global Compact participation - Communication on Progress required in 90 days", response.subject
       assert_equal "email@example.com", response.to.first
     end  
 
     should "be able to send 30 days reminder" do
       response = CopMailer.deliver_cop_due_in_30_days(@organization)
       assert_equal "text/html", response.content_type
-      assert_equal "COP due in 30 days", response.subject
+      assert_equal "Your UN Global Compact participation - Communication on Progress required in 30 days", response.subject
       assert_equal "email@example.com", response.to.first
     end  
   
     should "be able to send today's reminder" do
       response = CopMailer.deliver_cop_due_today(@organization)
       assert_equal "text/html", response.content_type
-      assert_equal "COP due today", response.subject
+      assert_equal "Your organization is Non-Communicating with the UN Global Compact", response.subject
       assert_equal "email@example.com", response.to.first
     end  
   end  
