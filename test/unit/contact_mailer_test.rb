36a4c924eda90b94485b23a56be13ab3c6fa6c59
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-12-09 13:36:29 -0500

    Fix for test failure

--- test/unit/contact_mailer_test.rb
+++ test/unit/contact_mailer_test.rb
@@ -8,7 +8,7 @@ class ContactMailerTest < ActionMailer::TestCase
   test "reset password mailer is sent" do
     response = ContactMailer.deliver_reset_password(@organization_user)
     assert_equal "text/html", response.content_type
-    assert_equal "Reset Your Password", response.subject
+    assert_equal "United Nations Global Compact - Reset Password", response.subject
     assert_equal "email@example.com", response.to.first
   end
 end
