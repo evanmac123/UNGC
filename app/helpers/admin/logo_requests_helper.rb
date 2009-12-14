4d2e3cddac099186ed38684584d9e5df9c4e0101
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-12-14 18:28:53 -0500

    Starting implementing design for logo request page

--- app/helpers/admin/logo_requests_helper.rb
+++ app/helpers/admin/logo_requests_helper.rb
@@ -17,4 +17,18 @@ module Admin::LogoRequestsHelper
   def comment_date(logo_comment)
     logo_comment.added_on ? logo_comment.added_on : logo_comment.created_at
   end
+  
+  def link_to_attached_file(logo_comment)
+    if logo_comment.attachment_file_name.downcase.ends_with?('.pdf')
+      file_type = 'PDF'
+    elsif logo_comment.attachment_file_name.downcase.ends_with?('.doc') || 
+            logo_comment.attachment_file_name.downcase.ends_with?('.docx')
+      file_type = 'Word'
+    else
+      file_type = 'Other'
+    end
+
+    link_to "#{file_type} document", logo_comment.attachment.url, :class => "#{file_type.downcase}_doc",
+                                                                  :title => "Download #{file_type} document"
+  end
 end
