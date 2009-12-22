4cd66d46c0c7882ca3b43ff4e2bbd983d4ca9ae5
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-12-22 13:57:41 -0500

    Removing some old code

--- app/helpers/admin/logo_requests_helper.rb
+++ app/helpers/admin/logo_requests_helper.rb
@@ -1,16 +1,10 @@
 module Admin::LogoRequestsHelper
-  def logo_request_actions(logo_request)
-    actions = []
-    actions << link_to('Back', admin_organization_path(@organization.id))
-    actions.join(" | ")
-  end
-  
   def logo_file_image_tag(logo_file)
-    # TODO we'll no longer use demo.unglobalcompact.org
     image_tag logo_file_image_url(logo_file), :alt => logo_file.name
   end
   
   def logo_file_image_url(logo_file)
+    # TODO no longer use demo.unglobalcompact.org
     "http://demo.unglobalcompact.org/admin/images/gc_logos/#{logo_file.thumbnail}"
   end
   
