9d13a85c9309f3a4fbe75014f50c322082583f9e
Venu Keesari <keesari@unglobalcompact.org>
2009-12-29 16:06:18 -0500

    Resolve additional conflicts from merge

--- test/unit/cop_mailer_test.rb
+++ test/unit/cop_mailer_test.rb
@@ -55,8 +55,40 @@ class CopMailerTest < ActionMailer::TestCase
       assert_equal "text/html", response.content_type
       assert_equal "Your organization has been delisted from the Global Compact", response.subject
       assert_equal "email@example.com", response.to.first
-    end  
-
+    end
   end  
+
+  context "given a non-communicating organization with a local network" do
+    setup do
+      create_organization_and_user
+      create_local_network_with_report_recipient
+      @organization = create_organization(:name => 'UNGC', :country_id => @country.id)
+      @organization.communication_late
+    end
+
+    should "be able to send 90 days until delisting to network report recipient" do
+      response = CopMailer.deliver_cop_due_in_90_days_notify_network(@organization)
+      assert_equal 1, @organization.network_report_recipients.count
+      assert_equal "text/html", response.content_type
+      assert_equal "#{@organization.name} - Communication on Progress required in 90 days", response.subject
+      assert_equal @network_contact.email, response.to.first
+    end
+    
+    should "be able to send 30 days until delisting to local network" do
+      response = CopMailer.deliver_delisting_in_30_days_notify_network(@organization)
+      assert_equal 1, @organization.network_report_recipients.count
+      assert_equal "text/html", response.content_type
+      assert_equal "#{@organization.name} is at risk of being delisted from the Global Compact in 30 days", response.subject
+      assert_equal @network_contact.email, response.to.first
+    end
+    
+    should "be able to send notice of delisting today to network report recipient" do
+      response = CopMailer.deliver_delisting_today_notify_network(@organization)
+      assert_equal 1, @organization.network_report_recipients.count
+      assert_equal "text/html", response.content_type
+      assert_equal "#{@organization.name} has been delisted from the Global Compact", response.subject
+      assert_equal @network_contact.email, response.to.first
+    end
+  end
   
 end
