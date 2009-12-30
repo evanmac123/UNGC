b69c947adb544a785695f68373cf100c82cde77a
Venu Keesari <keesari@unglobalcompact.org>
2009-12-30 10:37:55 -0500

    WIP: add Local Networks to COP reminder emails, need to fix test error -  network email is not recognized as symbol - TypeError: [:deliver_cop_due_in_90_days_notify_network] is not a symbol

--- lib/cop_reminder.rb
+++ lib/cop_reminder.rb
@@ -6,22 +6,38 @@ class CopReminder
   end
   
   def notify_cop_due_in_90_days
-    notify_cop_due_on Organization.businesses.participants.with_cop_status(:active).with_cop_due_on(90.days.from_now.to_date), :deliver_cop_due_in_90_days
-    notify_cop_due_on Organization.businesses.participants.with_cop_status(:noncommunicating).with_cop_due_on(90.days.from_now.to_date), :deliver_delisting_in_90_days
+    notify_cop_due_on Organization.businesses.participants.with_cop_status(:active).with_cop_due_on(90.days.from_now.to_date),
+                      :deliver_cop_due_in_90_days, :deliver_cop_due_in_90_days_notify_network
+
+    notify_cop_due_on Organization.businesses.participants.with_cop_status(:noncommunicating).with_cop_due_on(90.days.from_now.to_date),
+                      :deliver_delisting_in_90_days
   end
   
   def notify_cop_due_in_30_days
-    notify_cop_due_on Organization.businesses.participants.with_cop_status(:active).with_cop_due_on(30.days.from_now.to_date), :deliver_cop_due_in_30_days
-    notify_cop_due_on Organization.businesses.participants.with_cop_status(:noncommunicating).with_cop_due_on(30.days.from_now.to_date), :deliver_delisting_in_30_days
+    notify_cop_due_on Organization.businesses.participants.with_cop_status(:active).with_cop_due_on(30.days.from_now.to_date),
+                      :deliver_cop_due_in_30_days, :deliver_cop_due_in_30_days_notify_network
+
+    notify_cop_due_on Organization.businesses.participants.with_cop_status(:noncommunicating).with_cop_due_on(30.days.from_now.to_date),
+                      :deliver_delisting_in_30_days
   end
   
   def notify_cop_due_today
-    notify_cop_due_on Organization.businesses.participants.with_cop_status(:active).with_cop_due_on(Date.today), :deliver_cop_due_today
-    notify_cop_due_on Organization.businesses.participants.with_cop_status(:noncommunicating).with_cop_due_on(Date.today), :deliver_delisting_today
+    notify_cop_due_on Organization.businesses.participants.with_cop_status(:active).with_cop_due_on(Date.today),
+                      :deliver_cop_due_today, :deliver_cop_due_today_notify_network
+
+    notify_cop_due_on Organization.businesses.participants.with_cop_status(:noncommunicating).with_cop_due_on(Date.today),
+                      :deliver_delisting_today, :deliver_delisting_today_notify_network
   end
   
   private
-    def notify_cop_due_on(organizations, mailer)
-      organizations.each { |org| CopMailer.send(mailer, org) }
+
+  def notify_cop_due_on(organizations, mailer, *network_mailer)
+    organizations.each do |org|
+      CopMailer.send(mailer, org)
+      if network_mailer && org.network_report_recipients.count > 0
+        CopMailer.send(network_mailer, org)
+      end      
     end
+  end
+
 end
