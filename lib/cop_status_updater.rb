a3395b186fc1d473ef6a21125c42acbe0cd779ba
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-12-31 13:47:33 -0500

    All methods are class methods

--- lib/cop_status_updater.rb
+++ lib/cop_status_updater.rb
@@ -1,16 +1,18 @@
 class CopStatusUpdater
-  def update_all
-    move_active_organizations_to_noncommunicating
-    move_noncommunicating_organizations_to_delisted
-  end
-  
-  def move_active_organizations_to_noncommunicating
-    organizations = Organization.businesses.participants.about_to_become_noncommunicating
-    organizations.each { |org| org.communication_late }
-  end
-  
-  def move_noncommunicating_organizations_to_delisted
-    organizations = Organization.businesses.participants.about_to_become_delisted
-    organizations.each { |org| org.delist }
+  class << self
+    def update_all
+      move_active_organizations_to_noncommunicating
+      move_noncommunicating_organizations_to_delisted
+    end
+
+    def move_active_organizations_to_noncommunicating
+      organizations = Organization.businesses.participants.about_to_become_noncommunicating
+      organizations.each { |org| org.communication_late }
+    end
+
+    def move_noncommunicating_organizations_to_delisted
+      organizations = Organization.businesses.participants.about_to_become_delisted
+      organizations.each { |org| org.delist }
+    end
   end
 end
