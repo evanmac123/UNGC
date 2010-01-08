139fd8458c87d6cfeefcf95baf21af0431b3fc1c
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-01-08 12:25:14 -0500

    Using the logo preview from the model

--- app/helpers/admin/logo_requests_helper.rb
+++ app/helpers/admin/logo_requests_helper.rb
@@ -1,8 +1,4 @@
 module Admin::LogoRequestsHelper
-  def logo_file_image_tag(logo_file)
-    image_tag logo_file_image_url(logo_file), :alt => logo_file.name
-  end
-  
   def comment_date(logo_comment)
     logo_comment.added_on ? logo_comment.added_on : logo_comment.created_at
   end
