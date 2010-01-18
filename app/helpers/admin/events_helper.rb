f5e773b4b5439138ae22279dbe088aedc95a905a
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-01-18 15:27:32 -0500

    First implementation of table headers for events

--- app/helpers/admin/events_helper.rb
+++ app/helpers/admin/events_helper.rb
@@ -1,6 +1,7 @@
 module Admin::EventsHelper
   def paged_events
-    @paged_events ||= Event.paginate(:page => (params[:page] || 1))
+    @paged_events ||= Event.paginate(:page => (params[:page] || 1),
+                                     :order => order_from_params)
   end
   
   def date_range(event)
