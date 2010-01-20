516e104d5cb7c91547044f0702020716090343ba
Venu Keesari <keesari@unglobalcompact.org>
2010-01-19 21:50:33 -0500

    COP updates - language changes - add boolean column for web-based COPs

---
+++ db/migrate/20100120012922_add_cops_web_based_only.rb
@@ -0,0 +1,9 @@
+class AddCopsWebBasedOnly < ActiveRecord::Migration
+  def self.up
+    add_column :communication_on_progresses, :web_based, :boolean
+  end
+
+  def self.down
+    remove_column :communication_on_progresses, :web_based, :boolean
+  end
+end
