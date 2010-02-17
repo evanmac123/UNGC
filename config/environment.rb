89abb176d5cdcd4f89c964354d782cc1d5eae12b
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-02-17 16:45:35 -0500

    Removed shoulda from list of required gems, it's only required for testing

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,6 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
   config.gem 'thinking-sphinx', :lib => 'thinking_sphinx', :version => '1.3.14'
-  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
 
   config.gem 'money'
   config.gem 'haml'
