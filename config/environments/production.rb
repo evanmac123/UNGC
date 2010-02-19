a494ef4dba02ccf19e48b4895487c3d389ed6fbd
Venu Keesari <keesari@unglobalcompact.org>
2010-02-18 23:16:49 -0500

    Changed SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,11 @@ config.action_view.cache_template_loading            = true
 # config.action_mailer.raise_delivery_errors = false
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :address        => "mail.unglobalcompact.org",
-  :port           => 26,
+  :address        => "smtp.emailsrvr.com",
+  :port           => 25,
   :domain         => "unglobalcompact.org",
   :authentication => :login,
-  :user_name      => "info+unglobalcompact.org",
+  :user_name      => "info@unglobalcompact.org",
   :password       => "ungcmail2010"
 }
 
