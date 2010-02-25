f2a0861e0e0ac0fa95dafc1cbbbdb609d8d980f1
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2010-02-25 11:31:55 -0500

    Disable caching on development

--- app/models/page_sweeper.rb
+++ app/models/page_sweeper.rb
@@ -4,9 +4,11 @@ class PageSweeper < ActionController::Caching::Sweeper
   def after_update(page)
     # only expire cache if there are no pending version for the path
     if Page.pending_version_for(page.path).nil?
-      expire_page(page.path)
-      # FIXME for some reason, only a hard file delete is clearing the cache
-      FileUtils.rm File.join(RAILS_ROOT, 'public', page.path)
+      unless page.path.nil?
+        expire_page(page.path)
+        # FIXME for some reason, only a file delete is clearing the cache
+        FileUtils.rm(File.join(RAILS_ROOT, 'public', page.path), :force => true)
+      end
     end
   end
 
