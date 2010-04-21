16eed33f0688d49d7b4f14456e962654edcfc313
Venu Keesari <keesari@unglobalcompact.org>
2010-04-21 18:07:01 -0400

    Fix: Logo request error when contact is deleted - should look into solution that prevents contacts from being deleted

--- app/helpers/admin/logo_requests_helper.rb
+++ app/helpers/admin/logo_requests_helper.rb
@@ -2,4 +2,11 @@ module Admin::LogoRequestsHelper
   def comment_date(logo_comment)
     logo_comment.added_on ? logo_comment.added_on : logo_comment.created_at
   end
+  
+  def contact_name(logo_request_object)
+    unless logo_request_object.try(:contact).try(:name)
+      content_tag :span, "Contact deleted", :style => 'color: red;'
+    end
+  end
+  
 end
