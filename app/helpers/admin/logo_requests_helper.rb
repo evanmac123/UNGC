a5ef6e094e0531d8b8c93ab6af4b0ab47344ce1b
Venu Keesari <keesari@unglobalcompact.org>
2010-04-21 18:33:04 -0400

    Fix: Logo request error when contact is deleted - helps to actually display the name

--- app/helpers/admin/logo_requests_helper.rb
+++ app/helpers/admin/logo_requests_helper.rb
@@ -4,7 +4,9 @@ module Admin::LogoRequestsHelper
   end
   
   def contact_name(logo_request_object)
-    unless logo_request_object.try(:contact).try(:name)
+    if logo_request_object.try(:contact).try(:name)
+      logo_request_object.contact.name
+    else
       content_tag :span, "Contact deleted", :style => 'color: red;'
     end
   end
