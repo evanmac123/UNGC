a64660fb28c2c025e0a687081216775aaaa52f78
Venu Keesari <keesari@unglobalcompact.org>
2010-04-23 11:13:29 -0400

    Participant Search updates - fix number of results dropdown list to keep previous selection - generate sentence to summarize results based on search criteria - minor changes to search form and results page

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -30,24 +30,40 @@ module ParticipantsHelper
   
   def searched_for
     returning('') do |response|
-      response << " for: '#{@searched_for[:keyword]}'" unless @searched_for[:keyword].blank?
-      response << " from #{countries_list}" unless @searched_for[:country_id].blank?
+      
+      response << ' participants' if params[:business_type] == 'all'
+      
       if @searched_for[:joined_after] && @searched_for[:joined_after] > Time.parse("2000-01-01")
         response << " who joined after #{@searched_for[:joined_after]}"
       end
+      
       if @searched_for[:business] == OrganizationType::BUSINESS
-        response << " matching businesses " 
+        response << ' business participants'
+
+        response << " from #{countries_list}" unless @searched_for[:country_id].blank?
+        
+        if @searched_for[:sector_id].blank?
+          response << ', in all sectors'
+        else
+          response << " in the #{Sector.find(@searched_for[:sector_id]).name} sector"
+        end
 
         case @searched_for[:cop_state]
-        when Organization::COP_STATES[:active].to_crc32
-          response << " with active status "
-        when Organization::COP_STATES[:noncommunicating].to_crc32
-          response << ' with non-communicating status '
+          when Organization::COP_STATES[:active].to_crc32
+            response << ', with an Active COP status'
+          when Organization::COP_STATES[:noncommunicating].to_crc32
+            response << ', with a Non-communicating COP status'
         end
+        
       elsif @searched_for[:business] == OrganizationType::NON_BUSINESS
         organization_type = OrganizationType.find_by_id(@searched_for[:organization_type_id])
-        response << " matching '#{organization_type.try(:name) || ''}' non-businesses "
+        response << " #{organization_type.try(:name) || ''} non-business participants"
+        response << " from #{countries_list}" unless @searched_for[:country_id].blank?
       end
+
+      response << " matching '#{@searched_for[:keyword]}'" unless @searched_for[:keyword].blank?
+      
     end
   end
+  
 end
