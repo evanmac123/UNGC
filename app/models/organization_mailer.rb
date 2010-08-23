f248a4e917f3b10e633f9fd5b92f613a4092e7dc
Venu Keesari <keesari@unglobalcompact.org>
2010-08-23 12:15:09 -0400

    Change cc address in organization mailer

--- app/models/organization_mailer.rb
+++ app/models/organization_mailer.rb
@@ -84,7 +84,7 @@ class OrganizationMailer < ActionMailer::Base
   
   def foundation_invoice(organization)
     from 'foundation@unglobalcompact.org'
-    cc 'radulescu@globalcompactfoundation.org'
+    cc 'contributions@globalcompactfoundation.org'
     bcc 'vkeesari@yahoo.com'
     subject "Your pledge to The Foundation for the Global Compact"
     content_type "text/html"
