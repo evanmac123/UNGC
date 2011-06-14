6b35af5a60bc6eda54c6a99dbf1a8a0dbcaa3a93
Venu Keesari <keesari@unglobalcompact.org>
2011-06-14 17:53:31 -0400

    Update cruise control config email

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,7 +1,7 @@
 # Project-specific configuration for CruiseControl.rb
 Project.configure do |project|
   # Send email notifications about broken and fixed builds to email1@your.site, email2@your.site (default: send to nobody)
-  project.email_notifier.emails = ['joshua@unspace.ca', 'wes@unspace.ca', 'cloves@unspace.ca']
+  project.email_notifier.emails = ['keesari@unglobalcompact.org']
   # Set email 'from' field to john@doe.com:
-  project.email_notifier.from = 'cruisecontrol@unspace.ca'
+  project.email_notifier.from = 'info@unglobalcompact.org'
 end
