f274902d52dbf15b48e44b838a01e6e984e6cf91
Venu Keesari <keesari@unglobalcompact.org>
2011-08-05 13:33:47 -0400

    Annotate model

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: awards
+#
+#  id               :integer(4)      not null, primary key
+#  local_network_id :integer(4)
+#  title            :string(255)
+#  description      :text
+#  type             :string(255)
+#  date             :date
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class Award < ActiveRecord::Base
   belongs_to :local_network
   has_one :attachment, :class_name => 'UploadedFile', :as => :attachable
