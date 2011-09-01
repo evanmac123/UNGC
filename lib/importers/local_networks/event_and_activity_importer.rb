179a133b33a72827609133a6890ff6aa7841e2e0
Aanand Prasad <aanand.prasad@gmail.com>
2011-09-01 17:10:16 -0400

    Import local network event principles

--- lib/importers/local_networks/event_and_activity_importer.rb
+++ lib/importers/local_networks/event_and_activity_importer.rb
@@ -19,6 +19,38 @@ module Importers
         model.num_participants          = get_integer(row, "Total Number Of Participants")
         model.gc_participant_percentage = get_integer(row, "Out Of Which, Approximate % Were GC Participants")
 
+        principle_column_map = {
+          "Human Rights"                => "Human Rights",
+          "Labour Rights"               => "Labour",
+          "Environment"                 => "Environment",
+          "Anti-corruption"             => "Anti-Corruption",
+          "Partnership for Development" => "Business for Development",
+          "Other"                       => "Unknown"
+        }
+
+        principle_presence_map = {}
+
+        principle_column_map.each_pair do |col_principle_name, db_principle_name|
+          principle_presence_map[db_principle_name] = get_1(row, "Issue Area Cover(#{col_principle_name})")
+        end
+
+        if get_1(row, "Issue Area Cover(General (Addressing all Global Compact Issue Areas))")
+          Principle.find_all_by_type("PrincipleArea").each do |principle_area|
+            principle_presence_map[principle_area.name] = true
+          end
+        end
+
+        principle_presence_map.each_pair do |principle_name, should_be_present|
+          principle = Principle.find_by_name(principle_name)
+          present   = model.principles.include?(principle)
+
+          if present and !should_be_present
+            model.principles.delete(principle)
+          elsif !present and should_be_present
+            model.principles << principle
+          end
+        end
+
         model.stakeholder_company              = get_1(row, "Stakeholder Group(Business)")
         model.stakeholder_sme                  = get_1(row, "Stakeholder Group(SMEs)")
         model.stakeholder_business_association = get_1(row, "Stakeholder Group(Business Associations)")
