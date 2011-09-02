4e220b4bdb8d32318acdc184a2ba716576526388
Venu Keesari <keesari@unglobalcompact.org>
2011-09-02 14:00:56 -0400

    Annotate models

--- app/models/integrity_measure.rb
+++ app/models/integrity_measure.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: integrity_measures
+#
+#  id               :integer(4)      not null, primary key
+#  local_network_id :integer(4)
+#  title            :string(255)
+#  policy_type      :string(255)
+#  description      :text
+#  date             :date
+#
+
 class IntegrityMeasure < ActiveRecord::Base
   TYPES = { :cop      => 'Communication on Progress',
             :logo     => 'Network Logo Policy',
--- app/models/local_network_event.rb
+++ app/models/local_network_event.rb
@@ -1,3 +1,30 @@
+# == Schema Information
+#
+# Table name: local_network_events
+#
+#  id                               :integer(4)      not null, primary key
+#  local_network_id                 :integer(4)
+#  title                            :string(255)
+#  description                      :text
+#  date                             :date
+#  event_type                       :string(255)
+#  num_participants                 :integer(4)
+#  gc_participant_percentage        :integer(4)
+#  stakeholder_company              :boolean(1)
+#  stakeholder_sme                  :boolean(1)
+#  stakeholder_business_association :boolean(1)
+#  stakeholder_labour               :boolean(1)
+#  stakeholder_un_agency            :boolean(1)
+#  stakeholder_ngo                  :boolean(1)
+#  stakeholder_foundation           :boolean(1)
+#  stakeholder_academic             :boolean(1)
+#  stakeholder_government           :boolean(1)
+#  stakeholder_media                :boolean(1)
+#  stakeholder_others               :boolean(1)
+#  created_at                       :datetime
+#  updated_at                       :datetime
+#
+
 class LocalNetworkEvent < ActiveRecord::Base
   belongs_to :local_network
   has_and_belongs_to_many :principles
