1556e35ba6c0e56d8b2aa1eb9069a0a3bcd85f8f
Venu Keesari <keesari@unglobalcompact.org>
2011-09-06 10:46:39 -0400

    Change order of types and add model error messages

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -11,12 +11,24 @@
 #
 
 class Meeting < ActiveRecord::Base
-  TYPES = ["Steering Committee", "General", "Governance"]
+  TYPES = ["General", "Governance", "Steering Committee"]
 
   include HasFile
   belongs_to :local_network
 
   validates_inclusion_of :meeting_type, :in => TYPES, :allow_nil => false
   validates_presence_of :date
+  
+  def readable_error_messages
+    error_messages = []
+    errors.each do |error|
+      case error
+        when 'file'
+          error_messages << 'Choose a file to upload'
+       end
+    end
+    error_messages
+  end
+  
 end
 
