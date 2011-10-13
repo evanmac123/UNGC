fcbd31454ed706e48438f2882fdeae406835c751
Venu Keesari <keesari@unglobalcompact.org>
2011-10-13 18:19:33 -0400

    Allow Staff to assign Mailing List type

--- app/models/organization_type.rb
+++ app/models/organization_type.rb
@@ -20,7 +20,7 @@ class OrganizationType < ActiveRecord::Base
   named_scope :non_business, :conditions => ['type_property=?', NON_BUSINESS]
   named_scope :business, :conditions => ['type_property=?', BUSINESS]
   named_scope :participants, :conditions => ['type_property in (?)', PARTICIPANT]
-  named_scope :staff_types, :conditions => ["type_property in (?) or name = 'Micro Enterprise'", PARTICIPANT]
+  named_scope :staff_types, :conditions => ["type_property in (?) or name IN ('Micro Enterprise','Mailing List')", PARTICIPANT]
   
   FILTERS = {
     :academia         => 'Academic',
