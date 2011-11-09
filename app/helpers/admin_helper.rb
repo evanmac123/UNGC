7ff613e8ea268c2977cb2d0f7946a218e86612d8
Venu Keesari <keesari@unglobalcompact.org>
2011-11-09 11:06:30 -0500

    Create option hash outside of conditional

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -34,6 +34,7 @@ module AdminHelper
   end
   
   def link_to_attached_file(object, file='attachment')
+    options = {}
     # clean this up
     return 'Not available' if object.send("#{file}_file_name").blank?
     if object.send("#{file}_file_name").downcase.ends_with?('.pdf')
