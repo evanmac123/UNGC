45f2d3fc71b24aa490419b0871b22f04b4c51d2d
Venu Keesari <keesari@unglobalcompact.org>
2011-11-13 17:25:25 -0500

    Improve  feedback for logins that could not be assigned

--- lib/assign_network_logins.rb
+++ lib/assign_network_logins.rb
@@ -1,24 +1,32 @@
-# ./script/runner 'AssignNetworkLogins.new.run' -e production
+# ./script/runner 'AssignNetworkLogins.new.run' /path_to/file -e production
 require 'csv'
 
 class AssignNetworkLogins
   def run
 
-  abort "Usage: ./script/runner 'AssignNetworkLogins.new.run' '/path_to/file' -e production" unless ARGV.any?
+  abort "Usage: ./script/runner 'AssignNetworkLogins.new.run' /path_to/file -e production" unless ARGV.any?
   file = ARGV.first
   
   if FileTest.exists?(file)
 
+   puts ['Email', 'Username', 'Password', 'Name', 'Local Network / Organization', 'Status'].join("\t")
+
     CSV.foreach(file, :headers => :first_row) do |row|
       c = Contact.find_all_by_email(row["email"])
-      if c.count > 1
-        puts "Duplicate: #{row["email"]}"        
-      elsif c.count == 1
-        contact = c.first
-        puts "Updated:     #{contact.name}\t\t\t#{'gcln' + contact.id.to_s}" if contact.update_attributes( :login => row["username"], :password => 'gcln' + contact.id.to_s )
-      else
-        puts "No Match:  #{row["email"]}"
+      unless c.any?
+        puts "#{row["email"]}\t#{row["username"]}\t\t#{row["name"]}\t\tEmail not found"
       end
+      
+      c.each do |contact|        
+        if contact.from_network? && !contact.from_organization?
+          if contact.update_attributes( :login => row["username"], :password => 'gcln' + contact.id.to_s )
+            puts "#{contact.email}\t#{contact.login}\t#{contact.password}\t#{row["name"]}\t#{contact.organization_name}\tSuccess"
+          end
+        else
+          puts "#{contact.email}\t#{row["username"]}\t\t#{row["name"]}\t#{contact.organization_name}\tNot a Local Network"
+        end
+      end
+
     end
 
   else
