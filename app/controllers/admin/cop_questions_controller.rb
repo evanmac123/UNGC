d6326bf87feeaf6749b19a0c12c612be35cce8ea
Jamie Gilgen <jamie@gilgen.ca>
2012-01-03 11:50:47 -0500

    Store the year filter in the session so that redirects are convenient

--- app/controllers/admin/cop_questions_controller.rb
+++ app/controllers/admin/cop_questions_controller.rb
@@ -5,31 +5,33 @@ class Admin::CopQuestionsController < AdminController
   def index
     conditions = {}
     conditions[:year] = params[:year] if params[:year].present?
+    conditions[:year] = nil if conditions[:year] == 'no_year'
+    session[:year_filter] = params[:year]
     @cop_questions = CopQuestion.all(:order => 'position, grouping, year', :conditions => conditions)
   end
 
   def new
     @cop_question = CopQuestion.new
-    @cop_question.year = params[:year]
-  end
-
-  def edit
+    @cop_question.year = session[:year_filter]
   end
 
   def create
     @cop_question = CopQuestion.new(params[:cop_question])
     if @cop_question.save
       flash[:notice] = 'COP Question was successfully created.'
-      redirect_to admin_cop_questions_path(:year => @cop_question.year)
+      redirect_to admin_cop_questions_path(:year => session[:year_filter])
     else
       render :action => "new"
     end
   end
 
+  def edit
+  end
+
   def update
     if @cop_question.update_attributes(params[:cop_question])
       flash[:notice] = 'COP Question was successfully updated.'
-      redirect_to admin_cop_questions_path(:year => @cop_question.year)
+      redirect_to admin_cop_questions_path(:year => session[:year_filter])
     else
       render :action => "edit"
     end
@@ -37,7 +39,7 @@ class Admin::CopQuestionsController < AdminController
 
   def destroy
     @cop_question.destroy
-    redirect_to(admin_cop_questions_path)
+    redirect_to admin_cop_questions_path(:year => session[:year_filter])
   end
 
   private
