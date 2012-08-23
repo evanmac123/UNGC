8920898705f707eefe3a5aebe4e6ee73d30768c4
Venu Keesari <keesari@unglobalcompact.org>
2012-08-23 12:42:31 -0400

    Updates to Pledge and Logo Request reports - add separate columns for Financial Contact and Contact Point info - replace CSV with Dashboard table - add a new Users action icon

--- app/reports/foundation_pledge_report.rb
+++ app/reports/foundation_pledge_report.rb
@@ -1,15 +1,15 @@
 class FoundationPledgeReport < SimpleReport
-
-  @render_method = :render_xls
-
   def records
-    Organization.active.participants.companies_and_smes.joined_on(@options[:month].to_i, @options[:year].to_i)
+    Organization.active.participants.companies_and_smes.joined_on(@options[:month].to_i, @options[:year].to_i).all(:order => 'joined_on')
   end
 
   def headers
-    [ 'Contact',
-      'Email',
-      'Phone',
+    [ 'Financial Contact',
+      'Financial Contact Email',
+      'Financial Contact Phone',
+      'Contact Point',
+      'Contact Point Email',
+      'Contact Point Phone',
       'Company',
       'Country',
       'Number of Employees',
@@ -22,6 +22,9 @@ class FoundationPledgeReport < SimpleReport
     [ r.financial_contact_or_contact_point.try(:name),
       r.financial_contact_or_contact_point.try(:email),
       r.financial_contact_or_contact_point.try(:phone),
+      r.contacts.contact_points.first.try(:name),
+      r.contacts.contact_points.first.try(:email),
+      r.contacts.contact_points.first.try(:phone),
       r.try(:name),
       r.country.try(:name),
       r.employees,
