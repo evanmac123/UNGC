04ef367ca93794e74758d1bb60082a491dc3a559
Venu Keesari <keesari@unglobalcompact.org>
2012-12-19 19:09:00 -0500

    Hide delete button from contacts if organization has not been approved

--- app/helpers/admin/contacts_helper.rb
+++ app/helpers/admin/contacts_helper.rb
@@ -10,4 +10,15 @@ module Admin::ContactsHelper
   def login_fieldset_class(role)
     "role_for_login_fields" if Role.login_roles.include? role
   end
+
+  def current_user_can_delete(current_user, tabbed_contact)
+    return false if current_user == tabbed_contact
+
+    return true if current_user.from_ungc? || current_user.from_network?
+
+    if current_user.from_organization?
+      return current_user.organization.participant ? true : false
+    end
+  end
+
 end
