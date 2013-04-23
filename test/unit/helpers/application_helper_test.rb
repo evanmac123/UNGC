f8cc0b067f7b471295c39ea5bc0d3904b4297b4c
Venu Keesari <keesari@unglobalcompact.org>
2013-04-23 14:36:07 -0400

    Replace select_options_from_hash helper method with Hash#invert

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -2,16 +2,5 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-  context "given a hash of key/value" do
-    setup do
-      hash = {:one => "Number One"}
-      @select_tag = select 'model', 'attribute', select_options_from_hash(hash)
-    end
-
-    should "create select <option> based on reversed key/value" do
-      assert @select_tag.include?('<option value="one">Number One</option>')
-    end
-  end
-
 end
 
