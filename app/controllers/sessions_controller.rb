00ccaa32459b1883eb25cd73d91b7051c4e764c9
Julie Hache <elektrophile@gmail.com>
2013-05-23 11:04:01 -0400

    Redirect contact to edit screen if they haven't logged in in 6 months

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,31 @@ class SessionsController < Devise::SessionsController
   helper 'Admin'
   before_filter :redirect_user_to_dashboard, :only => :new
 
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+
+    if resource_name == 'contact'
+      if resource.from_rejected_organization?
+        logout_and_redirect_to_login(resource)
+      else
+        redirect_path = redirect_to_edit_or_dashboard(resource)
+        redirect_back_or_default redirect_path
+      end
+    else
+      respond_with resource, :location => after_sign_in_path_for(resource)
+    end
+  end
+
   protected
-  # Track failed login attempts
-  def note_failed_signin
-    flash[:error] = "Sorry, either your username or password was incorrect. Please click 'Forgot your username or password?' to retrieve your username and choose a new password."
-    logger.warn "Failed login for '#{params[:username]}' from #{request.remote_ip} at #{Time.now.utc}"
+
+  def redirect_to_edit_or_dashboard(resource)
+    if resource.needs_to_update_contact_info
+      edit_admin_organization_contact_path(resource.organization.id, resource, {:update => true})
+    else
+      dashboard_path
+    end
   end
 
   # Forward logged-in users to dashboard
