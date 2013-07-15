dfdcb0557f294029fff45eb4ae342f865ec2f6d8
Julie Hache <elektrophile@gmail.com>
2013-07-15 18:06:15 -0400

    Return the called block in with_format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,9 +65,9 @@ module ApplicationHelper
   def with_format(format, &block)
     old_formats = formats
     self.formats = [format]
-    block.call
+    content = block.call
     self.formats = old_formats
-    nil
+    content
   end
 
 end
