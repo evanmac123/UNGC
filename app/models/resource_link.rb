fe6b772dc3f5fd5361e2f8ab128076f76971795e
Mattia Gheda <ghedamat@gmail.com>
2013-08-06 14:26:41 -0400

    add types to ResourceLink

--- app/models/resource_link.rb
+++ app/models/resource_link.rb
@@ -1,4 +1,16 @@
 class ResourceLink < ActiveRecord::Base
   belongs_to :resource
   belongs_to :language
+
+  validates_presence_of :title, :link_type
+
+  TYPES = {
+    :web => 'Website',
+    :pdf => 'PDF',
+    :mp3 => 'MP3 audio file',
+    :ppt => 'PowerPoint presentation'
+  }
+
+  validates :link_type, :inclusion => { :in => TYPES.keys.to_s, :message => "%{value} is not a valid link_type value" }
+
 end
--- test/unit/resource_link_test.rb
+++ test/unit/resource_link_test.rb
@@ -3,4 +3,18 @@ require 'test_helper'
 class ResourceLinkTest < ActiveSupport::TestCase
   should belong_to :resource
   should belong_to :language
+
+  context "resource link creation" do
+
+    should "create a valid resource link" do
+      resource_link = ResourceLink.new title: "resource title 1", link_type: "pdf"
+      assert resource_link.valid?
+    end
+
+    should "not create an invalid resource link" do
+      resource_link = ResourceLink.new title: "resource title 1", link_type: ResourceLink::TYPES[:pdf]
+      assert !resource_link.valid?
+    end
+
+  end
 end
