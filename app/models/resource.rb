3da0d2ba46bab78082d823e8e8fbfc2ecc03715b
Mattia Gheda <ghedamat@gmail.com>
2013-08-06 13:33:06 -0400

    add ContentApproval module to Resource

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,7 @@
 class Resource < ActiveRecord::Base
+
+  include ContentApproval
+
   has_and_belongs_to_many :principles
   has_and_belongs_to_many :authors
   has_many :resource_links
--- test/unit/resource_test.rb
+++ test/unit/resource_test.rb
@@ -4,4 +4,21 @@ class ResourceTest < ActiveSupport::TestCase
   should have_and_belong_to_many :principles
   should have_and_belong_to_many :authors
   should have_many :resource_links
+
+  context "resource creation" do
+    setup do
+      @resource = Resource.create title: "resource title 1", description: "present", image_url: "http://www.google.it", year: "2001"
+    end
+
+    should "create a resource and set it to the pending state" do
+      assert_equal ContentApproval::STATES[:pending], @resource.approval
+    end
+
+    should "be approvable" do
+      @resource.approve!
+      assert_equal ContentApproval::STATES[:approved], @resource.approval
+    end
+
+  end
+
 end
