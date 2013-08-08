f61955fbd2ef9be13a2a6d7138d4b2098192be1b
Benjamin Moss <drteeth@gmail.com>
2013-08-08 16:21:11 -0400

    wip

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -1,7 +1,12 @@
 class Admin::ResourcesController < AdminController
 
+  before_filter :load_resource, except: [:index, :create]
+
   def index
-    @resources = Resource.order(order_from_params).paginate(:page     => params[:page], :per_page => Resource.per_page)
+    @resources = Resource
+      .order(order_from_params)
+      .paginate(page:params[:page],
+                per_page:Resource.per_page)
   end
 
   def show
@@ -14,17 +19,37 @@ class Admin::ResourcesController < AdminController
   end
 
   def create
+    @resource = Resource.new(params[:resource])
+    if @resource.save
+      redirect_to admin_resources_url, notice: 'Resource created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
+    if @resource.update_attributes(params[:resource])
+      redirect_to admin_resources_url, notice: 'Resource updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
   end
 
   private
-    def order_from_params
-      @order = [params[:sort_field] || 'updated_at', params[:sort_direction] || 'DESC'].join(' ')
+
+  def order_from_params
+    @order = [params[:sort_field] || 'updated_at', params[:sort_direction] || 'DESC'].join(' ')
+  end
+
+  def load_resource
+    if params.has_key?(:id)
+      @resource = Resource.find(params[:id])
+    else
+      @resource = Resource.new
     end
+  end
 
 end
--- test/functional/admin/resources_controller_test.rb
+++ test/functional/admin/resources_controller_test.rb
@@ -1,7 +1,75 @@
 require 'test_helper'
 
 class Admin::ResourcesControllerTest < ActionController::TestCase
-  test "the truth" do
-    assert false
+
+  context "When signed in as an admin" do
+
+    setup do
+      @admin = create_staff_user
+      sign_in @admin
+    end
+
+    should "get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:resources)
+    end
+
+    should "get show" do
+      resource = create_resource
+      get :show, id:resource
+      assert_response :success
+      assert_not_nil assigns(:resource)
+    end
+
+    should "get new" do
+      get :new
+      assert_response :success
+      assert_not_nil assigns(:resource)
+    end
+
+    should "get edit" do
+      resource = create_resource
+      get :edit, id:resource
+      assert_response :success
+      assert_not_nil assigns(:resource)
+    end
+
+    context "with valid attributes" do
+
+      should "create and redirect to the index" do
+        post :create, resource:valid_resource_attributes
+        assert_redirected_to action: :index
+        assert_equal 1, Resource.count
+      end
+
+      should "update and redirect to the index" do
+        resource = create_resource
+        params = valid_resource_attributes
+
+        put :update, id:resource, resource:params
+
+        assert_redirected_to action: :index
+        resource.reload
+        assert_equal params['title'], resource.title
+      end
+    end
+
+    context "with invalid attributes" do
+
+      should "render new action" do
+        post :create, resource:{}
+        assert_template :new
+        assert_equal 0, Resource.count
+      end
+
+      should "render edit action" do
+        resource = create_resource
+        put :update, id:resource, resource:{}
+        assert_template :edit
+      end
+    end
+
   end
+
 end
