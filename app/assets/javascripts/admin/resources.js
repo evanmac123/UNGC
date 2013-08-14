85d659fa5285808f810388d8914c7e63b0297033
Benjamin Moss <drteeth@gmail.com>
2013-08-14 10:54:33 -0400

    first pass at resource links.

--- app/assets/javascripts/admin/resources.js
+++ app/assets/javascripts/admin/resources.js
@@ -1,7 +1,11 @@
 $(function() {
+
+  // TODO check for a tell-tale element.
+
+  // authors
   var addAuthorButton = $('#add_author'),
       authorTable = $('.author_table')
-      template = $('#author_template').html()
+      template = $('#author_template').html();
 
   authorTable.on('click', function(ev) {
     var target = $(ev.target)
@@ -14,6 +18,24 @@ $(function() {
   addAuthorButton.on('click', function(ev) {
     ev.preventDefault();
     authorTable.append(template);
+  });
+
+  // links
+  var addLinkButton = $('#add_link'),
+      linkTable = $('.link_table')
+      template = $('#link_template').html();
+
+  linkTable.on('click', function(ev) {
+    var target = $(ev.target)
+    if(target.attr('class') === 'remove_link') {
+      ev.preventDefault();
+      target.parents('tr').remove();
+    }
+  });
+
+  addLinkButton.on('click', function(ev) {
+    ev.preventDefault();
+    linkTable.append(template);
   });
 
 })
