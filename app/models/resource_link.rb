209098ad1f6f4d66bef39b305d5822903f83c00e
Venu Keesari <keesari@unglobalcompact.org>
2013-08-17 13:47:11 -0700

    Add doc resource_link type and sort key order

--- app/models/resource_link.rb
+++ app/models/resource_link.rb
@@ -5,10 +5,11 @@ class ResourceLink < ActiveRecord::Base
   validates_presence_of :title, :link_type
 
   TYPES = {
-    :web => 'Website',
-    :pdf => 'PDF',
+    :doc => 'Word document',
     :mp3 => 'MP3 audio file',
-    :ppt => 'PowerPoint presentation'
+    :pdf => 'PDF',
+    :ppt => 'PowerPoint presentation',
+    :web => 'Website'
   }
 
   validates :link_type, :inclusion => { :in => TYPES.keys.to_s, :message => "%{value} is not a valid link_type value" }
