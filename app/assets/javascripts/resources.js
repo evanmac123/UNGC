bd5d9e2ad5a153a130a28116010aad9fd420ebc2
Mattia Gheda <ghedamat@gmail.com>
2013-08-26 12:16:52 -0400

    javascript toggler for topics tree

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   if ($('form.resources_search').length == 1) {
     $('#author').chosen();
   }
+
+  $('ol.resources-list .toggle').on('click', function(e){
+    var ol = $(this).siblings('ol')
+    ol.toggle();
+    $(this).removeClass('collapse','expand');
+    if(ol.is(':visible')){
+      $(this).addClass('collapse');
+    }else{
+      $(this).addClass('expand');
+    }
+  });
 });
