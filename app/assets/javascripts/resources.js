30a1f7a7432e5d4462e1588968945ccf58e7934a
Mattia Gheda <ghedamat@gmail.com>
2013-08-27 13:47:00 -0400

    open topics tree where topics are selected

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
+  // enable chosen for the search form
   if ($('form.resources_search').length == 1) {
     $('#author').chosen();
   }
 
-  $('ol.resources-list .toggle').on('click', function(e){
-    var ol = $(this).siblings('ol')
+  // deal with the topics tree
+  var hideResourceList = function(el){
+    var ol = $(el).siblings('ol')
     ol.toggle();
-    $(this).removeClass('collapse','expand');
+    $(el).removeClass('collapse','expand');
     if(ol.is(':visible')){
-      $(this).addClass('collapse');
+      $(el).addClass('collapse');
     }else{
-      $(this).addClass('expand');
+      $(el).addClass('expand');
     }
+  }
+
+  var showResourceList = function(el){
+    var ol = $(el).siblings('ol')
+    ol.show();
+    $(el).removeClass('expand');
+    $(el).addClass('collapse');
+  }
+
+
+  $('ol.resources-list input[checked]').each(function(i,e){
+    $(e).parents('ol.expanded-list').parent().children('span').each(function(j,o){
+      showResourceList(o);
+    })
   });
+
+  $('ol.resources-list .toggle').on('click', function(e){
+    hideResourceList(this);
+  });
+
 });
