506b218ff381cb51624364fefc1b2fd632fe67ee
Venu Keesari <keesari@unglobalcompact.org>
2013-09-17 02:24:19 -0400

    Group by language id

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,7 +1,7 @@
 module ResourcesHelper
 
   def languages_for_select(selected_language=nil)
-    languages = ResourceLink.includes(:language).joins(:resource).where("resources.approval='approved'").select(:language_id).map{ |c| [c.language.name, c.language_id] }.uniq
+    languages = ResourceLink.includes(:language).joins(:resource).where("resources.approval='approved'").group('languages.id').select(:language_id).map{ |c| [c.language.name, c.language_id] }
     options_for_select(languages, selected: selected_language)
   end
 
@@ -14,7 +14,7 @@ module ResourcesHelper
     selected = url_for(params.merge(:order => params[:order]))
     select_tag :per_page, options_for_select(options, :selected => selected), :class => 'autolink'
   end
-
+  
   def approval_name(resource)
     case resource.approval
     when 'pending'
@@ -35,5 +35,5 @@ module ResourcesHelper
   def link_to_all_resources(reference)
     link_to 'View All', resources_path(commit: 'search', resource_search: { topic: {principle_ids: [Principle.find_by_reference(reference)]}}), class: 'view-all'
   end
-
+  
 end
