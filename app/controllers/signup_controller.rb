9e7fb3ee78d20aa9832c0231724a1f9838a6b358
Mattia Gheda <ghedamat@gmail.com>
2013-10-15 10:45:19 -0400

    cleaning up

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,44 +1,27 @@
 class SignupController < ApplicationController
   before_filter :determine_navigation
-  #before_filter :load_objects
 
   BUSINESS_PARAM = 'business'
   NONBUSINESS_PARAM = 'non_business'
 
   # shows organization form
   def step1
-    clean_session
-    @organization.organization_type_id = @organization_types.first.id unless @organization.organization_type_id
-    # check if they were referred from the JCI website
-    if @organization.jci_referral? request.env["HTTP_REFERER"]
+    load_session
+    @os ||= OrganizationSignup.new(params[:org_type])
+    @organization_types = OrganizationType.send @os.org_type
+    session[:os] = nil
+
+    if @os.organization.jci_referral? request.env["HTTP_REFERER"]
       session[:is_jci_referral] = true
       @jci_referral = true
     end
   end
 
-  def step1
-    @os = session[:os] || OrganizationSignup.new(params[:org_type])
-    @organization_types = OrganizationType.send @os.org_type
-    session[:os] = nil
-  end
-
   # POST from organization form
   # shows contact form
   def step2
-    if params[:organization]
-      @organization.attributes = params[:organization]
-      session[:signup_organization] = @organization
-      set_default_values
-    end
-
-    unless @organization.valid?
-      redirect_to organization_step1_path(:org_type => extract_organization_type(@organization))
-    end
-
-  end
-
-  def step2
-    @os = session[:os] || OrganizationSignup.new(params[:org_type])
+    load_session
+    @os ||= OrganizationSignup.new(params[:org_type])
 
     @os.step2(params[:organization])
 
@@ -52,27 +35,13 @@ class SignupController < ApplicationController
   # POST from contact form
   # shows ceo form
   def step3
-    if params[:contact]
-      @contact.attributes = params[:contact]
-      session[:signup_contact] = @contact
-      set_default_values
-    end
-    # business make a financial contribution (step 4), non-business upload their letter of commitment (step 6)
-    @organization.attributes = params[:organization]
-    @next_step = @organization.business_entity? ? organization_step4_path : organization_step6_path
-
-    redirect_to organization_step2_path unless @contact.valid?
-  end
-
-  def step3
-    @os = session[:os]
+    load_session
 
     @os.step3(params[:contact])
 
     session[:os] = @os
 
     @next_step = @os.business? ? organization_step4_path : organization_step6_path
-
 
     unless @os.primary_contact.valid?
       redirect_to organization_step2_path
@@ -82,18 +51,7 @@ class SignupController < ApplicationController
   # POST from ceo form
   # pledge form if business organization
   def step4
-    save_ceo_info_from_params_into_session
-    redirect_to organization_step3_path unless @ceo.valid? and unique_emails?
-    # highlight amount by assigning CSS class
-    @suggested_pledge_amount = {}
-    @suggested_pledge_amount[@organization.revenue] = 'highlight_suggested_amount'
-    # preselect radio button
-    @checked_pledge_amount = {}
-    @checked_pledge_amount[@organization.revenue] = true
-  end
-
-  def step4
-    @os = session[:os]
+    load_session
 
     @os.step4(params[:contact])
 
@@ -114,19 +72,7 @@ class SignupController < ApplicationController
   # POST from pledge form
   # ask for financial contact if pledge was made
   def step5
-    # Financial Contact fields not included in the form, but which default to the Contact Point's
-    @financial_contact.address = @contact.address
-    @financial_contact.address_more = @contact.address_more
-    @financial_contact.city = @contact.city
-    @financial_contact.state = @contact.state
-    @financial_contact.postal_code = @contact.postal_code
-    @financial_contact.country_id = @contact.country_id
-    @organization.attributes = params[:organization]
-    redirect_to organization_step6_path unless @organization.pledge_amount.to_i > 0
-  end
-
-  def step5
-    @os = session[:os]
+    load_session
 
     @os.step5(params[:organization])
 
@@ -140,20 +86,10 @@ class SignupController < ApplicationController
   # POST from ceo or financial contact form
   # shows commitment letter form
   def step6
-    @organization.attributes = params[:organization]
-    if @organization.pledge_amount.to_i > 0
-      create_financial_contact_or_add_role_to_existing_contact
-      redirect_to organization_step5_path unless @financial_contact.valid? || @contact.is?(Role.financial_contact)
-    else
-      save_ceo_info_from_params_into_session
-    end
-    redirect_to organization_step3_path unless @ceo.valid? and unique_emails?
-  end
-
-  def step6
-    @os = session[:os]
+    load_session
 
     # coming from 3 or 5 but not 7
+    # XXX fix this
     if params[:contact]
       @os.step6(params[:contact])
     end
@@ -161,12 +97,12 @@ class SignupController < ApplicationController
     session[:os] = @os
 
     # coming from step5
+    # XXX could this bi @os.business??
     if @os.organization.pledge_amount.to_i > 0
       unless @os.financial_contact.valid? || @os.primary_contact.is?(Role.financial_contact)
         redirect_to organization_step5_path
       end
     end
-
 
     # coming from step3
     unless @os.ceo.valid? and @os.unique_emails?
@@ -210,7 +146,7 @@ class SignupController < ApplicationController
 
 
   def step7
-    @os = session[:os]
+    load_session
 
     @os.step7(params[:organization])
 
@@ -226,86 +162,13 @@ class SignupController < ApplicationController
     end
   end
 
-  def no_session
-    clean_session
-    redirect_to organization_step1_path(:org_type => 'business')
-  end
-
   private
+
+    def load_session
+      @os = session[:os]
+    end
     def default_navigation
       DEFAULTS[:signup_form_path]
-    end
-
-    def load_objects
-      @organization = session[:signup_organization] || Organization.new
-      load_organization_types
-      @contact = session[:signup_contact] || new_contact(Role.contact_point)
-      @ceo = session[:signup_ceo] || new_contact(Role.ceo)
-      @financial_contact = session[:financial_contact] || new_contact(Role.financial_contact)
-    end
-
-    def set_default_values
-      # organization country is default for contacts
-      @contact.country_id = @organization.country_id unless @contact.country
-
-      # ceo contact fields which default to contact
-      @ceo.phone = @contact.phone unless @ceo.phone
-      @ceo.fax = @contact.fax unless @ceo.fax
-      @ceo.address = @contact.address unless @ceo.address
-      @ceo.address_more = @contact.address_more unless @ceo.address_more
-      @ceo.city = @contact.city unless @ceo.city
-      @ceo.state = @contact.state unless @ceo.state
-      @ceo.postal_code = @contact.postal_code unless @ceo.postal_code
-      @ceo.country_id = @contact.country_id unless @ceo.country
-    end
-
-    # Makes sure the CEO and Contact point don't have the same email address
-    def unique_emails?
-      unique = (@ceo.email.try(:downcase) != @contact.email.try(:downcase))
-      @ceo.errors.add :email, "cannot be the same as the Contact Point" unless unique
-      return unique
-    end
-
-    def load_organization_types
-      method = ['business', 'non_business'].include?(params[:org_type]) ? params[:org_type] : 'business'
-      @organization_types = OrganizationType.send method
-    end
-
-    def clean_session
-      session[:signup_organization] = Organization.new
-      session[:signup_contact] = new_contact(Role.contact_point)
-      session[:signup_ceo] = new_contact(Role.ceo)
-      session[:financial_contact] = new_contact(Role.financial_contact)
-      session[:is_jci_referral] = nil
-    end
-
-    def new_contact(role)
-      contact = Contact.new
-      contact.role_ids = [role.id] if role
-      contact
-    end
-
-    def extract_organization_type(organization)
-      organization.business_entity? ? BUSINESS_PARAM : NONBUSINESS_PARAM
-    end
-
-    def save_ceo_info_from_params_into_session
-      if params[:contact]
-        @ceo.attributes = params[:contact]
-        session[:signup_ceo] = @ceo
-      end
-    end
-
-    def create_financial_contact_or_add_role_to_existing_contact
-      # value from checkbox to indicate that invoice should be sent to Contact Points
-      if params[:contact]
-        if params[:contact][:foundation_contact].to_i == 1
-          @contact.roles << Role.financial_contact
-        else
-          @financial_contact.attributes = params[:contact]
-          session[:financial_contact] = @financial_contact
-        end
-      end
     end
 
 end
