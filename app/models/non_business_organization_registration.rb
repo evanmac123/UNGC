c6090b4a85bfc71878dc3cdaeabda69f19a885ed
Mattia Gheda <ghedamat@gmail.com>
2013-10-17 12:46:25 -0400

    validates length of mission_statement

--- app/models/non_business_organization_registration.rb
+++ app/models/non_business_organization_registration.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: non_business_organization_registrations
+#
+#  id                :integer          not null, primary key
+#  date              :date
+#  place             :string(255)
+#  authority         :string(255)
+#  number            :string(255)
+#  mission_statement :text
+#  organization_id   :integer
+#
+
 class NonBusinessOrganizationRegistration < ActiveRecord::Base
+  validates :mission_statement, length: { maximum: 1000 }
 
   belongs_to :organization
 end
--- test/unit/non_business_organization_registration_test.rb
+++ test/unit/non_business_organization_registration_test.rb
@@ -1,5 +1,27 @@
+# == Schema Information
+#
+# Table name: non_business_organization_registrations
+#
+#  id                :integer          not null, primary key
+#  date              :date
+#  place             :string(255)
+#  authority         :string(255)
+#  number            :string(255)
+#  mission_statement :text
+#  organization_id   :integer
+#
+
 require 'test_helper'
 
 class NonBusinessOrganizationRegistrationTest < ActiveSupport::TestCase
   should belong_to :organization
+
+  should "limit length of mission statement" do
+    statement = "A" * 1001
+    reg = NonBusinessOrganizationRegistration.new(mission_statement: statement)
+    assert !reg.valid?
+    reg = NonBusinessOrganizationRegistration.new(mission_statement: "hi")
+    reg.valid?
+    assert reg.valid?
+  end
 end
