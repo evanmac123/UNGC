11982a4c9ccbc24a1852c98ad2ee4516dc5d9670
Mattia Gheda <ghedamat@gmail.com>
2013-12-03 01:49:16 -0500

    toggle organization.registration fields

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
   $("#organization_organization_type_id").change(function() {
-    org_type = $("#organization_organization_type_id option:selected").text();
-    if (org_type == "Company" || org_type == "SME") {
+    var org_type = $("#organization_organization_type_id option:selected").text();
+    if (org_type === "Company" || org_type === "SME") {
       $('.company_only').show('slow');
+      $('.organization_registration').fadeOut('slow');
     } else {
       $('.company_only').hide('slow');
       $('.public_company_only').hide('slow');
+      $('.organization_registration').fadeIn('slow');
     }
-  })
+  });
 
   $("#organization_listing_status_id").change(function() {
-    selected_listing_status = jQuery.trim($("#organization_listing_status_id option:selected").text());
+    var selected_listing_status = jQuery.trim($("#organization_listing_status_id option:selected").text());
     if (selected_listing_status == "Public Company") {
       $('.public_company_only').show();
     } else {
       $('.public_company_only').hide();
     }
-  })
+  });
 
   // used to set delisting date/reason when Active is unchecked
   $("#organization_active").change(function() {
@@ -28,7 +30,7 @@ $(document).ready(function() {
     else {
       $("#delisted_only").toggle();
     }
-  })
+  });
 
   // used when 'No Dialogue' is set
   $("#organization_non_comm_dialogue").change(function() {
@@ -39,7 +41,7 @@ $(document).ready(function() {
       $("select[id^=organization_non_comm_dialogue_on_]").val($("select[id^=organization_non_comm_dialogue_on_] option:first").val());
       $("#non_comm_dialogue_only").toggle();
     }
-  })
+  });
 
   // contact form
   $('.role_for_login_fields').change(function() {
@@ -48,18 +50,19 @@ $(document).ready(function() {
     } else {
       $('#login_information').hide();
     }
-  })
+  });
 
   // search
   $("#admin_search").focus(function() {
     $('#admin_search').attr('value', '');
-  })
+  });
+
   $("#admin_search").blur(function() {
     $('#admin_search').attr('value', 'Search organizations');
-  })
+  });
 });
 
-replace_ids = function(s){
+var replace_ids = function(s){
   var new_id = new Date().getTime();
   return s.replace(/NEW_RECORD/g, new_id);
-}
+};
