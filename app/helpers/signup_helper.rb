689fa821582e2e00828fc32601805dc5487af1e3
Mattia Gheda <ghedamat@gmail.com>
2014-01-02 14:00:21 -0500

    add a little helper to make the pledge form button clearer

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -4,4 +4,7 @@ module SignupHelper
     @signup.business? ? "Business" : "Non-Business"
   end
 
+  def pledge_radio_button(form, amount)
+    form.radio_button :pledge_amount, amount, { checked: (form.object.pledge_amount == amount), class: 'fixed_pledge' }
+  end
 end
