f156769b1d647107a86747c3013b723894612250
Venu Keesari <keesari@unglobalcompact.org>
2014-01-01 19:42:55 -0500

    Add funding model to Local Network listings and report

--- db/example_data.rb
+++ db/example_data.rb
@@ -5,7 +5,7 @@ module FixtureReplacement
 
   attributes_for :case_story do |c|
     c.title = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :comment do |c|
     c.body = FixtureReplacement.random_string
@@ -30,7 +30,7 @@ module FixtureReplacement
     c.username = FixtureReplacement.random_string
     c.plaintext_password = FixtureReplacement.random_string
     c.password = c.plaintext_password
-	end
+  end
 
   attributes_for :cop_question do |q|
     q.text = FixtureReplacement.random_string
@@ -40,13 +40,13 @@ module FixtureReplacement
   end
 
   attributes_for :cop_score do |a|
-	end
+  end
 
   attributes_for :country do |c|
     c.name = FixtureReplacement.random_string
     c.region = 'Americas'
     c.code = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :event do |e|
     e.title = FixtureReplacement.random_string
@@ -59,7 +59,7 @@ module FixtureReplacement
   end
 
   attributes_for :exchange do |a|
-	end
+  end
 
   attributes_for :headline do |a|
     a.title = FixtureReplacement.random_string
@@ -70,47 +70,48 @@ module FixtureReplacement
   end
 
   attributes_for :interest do |a|
-	end
+  end
 
   attributes_for :language do |l|
     l.name = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :listing_status do |l|
     l.name = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :local_network do |l|
     l.name = FixtureReplacement.random_string
+    l.funding_model = LocalNetwork::FUNDING_MODELS.keys.shuffle.first.to_s
   end
 
   attributes_for :logo_approval do |a|
   end
 
   attributes_for :logo_comment do |a|
-	end
+  end
 
   attributes_for :logo_file do |f|
     f.name = FixtureReplacement.random_string
     f.thumbnail = FixtureReplacement.random_string
     f.file = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :logo_publication do |p|
     p.name = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :logo_request do |r|
     r.purpose = FixtureReplacement.random_string
     r.organization_id = Organization.first.id
     r.contact_id = Contact.first.id
     r.publication_id = LogoPublication.first.id
-	end
+  end
 
   attributes_for :organization_type do |o|
     o.name = FixtureReplacement.random_string
     o.type_property = OrganizationType::BUSINESS
-	end
+  end
 
   attributes_for :organization do |o|
     o.name = FixtureReplacement.random_string
@@ -119,7 +120,7 @@ module FixtureReplacement
     o.url = 'http://www.example.com'
     o.pledge_amount = 1000
     o.joined_on = Date.new(2009,10,10)
-	end
+  end
 
   attributes_for :page do |a|
     a.path      = random_url
@@ -138,23 +139,23 @@ module FixtureReplacement
 
   attributes_for :principle do |a|
     a.name = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :principle_area do |a|
     a.name = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :removal_reason do |a|
-	end
+  end
 
   attributes_for :role do |r|
     r.name = FixtureReplacement.random_string
     r.description = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :sector do |s|
     s.name = FixtureReplacement.random_string
-	end
+  end
 
   attributes_for :signing do |s|
     s.added_on = Date.today
