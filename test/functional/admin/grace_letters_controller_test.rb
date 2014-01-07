876849ab79d54e8247b7ef9867c851720e38650d
Mattia Gheda <ghedamat@gmail.com>
2014-01-07 15:26:51 -0500

    WIP, admin/grace letter test

--- test/functional/admin/grace_letters_controller_test.rb
+++ test/functional/admin/grace_letters_controller_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class Admin::GraceLettersControllerTest < ActionController::TestCase
+  context "given a Grace Letter" do
+    setup do
+      create_approved_organization_and_user
+      create_cop_with_options(:type => 'grace')
+      sign_in @organization_user
+      get :show, :organization_id => @organization.id,
+                 :id              => @cop.id
+    end
 
-  test "should get show" do
-    get :show
-    assert_response :success
+    should "view with the Grace Letter partial" do
+      assert_template :partial => '_show_grace_style'
+    end
   end
 
 end
