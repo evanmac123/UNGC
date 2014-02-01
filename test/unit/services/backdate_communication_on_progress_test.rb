dd85b76e7b71466ac57e8c280eff403108eeda75
Benjamin Moss <drteeth@gmail.com>
2014-02-01 01:12:33 -0500

    Add service object for backdating communication on progresses.

---
+++ test/unit/services/backdate_communication_on_progress_test.rb
@@ -0,0 +1,80 @@
+require 'test_helper'
+
+class BackdateCommunicationOnProgressTest < ActiveSupport::TestCase
+
+  # TODO check nonbusiness/COEs
+  context "Given a non communicating business" do
+
+    setup do
+      @due_date = Date.new(2012, 1, 1)
+      @on_time = @due_date - 1.month
+      @late = @due_date  + 1.month
+
+      create_organization_and_user
+      @organization.update_attribute(:cop_due_on, @due_date)
+      @organization.communication_late!
+      @organization.save!
+    end
+
+    context "with a late communication on progress" do
+
+      setup do
+        @cop = create_communication_on_progress starts_on: @late, organization_id: @organization.id
+      end
+
+      context "when it is backdated" do
+
+        setup do
+          assert BackdateCommunicationOnProgress.backdate(@cop, @on_time)
+          @organization.reload
+        end
+
+        should "now be an active organization" do
+          assert_equal 'active', @organization.cop_state
+        end
+
+        should "have it's next cop due a year from the back date" do
+          assert_equal @on_time + 1.year, @organization.cop_due_on
+        end
+
+        should "have a published on date set to the back date" do
+          assert_equal @on_time, @cop.published_on
+        end
+
+      end
+
+    end
+
+    context "with 2 late communications" do
+
+      setup do
+        @earlier = create_communication_on_progress title: 'earlier', starts_on: @late, organization_id: @organization.id
+        @later = create_communication_on_progress title: 'later', starts_on: @late + 1.year, organization_id: @organization.id
+      end
+
+      context "when the earlier communication is backdated" do
+
+        setup do
+          assert BackdateCommunicationOnProgress.backdate(@earlier, @on_time)
+          @organization.reload
+        end
+
+        should "still be a non communicating organization" do
+          assert_equal 'noncommunicating', @organization.cop_state
+        end
+
+        should "still have the same cop due date" do
+          assert_equal @due_date, @organization.cop_due_on
+        end
+
+        should "have a published on date set to the back date" do
+          assert_equal @on_time, @earlier.published_on
+        end
+
+      end
+
+    end
+
+  end
+
+end
