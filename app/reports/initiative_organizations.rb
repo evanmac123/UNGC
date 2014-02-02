6ed0c099c9462827c50ea485265fb4d0541a27aa
Venu Keesari <keesari@unglobalcompact.org>
2014-02-02 14:37:50 -0800

    Additional columns and join tables for report

--- app/reports/initiative_organizations.rb
+++ app/reports/initiative_organizations.rb
@@ -1,10 +1,11 @@
 class InitiativeOrganizations < SimpleReport
 
   def records
+    join_tables = [:organization_type, :sector, :country]
     if @options[:initiatives]
-      Signing.for_initiative_ids(@options[:initiatives])
+      Signing.for_initiative_ids(@options[:initiatives]).joins(:organization => [join_tables] )
     else
-      Signing.for_initiative_ids(Initiative.for_select.map(&:id))
+      Signing.for_initiative_ids(Initiative.for_select.map(&:id)).joins(:organization => [join_tables] )
     end
   end
   
@@ -18,19 +19,33 @@ class InitiativeOrganizations < SimpleReport
   
   def headers
     [
-    'Organization ID',
-    'Organization name',
-    'Initiative name',
-    'Signed initiatve on'
-  ]
+      'Organization ID',
+      'Organization name',
+      'Participant',
+      'Joined Global Compact on',
+      'Initiative name',
+      'Signed initiative on',
+      'Organization Type',
+      'Employees',
+      'FT500',
+      'Sector',
+      'Country'
+    ]
   end
 
   def row(record)
   [
     record.organization.id,
     record.organization.name,
+    record.organization.participant ? 1:0,
+    record.organization.joined_on,
     record.initiative.name,
-    record.added_on
+    record.added_on,
+    record.organization.organization_type_name,
+    record.organization.employees,
+    record.organization.is_ft_500 ? 1:0,
+    record.organization.sector_name,
+    record.organization.country_name
   ]
   end
 
