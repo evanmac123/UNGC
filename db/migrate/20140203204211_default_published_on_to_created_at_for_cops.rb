30110826b62e97750289f15fbe611fdaa9b574ed
Mattia Gheda <ghedamat@gmail.com>
2014-02-18 13:37:05 -0500

    Only approved COPs should be published and have their published_on date set
    see https://www.pivotaltracker.com/story/show/65924558

--- db/migrate/20140203204211_default_published_on_to_created_at_for_cops.rb
+++ db/migrate/20140203204211_default_published_on_to_created_at_for_cops.rb
@@ -1,13 +1,13 @@
 class DefaultPublishedOnToCreatedAtForCops < ActiveRecord::Migration
   def up
     CommunicationOnProgress.transaction do
-      connection.update('update communication_on_progresses set published_on = created_at')
+      connection.update("update communication_on_progresses set published_on = created_at where state = 'approved'")
     end
   end
 
   def down
     CommunicationOnProgress.transaction do
-      connection.update('update communication_on_progresses set published_on = null')
+      connection.update("update communication_on_progresses set published_on = null where state = 'approved'")
     end
   end
 end
