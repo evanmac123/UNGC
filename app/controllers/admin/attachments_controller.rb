f355d1e88b649878a159009ea35ebc9fd1f0fd26
Benjamin Moss <drteeth@gmail.com>
2014-02-26 23:47:19 -0500

    add language to cop_file manager.

--- app/controllers/admin/attachments_controller.rb
+++ app/controllers/admin/attachments_controller.rb
@@ -4,10 +4,6 @@ class Admin::AttachmentsController < AdminController
   helper_method :header_string, :cancel_string, :cancel_path,
     :attachments, :attachments_path, :new_attachment_path, :upload_attachment_path, :attachment_path
 
-  def index
-    render '/admin/attachments/index', :layout => 'admin'
-  end
-
   def create
     if params[:uploaded_attachments].is_a?(Array)
       @submodel.uploaded_attachments = params[:uploaded_attachments]
