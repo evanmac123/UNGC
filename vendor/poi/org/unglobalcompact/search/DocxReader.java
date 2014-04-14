03a5d0e958081df411942508d3d5da74f96fa92a
Benjamin Moss <drteeth@gmail.com>
2014-04-14 16:04:06 -0400

    Split DocxReader off from WordReader. Add new docx_import ruby script.

--- vendor/poi/org/unglobalcompact/search/DocxReader.java
+++ vendor/poi/org/unglobalcompact/search/DocxReader.java
@@ -1,21 +1,23 @@
 package org.unglobalcompact.search;
 
-import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class WordReader {
+public class DocxReader {
 
   public static void main(String[] args) throws Exception {
     if (args.length < 1) {
-      System.out.println("Usage: java org.unglobalcompact.search.WordReader <file>");
+      System.out.println("Usage: java org.unglobalcompact.search.WordXmlReader <file>");
       return;
     }
 
     FileInputStream fin = new FileInputStream(args[0]);
-    WordExtractor e = new WordExtractor(fin);
+    XWPFDocument doc = new XWPFDocument(fin);
+    XWPFWordExtractor e = new XWPFWordExtractor(doc);
     System.out.println(e.getText());
     fin.close();
   }
