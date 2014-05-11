86bde333409180c8ecc9cb32f8c3a20a229e3865
Mattia Gheda <ghedamat@gmail.com>
2014-05-10 20:09:02 -0400

    install screen on server via moonshine

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -135,6 +135,7 @@ class ApplicationManifest < Moonshine::Manifest::Rails
     # package 'some_native_package', :ensure => :installed
     package 'default-jre-headless', :ensure => :installed
     package 'python', :ensure => :installed
+    package 'screen', :ensure => :installed
 
     # some_rake_task = "/usr/bin/rake -f #{configuration[:deploy_to]}/current/Rakefile custom:task RAILS_ENV=#{ENV['RAILS_ENV']}"
     # cron 'custom:task', :command => some_rake_task, :user => configuration[:user], :minute => 0, :hour => 0
