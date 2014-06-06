f641dae01919ae4f2f84140dcb7e59ab570d7b12
Venu Keesari <keesari@unglobalcompact.org>
2014-06-06 09:06:13 -0700

    Updates to Tools and Resources views - Pluralize labels in public Resource view - Change Browse / Search button names - fix css spelling - make css class names consistent with Principle.reference - separate helper methods for Featured Resources   and issues pages for linking to individual resources - Updated index of Featured Resources - resource link text was too long, increase truncation

--- app/services/resource_featured.rb
+++ app/services/resource_featured.rb
@@ -1,46 +1,60 @@
 class ResourceFeatured
   include ResourcesHelper
 
-  def leaders_summit
-    find_resources [441,371,451,381,391]
+  def new_resources
+    find_resources [881,308,461,451,891]
   end
 
-  def leaders_summit2
-    find_resources [461,401,411,421,491]
+  def new_resources_2
+    find_resources [831,791,781,771,371]
   end
 
   def global_compact
-    find_resources [229,231,241,240,312]
+    find_resources [229,231,241,240,441]
   end
 
-  def human_rights
-  end
-
-  def labour
-  end
-
-  def summit
-  end
-
-  def environment
-  end
-
-  def anti_corruption
-  end
-
-  def development
+  def cop
+    find_resources [305,306,531,49,154]
   end
 
   def local_networks
+    find_resources [312,310,431,491,177]
+  end
+  
+  def human_rights
+    find_resources [2,22,9,341,3]
   end
 
-  def business_for_peace
+  def labour
+    find_resources [261,75,79,104,89]
+  end
+
+  def environment
+    find_resources [118,481,138,501,139]
+  end
+
+  def anti_corruption
+    find_resources [162,154,152,411,771]
+  end
+
+  def business_peace
+    find_resources [381,491,281,165,262]
+  end
+
+  def development
+    find_resources [179,320,173,391,174]
   end
 
   def financial_markets
+    find_resources [811,210,211,215,209]
   end
 
-  def communication_on_progress
+  def partnerships
+    find_resources [841,361,200,201,202]
+  end
+
+  def supply_chain
+    find_resources [207,205,421,791,204]
   end
 
 end
