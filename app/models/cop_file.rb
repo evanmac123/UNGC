b9e77639c7b4617f50140b54483cb83b48f2904a
Mattia Gheda <ghedamat@gmail.com>
2014-10-30 16:14:06 -0400

    mattia fails in string interpolation

--- app/models/cop_file.rb
+++ app/models/cop_file.rb
@@ -16,7 +16,7 @@
 Paperclip.interpolates :cop_file_id do |attachment, style|
     id = attachment.instance.id
     if id > 90000
-      "SLOT_1/" + id
+      "SLOT_1/#{id}"
     else
       id
     end
