02e31ecb9e86eaed5f6a77e8e3f44378cefdf0b7
Venu Keesari <keesari@unglobalcompact.org>
2015-01-19 19:54:28 -0500

    Remove duplicate delisting method

--- lib/cop_status_updater.rb
+++ lib/cop_status_updater.rb
@@ -9,7 +9,6 @@ class CopStatusUpdater
     def update_all
       move_active_organizations_to_noncommunicating
       move_noncommunicating_organizations_to_delisted
-      move_noncommunicating_companies_to_delisted
     end
 
     def move_active_organizations_to_noncommunicating
@@ -32,20 +31,5 @@ class CopStatusUpdater
       end
     end
 
-    def move_noncommunicating_companies_to_delisted
-      log "Running move_noncommunicating_companies_to_delisted"
-      organizations = Organization.companies.participants.active.about_to_become_delisted
-      organizations.each do |organization|
-        organization.delist
-        begin
-          CopMailer.delisting_today(organization).deliver
-          log "Delist and email #{organization.id}:#{organization.name}"
-        rescue
-          log "error", "Could not email #{organization.id}:#{organization.name}"
-        end
-      end
-    end
-
   end # class << self
-
 end # class CopStatusUpdater
