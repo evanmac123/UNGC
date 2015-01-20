517b29cd562aa81e937ed7b06908623597068e7e
Carsten Nielsen <heycarsten@gmail.com>
2015-01-20 12:23:39 -0500

    Password reset tokens are now encrypted.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,12 @@
 class ContactMailer < Devise::Mailer
-  default :from => UNGC::Application::EMAIL_SENDER
+  default from: UNGC::Application::EMAIL_SENDER
 
   # opts={} required by Devise
-  def reset_password_instructions(contact, token, opts={})
-    @contact = contact
-    mail(:to => contact.email_recipient, :subject => "United Nations Global Compact - Reset Password")
+  def reset_password_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts.merge(
+      to: record.email_recipient,
+      subject: 'United Nations Global Compact - Reset Password'
+    ))
   end
-
 end
