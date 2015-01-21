0093e38544a006ee4321cca1a63766871182b4b9
Carsten Nielsen <heycarsten@gmail.com>
2015-01-21 16:05:17 -0500

    Use newer style moonshine configuration.
    Plugin README referenced old way of configuring itself.

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -36,13 +36,8 @@ class ApplicationManifest < Moonshine::Manifest::Rails
   recipe :sysctl
   recipe :redis
   recipe :sidekiq
-
   recipe :pdfminer
-
   recipe :xsendfile
-  configure xsendfile: {
-    x_send_file_path: '/tmp'
-  }
 
   on_stage(:production) do
     recipe :cron_tasks
