5accf208d9fdd7e64dc456a3d11eae41b55965fe
Mattia Gheda <ghedamat@gmail.com>
2015-02-05 12:41:13 -0500

    refactor contribution_status_isolation_test

--- test/models/services/contribution_status_isolation_test.rb
+++ test/models/services/contribution_status_isolation_test.rb
@@ -1,13 +1,5 @@
 require 'test_helper'
 
-class FakeQuery
-  def initialize(org)
-  end
-  def unique_campaigns
-    []
-  end
-end
-
 class ContributionStatusTest < ActionController::TestCase
 
   context "given a non delisted organization that contributed since 2010" do
@@ -18,7 +10,10 @@ class ContributionStatusTest < ActionController::TestCase
         delisted_on: nil,
         signatory_of?: false
       )
-      @p = ContributionStatus.new(org, FakeQuery)
+      campaigns = [stub(name: "2010 Annual Contributions"),
+                           stub(name: "2009 Annual Contributions"),
+                           stub(name: "2013 Annual Contributions")]
+      @p = ContributionStatus.new(org, campaigns)
     end
 
     should "have the proper latest_annual_contribution_year" do
@@ -30,17 +25,12 @@ class ContributionStatusTest < ActionController::TestCase
     end
 
     should "list all the years the organization has contributed" do
-      FakeQuery.any_instance.stubs(unique_campaigns: [stub(name: "2010 Annual Contributions"),
-                           stub(name: "2009 Annual Contributions"),
-                           stub(name: "2013 Annual Contributions")])
       assert_equal @p.contributed_years.to_set, [2010, 2009, 2013].to_set
     end
 
     should "know if organization has contributed on a given year" do
-      FakeQuery.any_instance.stubs(unique_campaigns: [stub(name: "2013 Annual Contributions")])
-
       assert @p.contributor_for_year?(2013), "has contributed should be true"
-      refute @p.contributor_for_year?(2010), "has contributed should be false"
+      refute @p.contributor_for_year?(2012), "has contributed should be false"
     end
   end
 
@@ -52,7 +42,7 @@ class ContributionStatusTest < ActionController::TestCase
         delisted_on: Date.parse("2012-12-01"),
         signatory_of?: false
       )
-      @p = ContributionStatus.new(org, FakeQuery)
+      @p = ContributionStatus.new(org, [])
     end
 
     should "have the proper latest_annual_contribution_year" do
@@ -72,9 +62,8 @@ class ContributionStatusTest < ActionController::TestCase
         delisted_on: nil,
         signatory_of?: false
       )
-      @p = ContributionStatus.new(org, FakeQuery)
-      FakeQuery.any_instance.stubs(unique_campaigns: [stub(name: "2015 Annual Contributions"),
-                           stub(name: "2016 Annual Contributions")])
+      campaigns= [stub(name: "2015 Annual Contributions"), stub(name: "2016 Annual Contributions")]
+      @p = ContributionStatus.new(org, campaigns)
     end
 
     should "have the proper latest_annual_contribution_year" do
@@ -91,9 +80,7 @@ class ContributionStatusTest < ActionController::TestCase
       )
       org.expects(:signatory_of?).with(:lead).returns(true).at_least_once
 
-      @p = ContributionStatus.new(org, FakeQuery)
-      FakeQuery.any_instance.stubs(unique_campaigns: [stub(name: "LEAD 2015 Contributions"),
-                           stub(name: "LEAD 2016 Contributions")])
+      @p = ContributionStatus.new(org, [stub(name: "LEAD 2015 Contributions"), stub(name: "LEAD 2016 Contributions")])
     end
 
     should "have the proper latest_annual_contribution_year" do
@@ -113,14 +100,14 @@ class ContributionStatusTest < ActionController::TestCase
         delisted_on: nil,
         signatory_of?: false
       )
-      @p = ContributionStatus.new(org, FakeQuery)
+      campaigns = [stub(name: "2010 Annual Contributions"),
+                           stub(name: "2009 Annual Contributions"),
+                           stub(name: "2013 Annual Contributions"),
+                           stub(name: "Other Contributions")]
+      @p = ContributionStatus.new(org, campaigns)
     end
 
     should "list all the years the organization has contributed" do
-      FakeQuery.any_instance.stubs(unique_campaigns: [stub(name: "2010 Annual Contributions"),
-                           stub(name: "2009 Annual Contributions"),
-                           stub(name: "2013 Annual Contributions"),
-                           stub(name: "Other Contributions")])
       assert_equal @p.contributed_years.to_set, [2010, 2009, 2013].to_set
     end
   end
