a6f3355bb8b010b5a6255c5285d3826e803bf367
Benjamin Moss <drteeth@gmail.com>
2015-03-06 15:01:40 -0500

    formatting.

--- app/indices/resource_index.rb
+++ app/indices/resource_index.rb
@@ -1,15 +1,15 @@
-ThinkingSphinx::Index.define :resource, :with => :active_record do
-  indexes :title, :sortable => true
-  indexes :description, :sortable => true
-  indexes :year, :sortable => true
-  indexes links.title, :as => "link_title", :sortable => true
+ThinkingSphinx::Index.define :resource, with: :active_record do
+  indexes :title,         sortable: true
+  indexes :description,   sortable: true
+  indexes :year,          sortable: true
+  indexes links.title,    sortable: true,   as: :link_title,
 
-  has authors(:id),     :as => :authors_ids, :facet => true
-  has principles(:id),     :as => :principle_ids, :facet => true
-  has languages(:id), :as => :language_ids, facet: true
+  has authors(:id),       sortable: true,   as: :authors_ids
+  has principles(:id),    sortable: true,   as: :principle_ids
+  has languages(:id),     sortable: true,   as: :language_ids
 
   where "approval = 'approved'"
 
-  set_property :enable_star => true
-  set_property :min_prefix_len => 4
+  set_property enable_star: true
+  set_property min_prefix_len: 4
 end
