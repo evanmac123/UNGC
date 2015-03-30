86d664dd8411908a29926c8bf729682c0ef3dd78
Mattia Gheda <ghedamat@gmail.com>
2015-03-30 12:56:14 -0400

    increse again rack multipart limit for cop advanced form

--- config/initializers/multipart_limit.rb
+++ config/initializers/multipart_limit.rb
@@ -1,3 +1,2 @@
 #http://stackoverflow.com/questions/27773368/rails-4-2-internal-server-error-with-maximum-file-multiparts-in-content-reached
-Rack::Utils.multipart_part_limit = 512
-
+Rack::Utils.multipart_part_limit = 1024
