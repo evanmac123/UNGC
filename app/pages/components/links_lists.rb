129c27a0c83fc269cb900c24f0a7a2396b4e9a11
Mattia Gheda <ghedamat@gmail.com>
2015-04-14 17:08:42 -0400

    another iteration on links lists component until we add array support

--- app/pages/components/links_lists.rb
+++ app/pages/components/links_lists.rb
@@ -6,7 +6,12 @@ class Components::LinksLists
   def data
     # XXX links_list will be an array in the payload once we add proper
     # validation support for nested arrays to the cms
-    Array(@data[:widget_links_list])
+    d = @data[:widget_links_list]
+    if d
+      [d]
+    else
+      []
+    end
   end
 end
 
