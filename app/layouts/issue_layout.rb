88a257b7fbb133db2e53c6aae14ffa1059b4c0c4
Mattia Gheda <ghedamat@gmail.com>
2015-04-14 14:20:27 -0400

    first pass at issue template cms

---
+++ app/layouts/issue_layout.rb
@@ -0,0 +1,67 @@
+class IssueLayout < UNGC::Layout
+  extend UNGC::Layout::Macros
+
+  THEMES = %w[
+    none
+    light
+    dark
+  ]
+
+  has_one_container!
+
+  label 'Issue'
+  layout :issue
+
+  has_meta_tags!
+  has_taggings!
+
+  scope :hero do
+    field :image, type: :image_url
+    field :theme, type: :string, enum: THEMES, default: 'light'
+
+    scope :title do
+      field :title1, type: :string, limit: 50, required: true
+      field :title2, type: :string, limit: 50
+    end
+
+    field :blurb, type: :string, limit: 200, required: true
+  end
+
+  scope :issue_block do
+    field :title,    type: :string, limit: 100, required: true
+
+    scope :principles, array: true, max: 5 do
+      field :label, type: :string, limit: 20, required: true
+      field :blurb, type: :string, required: true, required: true
+      field :url,   type: :href,   required: true, required: true
+    end
+
+    field :content,  type: :string, required: true
+  end
+
+  scope :widget_contact do
+    field :contact_id, type: :number
+  end
+
+  scope :widget_calls_to_action, array: true, min: 1, max: 2 do
+    field :label, type: :string, limit: 50, required: true
+    field :url,   type: :href,   required: true
+  end
+
+  scope :widget_links_list do
+    field :title, type: :string, limit: 50
+
+    scope :links, array: true, max: 5 do
+      field :label, type: :string, limit: 20, required: true
+      field :url,   type: :href,   required: true
+    end
+  end
+
+  scope :resources, array: true, size: 3 do
+    field :resource_id, type: :number, required: true
+  end
+
+  scope :related_content, array: true, size: 3 do
+    field :container_path, type: :string, required: true
+  end
+end
---
+++ app/pages/issue_page.rb
@@ -0,0 +1,31 @@
+class IssuePage < ContainerPage
+  def hero
+    @data[:hero] || {}
+  end
+
+  def main_content_section
+    data = @data[:issue_block] || {}
+    data
+  end
+
+  def sidebar_widgets
+    widgets = {}
+
+    widgets[:contact] = Components::Contact.new(@data).data
+
+    widgets[:calls_to_action] = Components::CallsToAction.new(@data).data
+
+    widgets[:links_lists] = Components::LinksLists.new(@data).data
+
+    widgets
+  end
+
+
+  def resources
+    Components::Resources.new(@data).data
+  end
+
+  def events
+    Components::Events.new(@data).data
+  end
+end
