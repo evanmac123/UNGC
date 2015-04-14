d566f48fe1a3ec5c488f754778298c13346c9a8e
Mattia Gheda <ghedamat@gmail.com>
2015-04-14 16:40:41 -0400

    fix components linkslists

--- app/pages/components/links_lists.rb
+++ app/pages/components/links_lists.rb
@@ -4,7 +4,9 @@ class Components::LinksLists
   end
 
   def data
-    [@data[:widget_links_list]]
+    # XXX links_list will be an array in the payload once we add proper
+    # validation support for nested arrays to the cms
+    Array(@data[:widget_links_list])
   end
 end
 
