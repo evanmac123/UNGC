82b8a0214c085bcb58877d00427a955978a8be6b
Mattia Gheda <ghedamat@gmail.com>
2015-04-15 19:41:25 -0400

    fix topics component

--- frontend/admin/app/components/container/macros/x-topics.js
+++ frontend/admin/app/components/container/macros/x-topics.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sectors: Ember.inject.service(),
+  topics: Ember.inject.service(),
 
   _onInsertElement: function() {
-    this.get('sectors.data').then( (data) => {
+    this.get('topics.data').then( (data) => {
       this.set('items', data);
     });
   }.on('didInsertElement')
