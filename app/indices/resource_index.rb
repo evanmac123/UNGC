8c335fb9c8987eb0df1da6b145a38171a294ed15
Benjamin Moss <drteeth@gmail.com>
2015-04-17 16:41:37 -0400

    split old and new resource index

--- app/indices/resource_index.rb
+++ app/indices/resource_index.rb
@@ -1,4 +1,4 @@
-ThinkingSphinx::Index.define :resource, with: :active_record do
+ThinkingSphinx::Index.define :resource, with: :active_record, name: 'resource_old' do
   indexes :title,         sortable: true
   indexes :description,   sortable: true
   indexes :year,          sortable: true
@@ -7,13 +7,23 @@ ThinkingSphinx::Index.define :resource, with: :active_record do
   has authors(:id),       facet: true,      as: :authors_ids
   has principles(:id),    facet: true,      as: :principle_ids
   has languages(:id),     facet: true,      as: :language_ids
+
+  where "approval = 'approved'"
+
+  set_property enable_star: true
+  set_property min_prefix_len: 4
+end
+
+ThinkingSphinx::Index.define :resource, with: :active_record, name: 'resource_new' do
+  indexes :title,         sortable: true
+  indexes :description,   sortable: true
+  indexes links.title,    sortable: true,   as: :link_title
+
+  has languages(:id),     facet: true,      as: :language_ids
   has :content_type,      facet: true
   has sectors(:id),       facet: true,      as: :sector_ids
-  has sector_groups(:id), facet: true,      as: :sector_group_ids
   has issues(:id),        facet: true,      as: :issue_ids
-  has issue_areas(:id),   facet: true,      as: :issue_area_ids
   has topics(:id),        facet: true,      as: :topic_ids
-  has topic_groups(:id),  facet: true,      as: :topic_group_ids
 
   where "approval = 'approved'"
 
