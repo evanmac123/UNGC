284ce6ee05e0b7bbc44211a2f1c8a2d99b53132c
Benjamin Moss <drteeth@gmail.com>
2015-04-18 23:17:54 -0400

    make has_many indices use multi: true

--- app/indices/resource_index.rb
+++ app/indices/resource_index.rb
@@ -20,11 +20,11 @@ ThinkingSphinx::Index.define :resource, with: :active_record, name: 'resource_ne
   indexes :year,          sortable: true
   indexes links.title,    sortable: true,   as: :link_title
 
-  has languages(:id),     facet: true,      as: :language_ids
   has :content_type,      facet: true
-  has sectors(:id),       facet: true,      as: :sector_ids
-  has issues(:id),        facet: true,      as: :issue_ids
-  has topics(:id),        facet: true,      as: :topic_ids
+  has languages(:id),     facet: true,      as: :language_ids,  multi: true
+  has sectors(:id),       facet: true,      as: :sector_ids,    multi: true
+  has issues(:id),        facet: true,      as: :issue_ids,     multi: true
+  has topics(:id),        facet: true,      as: :topic_ids,     multi: true
 
   where "approval = 'approved'"
 
