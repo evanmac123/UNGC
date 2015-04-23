823e887aa5604ee82b6ada1afbc63ed53d443707
Benjamin Moss <drteeth@gmail.com>
2015-04-23 16:46:29 -0400

    remove the foreign key contraint before renaming the issue column.

--- db/migrate/20150423180932_rename_issue_area_to_issue.rb
+++ db/migrate/20150423180932_rename_issue_area_to_issue.rb
@@ -1,5 +1,7 @@
 class RenameIssueAreaToIssue < ActiveRecord::Migration
   def change
+    remove_foreign_key :issues, column: :issue_area_id
     rename_column :issues, :issue_area_id, :parent_id
+    add_foreign_key :issues, :issues, column: :parent_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421204948) do
+ActiveRecord::Schema.define(version: 20150423180932) do
 
   create_table "announcements", force: :cascade do |t|
     t.integer  "local_network_id", limit: 4
