f702d44bb0378db5b792c382a511312e64dd7b63
Mattia Gheda <ghedamat@gmail.com>
2015-04-27 16:17:17 -0400

    use a nested resource instead of rootUrl

--- frontend/admin/app/router.js
+++ frontend/admin/app/router.js
@@ -2,15 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: '/redesign/admin/'
+  location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('containers', { path: '/' }, function() {
-    this.route('new', { path: '/containers/new/in/:parent_container' });
-    this.route('edit', { path: '/containers/:container_id' });
-    this.route('needs-approval');
+  this.resource('admin', { path: '/redesign/admin'}, function() {
+    this.resource('containers', { path: '/' }, function() {
+      this.route('new', { path: '/containers/new/in/:parent_container' });
+      this.route('edit', { path: '/containers/:container_id' });
+      this.route('needs-approval');
+    });
   });
 });
 
