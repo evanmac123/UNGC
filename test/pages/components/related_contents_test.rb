11947a10d56767ed6f85eaca65b97e43d2c5f90c
Mattia Gheda <ghedamat@gmail.com>
2015-04-27 14:34:06 -0400

    fix related contents test

--- test/pages/components/related_contents_test.rb
+++ test/pages/components/related_contents_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 require 'pages/pages_test_helper'
 
-class Components::RelatedContentTest < ActiveSupport::TestCase
+class Components::RelatedContentsTest < ActiveSupport::TestCase
   include PagesTestHelper
 
   context 'with data' do
@@ -11,24 +11,24 @@ class Components::RelatedContentTest < ActiveSupport::TestCase
       create_container_with_payload('/test-path2', 'test title 2')
       create_container_with_payload('/test-path3', 'test title 3')
       data = {
-        related_content: {
+        related_contents: [{
           title: 'test title',
           content_boxes: [
             {container_path: '/test-path'},
             {container_path: '/test-path2'},
             {container_path: '/test-path3'}
           ]
-        }
+        }]
       }
-      @subject = Components::RelatedContent.new(data)
+      @subject = Components::RelatedContents.new(data)
     end
 
     should 'prepare the correct data for the presenter' do
-      assert_equal 'test title', @subject.boxes.first.title
+      assert_equal 'test title', @subject.first.boxes.first.title
     end
 
     should 'should have the proper title' do
-      assert_equal @subject.title, 'test title'
+      assert_equal @subject.first.title, 'test title'
     end
 
     should 'should associate the correct label' do
