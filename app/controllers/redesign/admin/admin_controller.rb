323ce64e503ec8e919507405dcad2dd0abf7260f
Mattia Gheda <ghedamat@gmail.com>
2015-04-27 16:03:40 -0400

    /redesing/admin is allowed only for ungc contacts

--- app/controllers/redesign/admin/admin_controller.rb
+++ app/controllers/redesign/admin/admin_controller.rb
@@ -1,5 +1,13 @@
 class Redesign::Admin::AdminController < ApplicationController
   layout 'redesign/admin'
 
-  before_filter :authenticate_contact! ### XXX limit this to UNGC org contacts
+  before_filter :authenticate_contact!
+  before_filter :only_ungc_contacts!
+
+  def only_ungc_contacts!
+    unless current_contact.from_ungc?
+      flash[:error] = "You do not have permission to access that resource."
+      redirect_to dashboard_path
+    end
+  end
 end
