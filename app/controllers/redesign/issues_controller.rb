f359968656ec8427481e6e2c288e3d7e83b47fed
Mattia Gheda <ghedamat@gmail.com>
2015-04-29 14:08:38 -0400

    refactor all issue page

--- app/controllers/redesign/issues_controller.rb
+++ app/controllers/redesign/issues_controller.rb
@@ -1,6 +1,7 @@
 class Redesign::IssuesController < Redesign::ApplicationController
   def index
     set_current_container :all_issue, '/what-is-un-global-compact/our-focus/all-issues'
-    @page = AllIssuePage.new(current_container, current_payload_data)
+    results = Redesign::Container.issue.includes(:public_payload)
+    @page = AllIssuePage.new(current_container, current_payload_data, results)
   end
 end
--- app/pages/all_issue_page.rb
+++ app/pages/all_issue_page.rb
@@ -1,15 +1,21 @@
 class AllIssuePage < ContainerPage
+  attr_reader :results
+
+  def initialize(container, payload, results)
+    super(container, payload)
+    @results = results
+  end
 
   def hero
     {
       title: {
-        title1: 'Issues'
+        title1: 'All Issues'
       }
     }
   end
 
   def issues
-    Redesign::Container.issue.includes(:public_payload).map { |c| Components::ContentBox.new(c) }
+    results.map { |c| Components::ContentBox.new(c) }
   end
 
 end
