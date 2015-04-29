886acf2ef3dc661085d3e564923a403d0ca81c16
Mattia Gheda <ghedamat@gmail.com>
2015-04-29 16:54:04 -0400

    add stub for what you can do form and add pagination

--- app/controllers/redesign/actions_controller.rb
+++ app/controllers/redesign/actions_controller.rb
@@ -1,7 +1,7 @@
 class Redesign::ActionsController < Redesign::ApplicationController
   def index
     set_current_container :tile_grid, '/take-action/what-you-can-do'
-    results = Redesign::Container.action.includes(:public_payload)
-    @page = WhatYouCanDoPage.new(current_container, current_payload_data, results)
+    form= Redesign::WhatYouCanDoForm.new(params[:page])
+    @page = WhatYouCanDoPage.new(current_container, current_payload_data, form.results)
   end
 end
