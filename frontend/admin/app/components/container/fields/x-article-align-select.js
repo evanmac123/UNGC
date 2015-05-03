17620107fd95bf7ee590f09962c517243e3065a4
Mattia Gheda <ghedamat@gmail.com>
2015-05-03 17:08:26 -0400

    remove right align from highlight article for now

--- frontend/admin/app/components/container/fields/x-article-align-select.js
+++ frontend/admin/app/components/container/fields/x-article-align-select.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   items: [
     Ember.Object.create({ id: 'left', label: 'Left' }),
-    Ember.Object.create({ id: 'center', label: 'Center' }),
-    Ember.Object.create({ id: 'right',  label: 'Right' })
+    Ember.Object.create({ id: 'center', label: 'Center' })//,
+    //Ember.Object.create({ id: 'right',  label: 'Right' }) // TODO implement later
   ]
 });
