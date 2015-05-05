4f6db616f4a9c446ebb4f205083f0ce985ce4088
Mattia Gheda <ghedamat@gmail.com>
2015-05-05 16:35:31 -0400

    update admin deploy script

--- frontend/admin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ember build --environment=production
-cp dist/assets/admin.js dist/assets/vendor.js ../../app/assets/javascripts/redesign-admin/
-cp dist/assets/admin.css dist/assets/vendor.css ../../app/assets/stylesheets/redesign-admin/
-cp dist/assets/*gif dist/assets/*png ../../app/assets/images
