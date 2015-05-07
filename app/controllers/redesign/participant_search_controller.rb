259990189041fafb5996f63466c550f96cc8ef16
minusfive <mail@minusfive.com>
2015-05-07 16:01:12 -0400

    Added all results to participants search index

--- app/controllers/redesign/participant_search_controller.rb
+++ app/controllers/redesign/participant_search_controller.rb
@@ -4,7 +4,10 @@ class Redesign::ParticipantSearchController < Redesign::ApplicationController
     set_current_container :highlight, '/what-is-gc/participants/directory'
     @search = Redesign::ParticipantSearchForm.new
     @page = create_page
-    @results = []
+
+
+    # @ghedamat, @benmoss: not sure this is the right way of displaying "all" participants on index. Also, needs to be sorted alphabetically by default
+    @results = ParticipantSearch::ResultsPresenter.new(@search.execute)
   end
 
   def search
