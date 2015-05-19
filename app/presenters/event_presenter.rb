8cbee0a9187ed3f60472e706e08de17a0cb22c72
Jordan Yee <jordanyee@users.noreply.github.com>
2015-05-19 13:20:44 -0400

    Remove redundant event initializer

--- app/presenters/event_presenter.rb
+++ app/presenters/event_presenter.rb
@@ -1,10 +1,10 @@
 class EventPresenter < SimpleDelegator
-  def initialize(event)
-    @event = event
-    super @event
+  def tagging_presenter
+    @tagging_presenter ||= TaggingPresenter.new(event)
   end
 
-  def tagging_presenter
-    @tagging_presenter ||= TaggingPresenter.new(@event)
-  end
+  private
+    def event
+      __getobj__
+    end
 end
