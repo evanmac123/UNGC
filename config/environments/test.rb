2a5443ab3ca6a4adad2fcfb29d14037dd418ea49
Jordan Yee <jordanyee@users.noreply.github.com>
2015-05-20 14:47:07 -0400

    Remove accidentally added strong params error raising

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,6 +46,4 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.active_record.raise_in_transactional_callbacks = false
-
-  config.action_controller.action_on_unpermitted_parameters = :raise
 end
