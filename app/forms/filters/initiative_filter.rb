297c3c73e20b81e0fa1221ea2ff657a9f975f16f
Benjamin Moss <drteeth@gmail.com>
2015-05-19 22:41:26 -0400

    InitiativeFilter only uses active Initiatives.

--- app/forms/filters/initiative_filter.rb
+++ app/forms/filters/initiative_filter.rb
@@ -1,7 +1,7 @@
 class Filters::InitiativeFilter < Filters::FlatSearchFilter
 
   def initialize(selected)
-    items = Initiative.all.select(:id, :name)
+    items = Initiative.active.select(:id, :name)
     super(items, selected)
     self.label = 'Initiative'
     self.key = 'initiatives'
