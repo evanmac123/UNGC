3ce1b6daac2a9cfcce5b71fdfa7210517687948d
Benjamin Moss <drteeth@gmail.com>
2015-05-20 14:26:38 -0400

    rename Private Company ListingStatus to Privately Held

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520173620) do
+ActiveRecord::Schema.define(version: 20150520181837) do
 
   create_table "announcements", force: :cascade do |t|
     t.integer  "local_network_id", limit: 4
