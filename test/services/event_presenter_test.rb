698fc1fc364c8207431ef4047572399c3e4defad
Jordan Yee <jordanyee@users.noreply.github.com>
2015-05-21 19:54:44 -0400

    [WIP] Add event and tagging presenter test
    This commit includes tests for the edge case where sponsors and tagging
    were not being returned as selected on validation fail. The test for
    sponsors and topics pass as they should but the tests for issues and
    sectors fail. I can’t seem to figure out why. Mind having a look?

---
+++ test/services/event_presenter_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class EventPresenterTest < ActiveSupport::TestCase
+  context 'given an event with taggings' do
+    context 'that fails validation and does not save' do
+      setup do
+        create_sponsor
+        create_topic
+        create_issue
+        create_sector
+
+        @sponsor = create_sponsor
+        @topic = create_topic
+        @issue = create_issue
+        @sector = create_sector
+        @event = create_event
+
+        @event.update_attributes({
+          title: '',
+          sponsor_ids: [@sponsor.id],
+          topic_ids: [@topic.id],
+          issue_ids: [@issue.id],
+          sector_ids: [@sector.id]
+        })
+
+        @presenter = EventPresenter.new(@event)
+      end
+
+      context 'given #sponsor_options' do
+        should 'return the unsaved sponsor as selected' do
+          unsaved_sponsor = @presenter.sponsor_options.find { |sponsor_option|
+            sponsor_option.id == @sponsor.id
+          }
+
+          assert unsaved_sponsor.selected?, 'Unsaved sponsor was not selected.'
+        end
+      end
+
+      context 'given #topic_options' do
+        should 'return the unsaved topic as selected' do
+          unsaved_topic = @presenter.topic_options.find { |topic_option|
+            topic_option[0].id == @topic.id
+          }
+
+          assert unsaved_topic[0].selected?, 'Unsaved topic was not selected.'
+        end
+      end
+
+      context 'given #issue_options' do
+        should 'return the unsaved issue as selected' do
+          unsaved_issue = @presenter.issue_options.find { |issue_option|
+            issue_option[0].id == @issue.id
+          }
+
+          assert unsaved_issue[0].selected?, 'Unsaved issue was not selected.'
+        end
+      end
+
+      context 'given #sector_options' do
+        should 'return the unsaved sector as selected' do
+          unsaved_sector = @presenter.sector_options.find { |sector_option|
+            sector_option[0].id == @sector.id
+          }
+
+          assert unsaved_sector[0].selected?, 'Unsaved sector was not selected.'
+        end
+      end
+    end
+  end
+end
