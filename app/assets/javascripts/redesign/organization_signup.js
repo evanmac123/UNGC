37a63534e8f7b6e6d414e5ae81c3726a4be07eca
minusfive <mail@minusfive.com>
2015-05-22 14:15:09 -0400

    First pass at styling/debuggin signup forms, expanded/enhanced base form styles, styled datepicker

--- app/assets/javascripts/redesign/organization_signup.js
+++ app/assets/javascripts/redesign/organization_signup.js
@@ -23,6 +23,21 @@ $(function(){
     $('.legal-status').hide();
   }
 
+  function showBusinessOnly (argument) {
+    $('.for_stakeholders_only').fadeOut('slow');
+    $('.for_business_only').fadeIn('slow');
+  }
+
+  function showStakeholdersOnly (argument) {
+    $('.for_business_only').fadeOut('slow');
+    $('.for_stakeholders_only').fadeIn('slow');
+  }
+
+  function hideBusinessAndStakeholders (argument) {
+    $('.for_stakeholders_only').fadeOut('slow');
+    $('.for_business_only').fadeOut('slow');
+  }
+
   if(noErrors() || !emptyNumber()){
     hideLegalStatus();
   }
@@ -42,6 +57,50 @@ $(function(){
     return ret;
   });
 
+  function toggleRegistrationFields(){
+    var org_type = $("#organization_organization_type_id option:selected").text();
+    if (org_type === "Company" || org_type === "SME") {
+      $('.company_only').show('slow');
+      $('.organization_registration').hide();
+    } else if (org_type !== ""){
+      $('.company_only').hide('slow');
+      $('.public_company_only').hide('slow');
+      $('.organization_registration').fadeIn();
+    }
+  }
+
+  toggleRegistrationFields();
+
+  $("#organization_organization_type_id").change(function() {
+    toggleRegistrationFields();
+  });
+
+  $('body').on('click', 'form #business_only', showBusinessOnly);
+  $('body').on('click', 'form #stakeholders_only', showStakeholdersOnly);
+  $('body').on('click', 'form #hide_business_and_stakeholders', hideBusinessAndStakeholders);
+  $('body').on('change', '#listing_status_id', function() {
+    selected_listing_status = jQuery.trim($("#listing_status_id option:selected").text());
+    if (selected_listing_status === "Publicly Listed") {
+      console.log('test');
+      $('.public_company_only').show();
+    } else {
+      $('.public_company_only').hide();
+    }
+  });
+
+  $("#organization_organization_type_id").change(function() {
+    toggleRegistrationFields();
+  });
+
+  $("#organization_listing_status_id").change(function() {
+    var selected_listing_status = jQuery.trim($("#organization_listing_status_id option:selected").text());
+    if (selected_listing_status === "Publicly Listed") {
+      $('.public_company_only').show();
+    } else {
+      $('.public_company_only').hide();
+    }
+  });
+
   // called from signup/step6.html.haml and /admin/organizations/_default_form.html.haml
   $("#non_business_organization_registration_mission_statement").change(function() {
     if (this.value.length >= 1000) {
@@ -58,7 +117,6 @@ $(function(){
 
   // called from views/signup/pledge_form_*.html.haml
   // hide select unless a pledge value is chosen
-
   function togglePledgeReason(){
     if ($("#organization_pledge_amount_0").is(':checked')) {
       $("#organization_no_pledge_reason").show();
@@ -69,8 +127,13 @@ $(function(){
 
   togglePledgeReason();
 
-  $('.fixed_pledge').on('click', function() {
+  $('.fixed_pledge').on('change', function() {
     togglePledgeReason();
   });
 
+  // called from views/signup/step5.html.haml
+  $("body").on("click", "#contact_foundation_contact", function() {
+    $('#errorExplanation').toggle();
+    $('#contact_form').toggle();
+  });
 });
