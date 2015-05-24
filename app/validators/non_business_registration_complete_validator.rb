d651bf0551dbdadd23d118a5e859c103356fc815
Mattia Gheda <ghedamat@gmail.com>
2015-05-24 18:06:29 -0400

    attempt to tweak non business registration validator, seems to have broke with rails 4 upgrade

--- app/validators/non_business_registration_complete_validator.rb
+++ app/validators/non_business_registration_complete_validator.rb
@@ -1,4 +1,9 @@
 class NonBusinessRegistrationCompleteValidator < NonBusinessRegistrationValidator
+  def initialize(registration, legal_status_id = nil)
+    super(registration)
+    @legal_status_id = legal_status_id
+  end
+
   def validate_callbacks
     super
     validate_mission_statement
@@ -13,7 +18,7 @@ class NonBusinessRegistrationCompleteValidator < NonBusinessRegistrationValidato
     end
 
     def validate_legal_status
-      return true if registration.organization.nil?
+      return true if registration.organization.nil? || @legal_status_id.present?
       if registration.organization.legal_status.blank? && registration.number.blank?
         registration.errors.add :number, "should be present or a legal status should be added"
       end
