7c8d13d2c1162502b81cba517bd296d74182466c
Mattia Gheda <ghedamat@gmail.com>
2015-05-25 12:42:01 -0400

    stub first integration test

---
+++ test/integration/redesign/pages/article_page_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class ArticlePageTest < ActionDispatch::IntegrationTest
+  setup do
+    create_staff_user
+    login_as @staff_user
+
+    # TODO create container with layout article
+    # TODO create payload for container
+    #
+    @container = create_container({
+      path: 'new-article-path',
+      layout: 'article'
+    })
+    @payload = create_payload({
+      container_id: @container.id
+    });
+
+    @container.public_payload = @payload
+    @container.save
+
+    visit '/redesign/new-article-path'
+  end
+
+  should 'have a hero' do
+    # test for hero title
+    # test for hero blurb
+    # test for section nav
+  end
+
+  should 'have content' do
+  end
+
+  should 'have sidebar widgets' do
+    #links
+    # contact
+    # callst to action
+
+  end
+end
