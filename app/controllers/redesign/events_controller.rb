1d4d34b5bf9103d4353eaed1427ab5776b9dd94d
minusfive <mail@minusfive.com>
2015-05-26 17:45:46 -0400

    [Events] First pass at event detail page

--- app/controllers/redesign/events_controller.rb
+++ app/controllers/redesign/events_controller.rb
@@ -12,32 +12,32 @@ class Redesign::EventsController < Redesign::ApplicationController
 
   private
 
-    def find_event
-      id = event_id_from_permalink
-      Event.approved.find(id)
-    end
+  def find_event
+    id = event_id_from_permalink
+    Event.approved.find(id)
+  end
 
-    def event_id_from_permalink
-      # to_i will convert the leading id portion to an int
-      # or the whole thing it's just the id
-      params.fetch(:id).to_i
-    end
+  def event_id_from_permalink
+    # to_i will convert the leading id portion to an int
+    # or the whole thing it's just the id
+    params.fetch(:id).to_i
+  end
 
-    def search_params
-      params.fetch(:search, {})
-        .permit(
-          :start_date,
-          :end_date,
-          issues: [],
-          topics: [],
-          countries: [],
-          types: []
-        )
-        .merge(page: page)
-    end
+  def search_params
+    params.fetch(:search, {})
+      .permit(
+        :start_date,
+        :end_date,
+        issues: [],
+        topics: [],
+        countries: [],
+        types: []
+      )
+      .merge(page: page)
+  end
 
-    def page
-      params.fetch(:page, 1)
-    end
+  def page
+    params.fetch(:page, 1)
+  end
 
 end
