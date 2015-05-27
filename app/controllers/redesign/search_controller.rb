146cd5159272fe24c056d427d25755e72c6688f2
Benjamin Moss <drteeth@gmail.com>
2015-05-27 14:29:02 -0400

    WIP

--- app/controllers/redesign/search_controller.rb
+++ app/controllers/redesign/search_controller.rb
@@ -1,6 +1,19 @@
 class Redesign::SearchController < Redesign::ApplicationController
 
   def search
+    @resuts = SitewideSearch.new(search_params).execute
+  end
+
+  private
+
+  def search_params
+    params.require(:s)
+  end
+
+  class SitewideSearch
+
+    def execute
+    end
 
   end
 
