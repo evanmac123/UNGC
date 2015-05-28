b99e7967a69d46bfa67ef95075a68bb5a67ad3f2
Benjamin Moss <drteeth@gmail.com>
2015-05-28 18:06:21 -0400

    More search work
    * basic form
    * basic results
    * excerpting
    * Redesign::Searchable index

--- app/controllers/redesign/search_controller.rb
+++ app/controllers/redesign/search_controller.rb
@@ -1,7 +1,9 @@
 class Redesign::SearchController < Redesign::ApplicationController
 
   def search
-    @resuts = SitewideSearch.new(search_params).execute
+    @search = SitewideSearch.new(search_params)
+    @results = @search.execute
+    @results.context[:panes] << ThinkingSphinx::Panes::ExcerptsPane
   end
 
   private
@@ -12,7 +14,35 @@ class Redesign::SearchController < Redesign::ApplicationController
 
   class SitewideSearch
 
+    def initialize(terms)
+      @terms = terms
+    end
+
     def execute
+      Redesign::Searchable.search(@terms, {
+        page: page,
+        per_page: per_page_capped,
+        star: true,
+        with: options,
+        indices: ['searchable_redesign_core'],
+        sql: {
+          include: [
+            # TODO update includes
+          ]
+        }
+      })
+    end
+
+    def options
+      {}
+    end
+
+    def page
+      1
+    end
+
+    def per_page_capped
+      10
     end
 
   end
--- app/indices/searchable_index.rb
+++ app/indices/searchable_index.rb
@@ -1,4 +1,4 @@
-ThinkingSphinx::Index.define :searchable, :with => :active_record do
+ThinkingSphinx::Index.define :searchable, :with => :active_record, name: :searchable do
   indexes title
   indexes content
   indexes document_type, :as => :document_type, :facet => true
@@ -9,3 +9,16 @@ ThinkingSphinx::Index.define :searchable, :with => :active_record do
   set_property :enable_star => true
   set_property :min_prefix_len => 4
 end
+
+ThinkingSphinx::Index.define :'redesign/searchable', :with => :active_record, name: :searchable_redesign do
+  indexes title
+  indexes content
+  indexes document_type, :as => :document_type, :facet => true
+  has url, last_indexed_at
+  set_property :delta => true # TODO: Switch this to :delayed once we have DJ working
+  set_property :field_weights => {"title" => 100}
+
+  set_property :enable_star => true
+  set_property :min_prefix_len => 4
+end
+
