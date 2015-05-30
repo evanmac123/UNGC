127c2e601cf969bbbcff69e7c322f786dfba3b8d
Benjamin Moss <drteeth@gmail.com>
2015-05-30 13:35:40 -0400

    refactor searchable container test.

--- test/models/redesign/searchable/searchable_container_test.rb
+++ test/models/redesign/searchable/searchable_container_test.rb
@@ -15,18 +15,18 @@ class Redesign::Searchable::SearchableContainerTest < ActiveSupport::TestCase
   end
 
   should "have an empty title if there is no meta title" do
-    searchable = Redesign::Searchable::SearchableContainer.new(published_container)
-    assert_equal "", searchable.title
+    container = published_container(data: {})
+    searchable = Redesign::Searchable::SearchableContainer.new(container)
+    assert_equal '', searchable.title
   end
 
   should "index the title from the payload" do
-    data = {meta_tags: {title: 'foo'}}
-    searchable = Redesign::Searchable::SearchableContainer.new(published_container(data: data))
-    assert_equal 'foo',  searchable.title
+    searchable = Redesign::Searchable::SearchableContainer.new(published_container)
+    assert_equal 'some title',  searchable.title
   end
 
   should "index the url from the container's path" do
-    searchable = Redesign::Searchable::SearchableContainer.new(published_container(params: {path: '/one/two/three'}))
+    searchable = Redesign::Searchable::SearchableContainer.new(published_container)
     assert_equal '/one/two/three', searchable.url
   end
 
@@ -61,6 +61,9 @@ class Redesign::Searchable::SearchableContainerTest < ActiveSupport::TestCase
   end
 
   should "should replace the searchable when the container changes path" do
+    container = create_published_contaner
+    Redesign::Searchable.index_all
+    container.update_attribute(:path, '/new/path')
     skip
   end
 
@@ -74,8 +77,18 @@ class Redesign::Searchable::SearchableContainerTest < ActiveSupport::TestCase
     @published_container ||= create_published_contaner(*args)
   end
 
-  def create_published_contaner(params: {}, data: {})
-    create_country # for the contact
+  def create_published_contaner(params: nil, data: nil)
+    params ||= {
+      path: '/one/two/three'
+    }
+
+    data ||= {
+      meta_tags: {
+        title: 'some title'
+      }
+    }
+
+    create_country # required for the create_contact
     container = create_container(params)
     container.draft_payload = create_payload(container_id: container.id, json_data: data.to_json)
     assert ContainerPublisher.new(container, create_contact).publish
