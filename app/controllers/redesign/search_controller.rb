dccfc0a6a7fd97b19344a743157b6bfd8262f153
Benjamin Moss <drteeth@gmail.com>
2015-06-01 17:31:44 -0400

    move SitewideSearch into it's own file.

--- app/controllers/redesign/search_controller.rb
+++ app/controllers/redesign/search_controller.rb
@@ -1,7 +1,7 @@
 class Redesign::SearchController < Redesign::ApplicationController
 
   def search
-    @search = SitewideSearch.new(search_params)
+    @search = Redesign::SitewideSearchForm.new(search_params)
     @results = @search.execute
   end
 
@@ -9,45 +9,6 @@ class Redesign::SearchController < Redesign::ApplicationController
 
   def search_params
     params.require(:search).permit(:keywords)
-  end
-
-  class SitewideSearch
-    include Virtus.model
-
-    attribute :keywords,      String,   default: ''
-    attribute :page,          Integer,  default: 1
-    attribute :per_page,      Integer,  default: 10
-    attribute :document_type, String
-
-    def execute
-      results = if document_type.present?
-        matching_facets = Redesign::Searchable.facets(escaped_keywords, options)
-        matching_facets.for(document_type: document_type)
-      else
-        Redesign::Searchable.search(escaped_keywords, sphinx_options)
-      end
-      results.context[:panes] << ThinkingSphinx::Panes::ExcerptsPane
-      results
-    end
-
-    def escaped_keywords
-      Riddle::Query.escape(keywords)
-    end
-
-    def sphinx_options
-      {
-        page: page,
-        per_page: per_page,
-        star: true,
-        with: options,
-        indices: ['searchable_redesign_core']
-      }
-    end
-
-    def options
-      {}
-    end
-
   end
 
 end
---
+++ app/forms/redesign/sitewide_search_form.rb
@@ -0,0 +1,38 @@
+class Redesign::SitewideSearchForm
+    include Virtus.model
+
+    attribute :keywords,      String,   default: ''
+    attribute :page,          Integer,  default: 1
+    attribute :per_page,      Integer,  default: 10
+    attribute :document_type, String
+
+    def execute
+      results = if document_type.present?
+        matching_facets = Redesign::Searchable.facets(escaped_keywords, options)
+        matching_facets.for(document_type: document_type)
+      else
+        Redesign::Searchable.search(escaped_keywords, sphinx_options)
+      end
+      results.context[:panes] << ThinkingSphinx::Panes::ExcerptsPane
+      results
+    end
+
+    def escaped_keywords
+      Riddle::Query.escape(keywords)
+    end
+
+    def sphinx_options
+      {
+        page: page,
+        per_page: per_page,
+        star: true,
+        with: options,
+        indices: ['searchable_redesign_core']
+      }
+    end
+
+    def options
+      {}
+    end
+
+  end
