643eaba44d83ad11cb34833ae14bdaa52a0f0946
Benjamin Moss <drteeth@gmail.com>
2015-06-01 18:25:19 -0400

    add search facets

--- app/forms/redesign/sitewide_search_form.rb
+++ app/forms/redesign/sitewide_search_form.rb
@@ -1,38 +1,61 @@
 class Redesign::SitewideSearchForm
-    include Virtus.model
+  include Virtus.model
 
-    attribute :keywords,      String,   default: ''
-    attribute :page,          Integer,  default: 1
-    attribute :per_page,      Integer,  default: 10
-    attribute :document_type, String
+  attribute :keywords,      String,   default: ''
+  attribute :page,          Integer,  default: 1
+  attribute :per_page,      Integer,  default: 10
+  attribute :document_type, String
 
-    def execute
-      results = if document_type.present?
-        matching_facets = Redesign::Searchable.facets(escaped_keywords, options)
-        matching_facets.for(document_type: document_type)
-      else
-        Redesign::Searchable.search(escaped_keywords, sphinx_options)
-      end
-      results.context[:panes] << ThinkingSphinx::Panes::ExcerptsPane
-      results
+  def matched_facets
+    @results.facets[:document_type].map do |document_type, count|
+      MatchingFacet.new(document_type, count, attributes)
+    end
+  end
+
+  def execute
+    @results = if document_type.present? then faceted_search else keyword_search end
+    @results.context[:panes] << ThinkingSphinx::Panes::ExcerptsPane
+    @results
+  end
+
+  private
+
+  def faceted_search
+    matching_facets = Redesign::Searchable.facets(escaped_keywords, options)
+    matching_facets.for(document_type: document_type)
+  end
+
+  def keyword_search
+    Redesign::Searchable.search(escaped_keywords, options)
+  end
+
+  def escaped_keywords
+    Riddle::Query.escape(keywords)
+  end
+
+  def options
+    {
+      page: page,
+      per_page: per_page,
+      star: true,
+      indices: ['searchable_redesign_core']
+    }
+  end
+
+  MatchingFacet = Struct.new(:document_type, :count, :attributes) do
+
+    def name
+      "#{document_type} (#{count})"
     end
 
-    def escaped_keywords
-      Riddle::Query.escape(keywords)
-    end
-
-    def sphinx_options
-      {
-        page: page,
-        per_page: per_page,
-        star: true,
-        with: options,
-        indices: ['searchable_redesign_core']
+    def merge_params_into(params, key)
+      facet_params = {
+        document_type: document_type,
+        page: 1
       }
-    end
-
-    def options
-      {}
+      params.merge(key => params[key].merge(facet_params))
     end
 
   end
+
+end
