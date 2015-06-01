fde53c2b42d3f15e36563b13f03d36302e99a92e
Mattia Gheda <ghedamat@gmail.com>
2015-06-01 11:42:09 -0400

    add differentiation_level_with_default method to ensure that a :type is always present when using the cop_path redesign url

--- test/controllers/redesign/cops_controller_test.rb
+++ test/controllers/redesign/cops_controller_test.rb
@@ -3,16 +3,16 @@ require 'test_helper'
 class Redesign::CopsControllerTest < ActionController::TestCase
 
   should "link to a COP" do
-    expected = "/redesign/participation/report/cop/create-and-submit/basic/#{cop.id}"
-    cop_path = show_redesign_cops_path(type: cop.cop_type, id: cop.id)
+    expected = "/redesign/participation/report/cop/create-and-submit/learner/#{cop.id}"
+    cop_path = show_redesign_cops_path(type: cop.differentiation_level_with_default, id: cop.id)
     assert_equal expected, cop_path
   end
 
   should "link to a COP without a type" do
     # there are many existing COPs that lack a value for cop_type
     cop = cop(cop_type: nil)
-    expected = "/redesign/participation/report/cop/create-and-submit/basic/#{cop.id}"
-    cop_path = show_redesign_cops_path(type: cop.cop_type, id: cop.id)
+    expected = "/redesign/participation/report/cop/create-and-submit/learner/#{cop.id}"
+    cop_path = show_redesign_cops_path(type: cop.differentiation_level_with_default, id: cop.id)
     assert_equal expected, cop_path
   end
 
@@ -25,5 +25,3 @@ class Redesign::CopsControllerTest < ActionController::TestCase
   end
 
 end
-
-
