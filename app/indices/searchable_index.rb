ba61733e82eae6e7af5a62d412e9d2f7d75cc8d7
Benjamin Moss <drteeth@gmail.com>
2015-06-01 22:49:38 -0400

    add meta to searchable index.

--- app/indices/searchable_index.rb
+++ app/indices/searchable_index.rb
@@ -13,8 +13,9 @@ end
 ThinkingSphinx::Index.define :'redesign/searchable', :with => :active_record, name: :searchable_redesign do
   indexes title
   indexes content
+  indexes meta
   indexes document_type, :as => :document_type, :facet => true
-  has url, last_indexed_at
+
   set_property :delta => true # TODO: Switch this to :delayed once we have DJ working
   set_property :field_weights => {"title" => 100}
 
