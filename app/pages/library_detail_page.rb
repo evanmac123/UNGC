8c420ebcd97df0945bc7c08904ee8aed867d8773
Mattia Gheda <ghedamat@gmail.com>
2015-06-03 18:47:58 -0400

    add hero for library detail

---
+++ app/pages/library_detail_page.rb
@@ -0,0 +1,32 @@
+class LibraryDetailPage
+  attr_reader :container, :resource
+
+  def initialize(container, resource)
+    @container = container
+    @resource = resource
+  end
+
+  def hero
+    {size: 'small'}
+    {
+      title: {
+        title1: 'Explore Our',
+        title2: 'Library'
+      },
+      image: 'https://d306pr3pise04h.cloudfront.net/uploads/02/029f94ffc19f95c80a441f47697f54055cda1b25---library.jpg',
+      size: 'small',
+      show_section_nav: true
+    }
+  end
+
+  def meta_title
+    resource.title
+  end
+
+  def meta_description
+  end
+
+  def section_nav
+    return Components::SectionNav.new(container)
+  end
+end
