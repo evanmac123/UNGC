fae096bee258b39a3f20602fc364c498d986b6e4
Mattia Gheda <ghedamat@gmail.com>
2015-06-03 18:53:54 -0400

    add hero to sitewide search

---
+++ app/pages/sitewide_search_page.rb
@@ -0,0 +1,19 @@
+class SitewideSearchPage
+  def hero
+    {size: 'small'}
+    {
+      title: {
+        title1: 'Search the website'
+      },
+      image: 'https://d306pr3pise04h.cloudfront.net/uploads/c2/c2d709ffa21a94233931ba8e51b2ca85296e88bc---UN.jpg',
+      size: 'small'
+    }
+  end
+
+  def meta_title
+    'Search'
+  end
+
+  def meta_description
+  end
+end
