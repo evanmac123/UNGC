c3e3d3bb3a73ee68b841ef6b5e1a6435e48f6d55
Benjamin Moss <drteeth@gmail.com>
2015-06-03 08:31:45 -0400

    move EmptySearch to it's own file.

--- app/forms/redesign/sitewide_search_form.rb
+++ app/forms/redesign/sitewide_search_form.rb
@@ -84,24 +84,4 @@ class Redesign::SitewideSearchForm
 
   end
 
-  class EmptySearch
-
-    def method_missing(*args)
-      self
-    end
-
-    def any?
-      false
-    end
-
-    def none?
-      false
-    end
-
-    def total_pages
-      0
-    end
-
-  end
-
 end
