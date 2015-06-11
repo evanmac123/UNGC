83f9a970d9b4e536a1a86fc871a46683441a352a
Mattia Gheda <ghedamat@gmail.com>
2015-06-11 13:54:45 -0400

    index only certain keys

--- app/models/redesign/searchable/searchable_container.rb
+++ app/models/redesign/searchable/searchable_container.rb
@@ -31,23 +31,28 @@ class Redesign::Searchable::SearchableContainer < Redesign::Searchable::Base
   end
 
   def meta
-    container.taggings.map(&:content).join(' ')
+    meta_tags = container.payload.data[:meta_tags]
+    container.taggings.map(&:content).join(' ') + extract_values(meta_tags).reject(&:blank?).join(' ')
   end
 
   private
 
-  def extract_values(input)
+  def valid_key?(key)
+    [:blurb, :content, :title, :title1, :title2, :description, :keywords].include? key
+  end
+
+  def extract_values(input, key = nil)
     case
     when input.is_a?(Hash)
       input.flat_map do |k, v|
-        extract_values(v)
+        extract_values(v, k) unless k == :meta_tags
       end
     when input.is_a?(Array)
       input.flat_map do |v|
         extract_values(v)
       end
     else
-      input.to_s
+      input.to_s if valid_key?(key)
     end
   end
 
