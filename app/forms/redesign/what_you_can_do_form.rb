aaccfe4c1f84b0da9f6dd3e2fe5a66001720b751
Benjamin Moss <drteeth@gmail.com>
2015-06-16 23:31:44 -0400

    DRY up AllOurWorkForm and WhatYouCanDoForm
    * implemented using delegation to step around complex module initialization issues with Filters + Virtus

--- app/forms/redesign/what_you_can_do_form.rb
+++ app/forms/redesign/what_you_can_do_form.rb
@@ -1,43 +1,7 @@
-class Redesign::WhatYouCanDoForm < Redesign::FilterableForm
-  include Virtus.model
+class Redesign::WhatYouCanDoForm < SimpleDelegator
 
-  attribute :page,        Integer,        default: 1
-  attribute :per_page,    Integer,        default: 12
-  attribute :issues,      Array[Integer], default: []
-  attribute :topics,      Array[Integer], default: []
-
-  filter :issue
-  filter :topic
-
-  attr_reader :seed
   def initialize(params, seed)
-    super(params)
-    @seed = seed
-  end
-
-  def facets
-    Redesign::Container.actions.facets ''
-  end
-
-  def execute
-    Redesign::Container.actions.search '', options
-  end
-
-  def options
-    # TODO randomize the order
-    # sphinx does not support rand() with a seed until 2.2.4
-    {
-      page: page,
-      per_page: per_page,
-      with: facet_options,
-    }
-  end
-
-  def facet_options
-    {
-      issue_ids: issue_filter.effective_selection_set,
-      topic_ids: topic_filter.effective_selection_set,
-    }.reject { |_, value| value.blank? }
+    super Redesign::ContainerForm.new(params, seed, sphinx_scope: Redesign::Container.actions)
   end
 
 end
