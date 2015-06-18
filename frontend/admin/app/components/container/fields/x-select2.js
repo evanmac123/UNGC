3464f04d38ab09e0c07bd26873ec51444d73a918
Mattia Gheda <ghedamat@gmail.com>
2015-06-18 17:41:42 -0400

    workaround for select2 problem with deleted records

--- frontend/admin/app/components/container/fields/x-select2.js
+++ frontend/admin/app/components/container/fields/x-select2.js
@@ -6,5 +6,26 @@ export default Ember.Component.extend({
   labelPath: null,
   placeholder: null,
   allowClear: false,
-  multiple: false
+  multiple: false,
+
+  // XXX hack, if the value that was in the field doesn't exist anymore
+  // i.e. it was deleted from the db
+  // select2 will disable the field
+  //
+  // we have to detect this in advance and reset value to null
+  // so that the user can edit it again
+  _onItems: function() {
+    let items = this.get('field.items');
+    if (!items) {
+      return;
+    }
+    let exists = items.any((i) => {
+      let val = this.get('field.value');
+      let id = parseInt(i.get('id'), 10);
+      return id === val;
+    });
+    if (!exists) {
+      this.set('field.value', null);
+    }
+  }.observes('field.items.[]')
 });
