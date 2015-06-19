92fa367cbaebeb61e30ef702070ccd88a46dab57
Mattia Gheda <ghedamat@gmail.com>
2015-06-19 13:39:46 -0400

    gc15 redirect

--- config/routes.rb
+++ config/routes.rb
@@ -457,7 +457,8 @@ UNGC::Application.routes.draw do
 
     # new redirects (to be tested)
     get '/index.html', to: redirect('/')
-    get '/NewsAndEvents/global_compact_15.html', to: redirect('take-action/events/31-global-compact-15-business-as-a-force-for-good')
+    get '/NewsAndEvents/global_compact_15.html', to: redirect('/take-action/events/31-global-compact-15-business-as-a-force-for-good')
+    get ':gc15', to: redirect('/take-action/events/31-global-compact-15-business-as-a-force-for-good'), :constraints => { :gc15 => /gc15/i }
     get '/aboutthegc/thetenprinciples(*path)', to: redirect('/what-is-gc/mission/principles')
     get '/AboutTheGC/TheTenPrinciples/principle1.html' => 'static#redirect_to_page', page: '/what-is-gc/mission/principles/principle-1'
     get '/participantsandstakeholders/civil_society.html', to: redirect('/what-is-gc/participants')
