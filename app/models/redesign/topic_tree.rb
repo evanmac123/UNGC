502e81dfc00d462188528431f98effa5b3b22953
Mattia Gheda <ghedamat@gmail.com>
2015-06-19 11:07:23 -0400

    hardcode lists of allowed issues topics for take-action/action and our-work/all /cc @drteeth

--- app/models/redesign/topic_tree.rb
+++ app/models/redesign/topic_tree.rb
@@ -1,11 +1,12 @@
 class Redesign::TopicTree < Redesign::Tree
 
-  def initialize
+  def initialize(excluded: ['none'])
     super(Topic.where(parent_id: nil)
+      .where.not('name in (?)', excluded)
       .includes(:children)
       .select([:id, :name])
       .map do |parent|
-        [parent, parent.children]
+        [parent, parent.children.where.not('name in (?)', excluded)]
       end)
   end
 
