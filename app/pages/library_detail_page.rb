e05a9b9dc5fe8840ab04f7a12f0dbe6b09743f6c
Mattia Gheda <ghedamat@gmail.com>
2015-06-22 10:03:17 -0400

    change casing on library detail page hero

--- app/pages/library_detail_page.rb
+++ app/pages/library_detail_page.rb
@@ -10,7 +10,7 @@ class LibraryDetailPage
     {size: 'small'}
     {
       title: {
-        title1: 'Explore Our Library'
+        title1: 'Explore our library'
       },
       image: 'https://d306pr3pise04h.cloudfront.net/uploads/9e/9e9d00411ce7c64d9e8ad95595342210068a29d0---library.jpg',
       size: 'small',
