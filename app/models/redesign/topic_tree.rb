9f1a0e3adb988c9d716ce074de9657026b18bb9a
Benjamin Moss <drteeth@gmail.com>
2015-07-06 15:25:45 -0400

    remove excluded list from topic filter

--- app/models/redesign/topic_tree.rb
+++ app/models/redesign/topic_tree.rb
@@ -1,12 +1,11 @@
 class Redesign::TopicTree < Redesign::Tree
 
-  def initialize(excluded: ['none'])
+  def initialize
     super(Topic.where(parent_id: nil)
-      .where.not('name in (?)', excluded)
       .includes(:children)
       .select([:id, :name])
       .map do |parent|
-        [parent, parent.children.where.not('name in (?)', excluded)]
+        [parent, parent.children]
       end)
   end
 
