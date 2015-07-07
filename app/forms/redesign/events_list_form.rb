70fe6f67b40b249b29c2fbe0913fcd3d846670df
Benjamin Moss <drteeth@gmail.com>
2015-07-07 14:38:38 -0400

    replace sql event search with a sphinx one.

--- app/forms/redesign/events_list_form.rb
+++ app/forms/redesign/events_list_form.rb
@@ -1,28 +1,50 @@
 class Redesign::EventsListForm < Redesign::FilterableForm
   include Virtus.model
+  include Redesign::FilterMacros
 
   attribute :page,        Integer,        default: 1
   attribute :per_page,    Integer,        default: 12
   attribute :start_date,  Date,           default: -> (page, attribute) { Date.today }
   attribute :end_date,    Date
 
+  # http://stackoverflow.com/questions/535721/ruby-max-integer
+  FIXNUM_MAX = (2**(0.size * 8 -2) -1)
+
   def execute
-    events = Event.approved.includes(:country).order('starts_at asc')
+    Event.search '', options
+  end
 
-    e = Event.arel_table
-    case
-    when start_date.present? && end_date.present?
-      events = events.where(
-        (e[:starts_at].gteq(start_date).or(e[:ends_at].gteq(start_date)))
-        .and(e[:starts_at].lteq(end_date).or(e[:ends_at].lteq(end_date)))
-      )
-    when start_date.present?
-      events = events.where(e[:starts_at].gteq(start_date).or(e[:ends_at].gteq(start_date)))
-    when end_date.present?
-      events = events.where(e[:starts_at].lteq(end_date).or(e[:ends_at].lteq(end_date)))
+  def options
+    {
+      select: date_range_clause.join,
+      with: {in_date_range: true}
+    }
+  end
+
+  def date_range_clause
+    [
+      "*, ",
+      "(starts_at >= #{start_of_first_date} and starts_at <= #{end_of_last_date})",
+      " or ",
+      "(ends_at >= #{start_of_first_date} and ends_at <= #{end_of_last_date})",
+      " as in_date_range"
+    ]
+  end
+
+  def start_of_first_date
+    if start_date.present?
+      start_date.to_datetime.beginning_of_day.to_i
+    else
+      0
     end
+  end
 
-    events.distinct('events.id').paginate(page: page, per_page: per_page)
+  def end_of_last_date
+    if end_date.present?
+      end_date.to_datetime.end_of_day.to_i
+    else
+      FIXNUM_MAX
+    end
   end
 
 end
