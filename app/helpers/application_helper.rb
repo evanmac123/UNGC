437ee2d9fd1e330ff5538f73aa2b7147166f76d3
Jordan Yee <jordanyee@users.noreply.github.com>
2015-07-14 18:15:53 -0400

    Move pages helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,11 +19,11 @@ module ApplicationHelper
   def staff_only(&block)
     yield if current_contact && current_contact.from_ungc?
   end
-  
+
   def participant_manager_only(&block)
     yield if current_contact.is? Role.participant_manager
   end
-  
+
 
   def organization_only(&block)
     yield if current_contact && current_contact.from_organization?
@@ -85,4 +85,30 @@ module ApplicationHelper
     image_tag model.cover_image(size), options
   end
 
+  # TODO: Extracted from the now deleted PagesHelper. Maybe could be cleaned up later?
+  def per_page_select(steps=[10,25,50,100,250])
+    # ['Number of results per page', 10, 25, 50, 100]
+    options = []
+    steps.each do |step|
+      options << ["#{step} results per page", url_for(params.merge(:page => 1, :per_page => step))]
+    end
+    selected = url_for(params.merge(:page => 1, :per_page => params[:per_page]))
+    select_tag :per_page, options_for_select(options, :selected => selected), :class => 'autolink'
+  end
+
+  def cop_link(cop, navigation=nil)
+    if navigation
+      cop_detail_with_nav_path(navigation, cop.id)
+    else
+      show_redesign_cops_path(differentiation: cop.differentiation_level_with_default, id: cop.id)
+    end
+  end
+
+  def participant_link(organization, navigation=nil)
+    if navigation
+      participant_with_nav_path(navigation, organization)
+    else
+      redesign_participant_path(organization)
+    end
+  end
 end
