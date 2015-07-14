40cb5efd62f54071dc44947b2c098f20f60db927
Jordan Yee <jordanyee@users.noreply.github.com>
2015-07-14 15:42:33 -0400

    Remove unused methods from resource helper

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,47 +1,10 @@
 module ResourcesHelper
-
-  def languages_for_select(selected_language=nil)
-    languages = ResourceLink.includes(:language).joins(:resource).where("resources.approval='approved'").group('language_id').select(:language_id).map { |c| [c.language.name, c.language_id] }
-    options_for_select(languages, selected: selected_language)
-  end
-
-  def resource_sort_by
-    options = [
-      ["Sort by Year", url_for(params.merge(:order => "year desc"))],
-      # ["Sort by Year (oldest)", url_for(params.merge(:order => "year asc"))],
-      ["Sort by Title", url_for(params.merge(:order => "title asc"))]
-    ]
-    selected = url_for(params.merge(:order => params[:order]))
-    select_tag :per_page, options_for_select(options, :selected => selected), :class => 'autolink'
-  end
-
-  def approval_name(resource)
-    case resource.approval
-    when 'pending'
-      'Pending review'
-    when 'approved'
-      'Approved'
-    when 'previously'
-      'Revoked'
-    else
-      'Unknown'
-    end
-  end
-
-  def approver_name(resource)
-    Contact.find(resource.approved_by_id).first_name rescue 'Unknown'
-  end
-
   def status_date(resource)
     resource.approved? ? resource.approved_at : resource.updated_at
   end
 
   def resource_sort_field(status)
     status == :approved ? 'approved_at' : 'updated_at'
-  end
-
-  def link_to_all_resources_from_display_shelf(reference)
-    link_to 'View All', resources_path(commit: 'search', resource_search: { topic: {principle_ids: [Principle.find_by_reference(reference)]}}), class: 'view-all'
   end
 
   def link_to_all_resources(reference)
@@ -52,5 +15,4 @@ module ResourcesHelper
     resources = Resource.find ids
     ids.collect { |id| resources.detect {|x| x.id == id} }
   end
-
 end
