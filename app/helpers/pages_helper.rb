f88e8f755cf1f48c4cbc289f712ef584787afa65
Jordan Yee <jordanyee@users.noreply.github.com>
2015-07-14 18:11:05 -0400

    Remove now unused method from PagesHelper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -18,16 +18,6 @@ module PagesHelper
     end
   end
 
-
-
-  # NOTE: Used to combine dynamic headlines with old, static content
-  # if there are headlines for old years (< 2009) there will be
-  # duplication of years on the archive page.
-  def news_archive_years
-    # (2009..Date.today.year).to_a.reverse
-    Headline.years
-  end
-
   def participant_link(organization, navigation=nil)
     if navigation
       participant_with_nav_path(navigation, organization)
