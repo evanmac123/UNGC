5db17443b29825c1b0e561ad101865f1b661ee1e
Jordan Yee <jordanyee@users.noreply.github.com>
2015-07-16 17:11:48 -0400

    Remove unused code in Indexable

--- lib/indexable.rb
+++ lib/indexable.rb
@@ -3,8 +3,6 @@ module Indexable
   # include Indexable
   # and ensure that Searchable has a method: remove_your_model_name(model)
   def self.included(receiver)
-    method = "remove_#{receiver.model_name.param_key}".to_sym
-
     receiver.class_eval do
       before_save do
         Redesign::Searchable.update_url(self)
