b723932adc24baf0d01c54bf327a7879987dcf81
Jordan Yee <jordanyee@users.noreply.github.com>
2015-07-20 14:58:40 -0400

    Remove link_to_redesign redesign helper

--- app/helpers/redesign/redesign_helper.rb
+++ app/helpers/redesign/redesign_helper.rb
@@ -1,17 +1,5 @@
 module Redesign::RedesignHelper
 
-  def link_to_redesign(name = nil, options = nil, html_options = nil, &block)
-    html_options, options, name = options, name, block if block_given?
-    options ||= {}
-
-    html_options = convert_options_to_data_attributes(options, html_options)
-
-    url = url_for(options)
-
-    html_options['href'] ||= url
-    content_tag(:a, name || url, html_options, &block)
-  end
-
   def search_filter(filter, disabled: false)
     options = {
       label: filter.label,
