02a0e118e4ddd64e9f26467e3027ddde90406a6b
Benjamin Moss <drteeth@gmail.com>
2015-07-26 15:12:12 -0400

    add missing principle areas for cops feed test.

--- test/integration/cops_feed_test.rb
+++ test/integration/cops_feed_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class CopsFeedTest < ActionDispatch::IntegrationTest
   setup do
     create_principle_area(name: "Human Rights")
+    create_principle_area(name: "Labour")
+    create_principle_area(name: "Environment")
+    create_principle_area(name: "Anti-Corruption")
+
     @org_type = create_organization_type
     @org = create_organization
     @cop = create_communication_on_progress(cop_type: 'basic')
