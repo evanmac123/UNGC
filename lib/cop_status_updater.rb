8ab71cfe45a1bf53186e143a30c6472844037e98
Benjamin Moss <drteeth@gmail.com>
2015-08-11 14:18:17 -0400

    Company stayed "Active" after passing COP deadline
    This commit doesn't fix the issue, which is that organizations that fail
    validations can't be delisted/late.
    
    This commit surfaces the errors that were failing silently and sends them to
    honeybadger.

--- lib/cop_status_updater.rb
+++ lib/cop_status_updater.rb
@@ -1,35 +1,68 @@
 class CopStatusUpdater
-  class << self
 
-    def log (method="info", string)
-      @logger = Logger.new(File.join(Rails.root, 'log', 'cop_reminder.log'))
-      @logger.send method.to_sym, "#{Time.now.strftime "%Y-%m-%d %H:%M:%S"} : #{string}"
+  def self.update_all
+    updater = self.new
+    updater.move_active_organizations_to_noncommunicating
+    updater.move_noncommunicating_organizations_to_delisted
+  end
+
+  def move_active_organizations_to_noncommunicating
+    info "Running move_active_organizations_to_noncommunicating"
+    organizations = Organization.businesses.participants.active.about_to_become_noncommunicating
+    organizations.each do |organization|
+      move_to_noncommunicating(organization)
     end
+  end
 
-    def update_all
-      move_active_organizations_to_noncommunicating
-      move_noncommunicating_organizations_to_delisted
+  def move_noncommunicating_organizations_to_delisted
+    info "Running move_noncommunicating_organizations_to_delisted"
+    organizations = Organization.businesses.participants.active.about_to_become_delisted
+    organizations.each do |organization|
+      move_to_noncommunicating(organization)
+      delist(organization)
     end
+  end
 
-    def move_active_organizations_to_noncommunicating
-      log "Running move_active_organizations_to_noncommunicating"
-      organizations = Organization.businesses.participants.active.about_to_become_noncommunicating
-      organizations.each { |org| org.communication_late; log "#{org.id}: #{org.name} is Non-Communicating" }
-    end
+  private
 
-    def move_noncommunicating_organizations_to_delisted
-      log "Running move_noncommunicating_organizations_to_delisted"
-      organizations = Organization.businesses.participants.active.about_to_become_delisted
-      organizations.each do |organization|
-        organization.delist
-        begin
-          CopMailer.delisting_today(organization).deliver
-          log "Delist and email #{organization.id}:#{organization.name}"
-        rescue
-          log "error", "Could not email #{organization.id}:#{organization.name}"
-        end
-      end
-    end
+  def move_to_noncommunicating(organization)
+    info "#{organization.id}: #{organization.name} is Non-Communicating"
+    organization.communication_late!
+  rescue => e
+    error "Failed to move organization to Non-Communicating #{organization.id}:#{organization.name}", e
+  end
 
-  end # class << self
-end # class CopStatusUpdater
+  def delist(organization)
+    organization.delist!
+    info "Delisted #{organization.id}:#{organization.name}"
+  rescue => e
+    error "Failed to delist #{organization.id}:#{organization.name}", e
+  end
+
+  def notify_of_delisting(organization)
+    CopMailer.delisting_today(organization).deliver
+    info "emailed delisted #{organization.id}:#{organization.name}"
+  rescue => e
+    error "Could not email #{organization.id}:#{organization.name}", e
+  end
+
+  def info(message)
+    log(message)
+  end
+
+  def error(message, error=nil, params={})
+    log("error", message)
+    debugger
+    Honeybadger.notify(
+      error_class:    "CopStatusUpdater",
+      error_message:  "message #{error}",
+      parameters:     params
+    )
+  end
+
+  def log (method="info", string)
+    @logger ||= Logger.new(File.join(Rails.root, 'log', 'cop_reminder.log'))
+    @logger.send method.to_sym, "#{Time.now.strftime "%Y-%m-%d %H:%M:%S"} : #{string}"
+  end
+
+end
