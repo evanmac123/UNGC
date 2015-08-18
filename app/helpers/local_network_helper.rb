11eaf86cb91bc32cbb1802404ff29b447e5549b7
Jordan Yee <jordanyee@users.noreply.github.com>
2015-08-18 18:34:32 -0400

    Reimplement list of benefits of participation PDFs in networks show view

--- app/helpers/local_network_helper.rb
+++ app/helpers/local_network_helper.rb
@@ -66,6 +66,17 @@ module LocalNetworkHelper
     end
   end
 
+  def link_to_local_network_value_proposition local_network_name, local_network_country_code
+    html = ''
+    [2011].each do |year|
+      filename = "/docs/networks_around_world_doc/communication/network_reports/#{year}/#{local_network_country_code}_VP.pdf"
+      if FileTest.exists?("public/#{filename}")
+        html += content_tag :li, link_to("#{local_network_name} - Value Proposition", filename, {:class => 'pdf'})
+      end
+    end
+    html.present? ? (content_tag :ul, html.html_safe, :class => 'links') : (content_tag :p, "No document is available")
+  end
+
   # temporary participant reports until Local Networks can login
   # these should be removed
 
