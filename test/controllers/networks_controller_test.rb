f29a1db3bbc39abc4d3dbc67869eb84a82792f6e
Jordan Yee <jordanyee@users.noreply.github.com>
2015-08-24 17:55:02 -0400

    Add regression test for "See All" link in networks show view

--- test/controllers/networks_controller_test.rb
+++ test/controllers/networks_controller_test.rb
@@ -2,21 +2,24 @@ require 'test_helper'
 
 class NetworksControllerTest < ActionController::TestCase
   setup do
-    create_container path: '/engage-locally/north-america'
+    create_container path: '/engage-locally/europe'
 
-    @local_network = create_local_network(name: 'Canada')
-    @country = create_country(name: 'Canada', code: 'CA', local_network_id: @local_network.id)
+    @local_network = create_local_network(name: 'Nordic Countries')
+    @denmark = create_country(name: 'Denmark', code: 'DK', local_network_id: @local_network.id)
+    @finland = create_country(name: 'Finland', code: 'FI', local_network_id: @local_network.id)
   end
 
   test 'should get show' do
-    get :show, { region: 'north-america', network: @local_network.name.downcase }
+    get :show, { region: 'europe', network: @local_network.name.downcase }
 
     assert_response :success
     assert_not_nil assigns(:page)
+
+    assert_select '.local-network-participants-header a[href=?]', "/what-is-gc/participants/search?#{CGI.escape("search[countries][]")}=#{@denmark.id}&#{CGI.escape("search[countries][]")}=#{@finland.id}"
   end
 
   test 'should get region' do
-    get :region, region: 'north-america'
+    get :region, region: 'europe'
 
     assert_response :success
     assert_not_nil assigns(:page)
