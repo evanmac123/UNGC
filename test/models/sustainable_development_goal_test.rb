8642af76ffbb731c3dc58dd3151f7cd0234254ea
Jordan Yee <jordanyee@users.noreply.github.com>
2015-08-24 18:45:45 -0400

    Add sustainable development goals model tests

---
+++ test/models/sustainable_development_goal_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class SustainableDevelopmentGoalTest < ActiveSupport::TestCase
+  should 'require a name' do
+    assert_raise ActiveRecord::StatementInvalid do
+      create_sustainable_development_goal name: nil
+    end
+  end
+
+  should 'have a name' do
+    sdg = create_sustainable_development_goal name: 'foo'
+    assert_equal 'foo', sdg.name
+  end
+end
