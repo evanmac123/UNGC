e5fead8f0d8ff33e724da7ce9d0d5e2b7ad2f06d
Jordan Yee <jordanyee@users.noreply.github.com>
2015-08-24 15:46:26 -0400

    Add migration to create sustainable development goals table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731001515) do
+ActiveRecord::Schema.define(version: 20150824193254) do
 
   create_table "announcements", force: :cascade do |t|
     t.integer  "local_network_id", limit: 4
@@ -973,6 +973,12 @@ ActiveRecord::Schema.define(version: 20150731001515) do
     t.string   "logo_url",    limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "sustainable_development_goals", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "taggings", force: :cascade do |t|
