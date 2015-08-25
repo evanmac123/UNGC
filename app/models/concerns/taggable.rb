f855bd245f970185bc049c6e14f326ed8489d95c
Jordan Yee <jordanyee@users.noreply.github.com>
2015-08-24 21:03:32 -0400

    Add sustainable development goals to taggable concern

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -2,13 +2,14 @@ module Taggable
   extend ActiveSupport::Concern
 
   included do
-    has_many :taggings,       dependent: :destroy
-    has_many :sectors,        through: :taggings
-    has_many :sector_groups,  through: :sectors, class_name: 'Sector', source: :parent
-    has_many :issues,         through: :taggings
-    has_many :issue_areas,    through: :issues, class_name: 'Issue', source: :parent
-    has_many :topics,         through: :taggings
-    has_many :topic_groups,   through: :topics, class_name: 'Topic', source: :parent
+    has_many :taggings,                      dependent: :destroy
+    has_many :issues,                        through: :taggings
+    has_many :issue_areas,                   through: :issues,  class_name: 'Issue',  source: :parent
+    has_many :sectors,                       through: :taggings
+    has_many :sector_groups,                 through: :sectors, class_name: 'Sector', source: :parent
+    has_many :sustainable_development_goals, through: :taggings
+    has_many :topics,                        through: :taggings
+    has_many :topic_groups,                  through: :topics,  class_name: 'Topic',  source: :parent
   end
 
 end
