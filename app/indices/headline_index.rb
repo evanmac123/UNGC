3c1223f2cc1b090e1346acace6936f7b28a22efb
Mattia Gheda <ghedamat@gmail.com>
2015-08-27 16:43:47 -0400

    optimize headline and event indexes as well /cc @drteeth

--- app/indices/headline_index.rb
+++ app/indices/headline_index.rb
@@ -6,9 +6,10 @@ ThinkingSphinx::Index.define :headline, with: :active_record do
   has :headline_type,                     facet: true
 
   has country(:id),                       facet: true,      as: :country_id
-  has issues(:id),                        facet: true,      as: :issue_ids,    multi: true
-  has topics(:id),                        facet: true,      as: :topic_ids,    multi: true
-  has sustainable_development_goals(:id), facet: true,      as: :sustainable_development_goal_ids,    multi: true
+  has "SELECT headline_id * 18 + 3 as `id`, topic_id as `topic_ids` FROM `taggings` ORDER BY headline_id", as: :topic_ids, source: :query, facet: true, multi: true, type: :integer
+  has "SELECT headline_id * 18 + 3 as `id`, issue_id as `issue_ids` FROM `taggings` ORDER BY headline_id", as: :issue_ids, source: :query, facet: true, multi: true, type: :integer
+  has "SELECT headline_id * 18 + 3 as `id`, sustainable_development_goal_id as `sustainable_development_goal_ids` FROM `taggings` ORDER BY headline_id", as: :sustainable_development_goal_ids, source: :query, facet: true, multi: true, type: :integer
+
 
 
   where "approval = 'approved'"
