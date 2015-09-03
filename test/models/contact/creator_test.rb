4be987ae0c7dee0bb767c3fce69543af31648f01
Jordan Yee <jordanyee@users.noreply.github.com>
2015-09-03 16:16:19 -0400

    Use real contact in contact creator test

--- test/models/contact/creator_test.rb
+++ test/models/contact/creator_test.rb
@@ -1,43 +1,53 @@
 require 'test_helper'
 
 class Contact::CreatorTest < ActiveSupport::TestCase
+  setup do
+    create_country # Needed by new_contact. See example_data.rb.
+    @contact = new_contact(image: fixture_file_upload('files/untitled.jpg', 'image/jpeg'))
+  end
+
   context 'create' do
     context 'policy allows create and image' do
+      setup do
+        @policy = stub(can_create?: true, can_upload_image?: true)
+      end
+
       should 'save contact and not set image nil' do
-        contact = mock()
+        assert_difference 'Contact.count', 1 do
+          Contact::Creator.new(@contact, @policy).create
+        end
 
-        contact.expects(:save).once
-        contact.expects(:image=).never
-
-        policy = stub(can_create?: true, can_upload_image?: true)
-
-        Contact::Creator.new(contact, policy).create
+        assert @contact.image, true
       end
     end
   end
 
   context 'policy allows create but not image' do
+    setup do
+      @policy = stub(can_create?: true, can_upload_image?: false)
+    end
+
     should 'save contact and set image nil' do
-      contact = mock()
+      assert_difference 'Contact.count', 1 do
+        Contact::Creator.new(@contact, @policy).create
+      end
 
-      contact.expects(:save).once
-      contact.expects(:image=).once.with(nil)
-
-      policy = stub(can_create?: true, can_upload_image?: false)
-
-      Contact::Creator.new(contact, policy).create
+      assert @contact.image, nil
     end
   end
 
   context 'policy does not allow create' do
+    setup do
+      @policy = stub(can_create?: false)
+    end
+
     should 'add an error to contact' do
-      contact = mock()
+      assert_no_difference 'Contact.count' do
+        Contact::Creator.new(@contact, @policy).create
+      end
 
-      contact.expects(:errors).once.returns(stub(:add))
-
-      policy = stub(can_create?: false)
-
-      Contact::Creator.new(contact, policy).create
+      assert_equal @contact.errors.count, 1
+      assert_equal @contact.errors[:base].first, 'You are not authorized to create that contact.'
     end
   end
 end
