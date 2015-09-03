1bd5d641900f0222634353bb2130f4ff09949617
Jordan Yee <jordanyee@users.noreply.github.com>
2015-09-03 17:18:05 -0400

    Assert contact updater return value

--- test/models/contact/updater_test.rb
+++ test/models/contact/updater_test.rb
@@ -3,65 +3,54 @@ require 'test_helper'
 class Contact::UpdaterTest < ActiveSupport::TestCase
   setup do
     create_country # Needed by create_contact. See example_data.rb.
-    @contact = create_contact(
-      username: 'venu',
-      password: 'password'
-    )
+    @contact = create_contact(password: 'password')
   end
 
   context 'update' do
     context 'policy allows update' do
       setup do
-        policy = stub(can_update?: true)
-        params = { username: 'keesari' }
-        Contact::Updater.new(@contact, policy).update(params)
+        @policy = stub(can_update?: true)
+        @params = {}
       end
 
       should 'update contact' do
-        assert_equal 'keesari', @contact.username
+        assert Contact::Updater.new(@contact, @policy).update(@params)
       end
     end
 
     context 'policy allows update and params include empty password' do
       setup do
-        policy = stub(can_update?: true)
-        params = { username: 'keesari', password: '' }
-        Contact::Updater.new(@contact, policy).update(params)
+        @policy = stub(can_update?: true)
+        @params = { password: '' }
       end
 
-      should 'remove password from params' do
+      should 'update contact but not set password' do
+        assert Contact::Updater.new(@contact, @policy).update(@params)
         assert_equal 'password', @contact.password
-      end
-
-      should 'update contact' do
-        assert_equal 'keesari', @contact.username
       end
     end
 
     context 'policy allows update but not image' do
       setup do
-        policy = stub(can_update?: true, can_upload_image?: false)
-        params = { username: 'keesari', image: fixture_file_upload('files/untitled.jpg', 'image/jpeg') }
-        Contact::Updater.new(@contact, policy).update(params)
+        @policy = stub(can_update?: true, can_upload_image?: false)
+        @params = { image: fixture_file_upload('files/untitled.jpg', 'image/jpeg') }
+        Contact::Updater.new(@contact, @policy).update(@params)
       end
 
-      should 'not set image' do
+      should 'update contact but not set image' do
+        assert Contact::Updater.new(@contact, @policy).update(@params)
         assert_not @contact.image?
-      end
-
-      should 'update contact' do
-        assert_equal 'keesari', @contact.username
       end
     end
 
     context 'policy does not allow update' do
       setup do
-        policy = stub(can_update?: false)
-        params = { username: 'keesari' }
-        Contact::Updater.new(@contact, policy).update(params)
+        @policy = stub(can_update?: false)
+        @params = {}
       end
 
-      should 'add an error to contact' do
+      should 'not update contact and instead add an error to contact' do
+        assert_not Contact::Updater.new(@contact, @policy).update(@params)
         assert_equal @contact.errors[:base].first, 'You are not authorized to edit that contact.'
       end
     end
