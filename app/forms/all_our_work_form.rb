70d28862bc986c5a6d92d3803acdbe2740ee2f8b
Jordan Yee <jordanyee@users.noreply.github.com>
2015-09-04 18:30:50 -0400

    Add sustainable development goal filtering test to what you can do and all our work form tests

--- app/forms/all_our_work_form.rb
+++ app/forms/all_our_work_form.rb
@@ -1,7 +1,5 @@
 class AllOurWorkForm < SimpleDelegator
-
-  def initialize(params, seed)
-    super ContainerForm.new(params, seed, sphinx_scope: Container.issues)
+  def initialize(params, seed, search_scope = Container.issues)
+    super ContainerForm.new(params, seed, sphinx_scope: search_scope)
   end
-
 end
--- app/forms/what_you_can_do_form.rb
+++ app/forms/what_you_can_do_form.rb
@@ -1,7 +1,5 @@
 class WhatYouCanDoForm < SimpleDelegator
-
-  def initialize(params, seed)
-    super ContainerForm.new(params, seed, sphinx_scope: Container.actions)
+  def initialize(params, seed, search_scope = Container.actions)
+    super ContainerForm.new(params, seed, sphinx_scope: search_scope)
   end
-
 end
