8fa93464bd5c416efcf31b4cf8fe8f1b03a79d08
Benjamin Moss <drteeth@gmail.com>
2015-09-18 13:48:14 -0400

    Hack newlines after Goal X

--- app/forms/filters/sustainable_development_goal_filter.rb
+++ app/forms/filters/sustainable_development_goal_filter.rb
@@ -5,4 +5,13 @@ class Filters::SustainableDevelopmentGoalFilter < Filters::FlatSearchFilter
     self.label = 'SDG'
     self.key = 'sustainable_development_goals'
   end
+
+  def options
+    # HACK to add a line break after "Goal X:"
+    super.map do |option|
+      option.name = raw(option.name.split(':').join(":\n"))
+      option
+    end
+  end
+
 end
