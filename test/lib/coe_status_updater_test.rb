fde2729422e6a0f5a9eb8791a1bda3170aafff99
Benjamin Moss <drteeth@gmail.com>
2015-09-28 23:58:24 -0400

    split CoeStatusUpdater from CopStatusUpdater

--- test/lib/coe_status_updater_test.rb
+++ test/lib/coe_status_updater_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-class CopStatusUpdaterTest < ActiveSupport::TestCase
+class CoeStatusUpdaterTest < ActiveSupport::TestCase
 
   setup do
-    @status_updater = CopStatusUpdater.new(logger, mailer)
+    @status_updater = CoeStatusUpdater.new(logger, mailer)
     @active = {
       cop_due_on: Date.today - 2.days,
       cop_state: Organization::COP_STATE_ACTIVE
@@ -16,22 +16,22 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
 
   context 'active participants with COP due' do
 
-    should 'move businesses to non-communicating status' do
-      business = create_business(@active)
-      assert business.active?
+    should 'move non-businesses to non-communicating status' do
+      non_business = create_non_business(@active)
+      assert non_business.active?
       @status_updater.update_all
-      assert business.reload.noncommunicating?
+      assert non_business.reload.noncommunicating?
     end
 
   end
 
   context 'non-communicating participants with very late COP' do
 
-    should 'delist businesses' do
-      business = create_business(@noncommunicating)
-      assert business.noncommunicating?
+    should 'delist non-businesses' do
+      non_business = create_non_business(@noncommunicating)
+      assert non_business.noncommunicating?
       @status_updater.update_all
-      assert business.reload.delisted?
+      assert non_business.reload.delisted?
     end
 
   end
@@ -39,10 +39,10 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
   context 'logging' do
 
     setup do
-      @active_business = create_business(@active)
-      @noncommunicating_business = create_business(@noncommunicating)
+      @active_non_business = create_non_business(@active)
+      @noncommunicating_non_business = create_non_business(@noncommunicating)
 
-      @updater = CopStatusUpdater.new(logger, mailer)
+      @updater = CoeStatusUpdater.new(logger, mailer)
     end
 
     should 'not have logged any errors' do
@@ -57,8 +57,8 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
         @updater.update_all
       end
 
-      should 'log the newly non-communicating business' do
-        message = "#{@active_business.id}: #{@active_business.name} is Non-Communicating"
+      should 'log the newly non-communicating non-business' do
+        message = "#{@active_non_business.id}: #{@active_non_business.name} is Non-Communicating"
         logger.expects(:info).with(message)
         @updater.update_all
       end
@@ -68,7 +68,7 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
     context "delisted" do
 
       setup {
-        @business = "#{@noncommunicating_business.id}:#{@noncommunicating_business.name}"
+        @non_business = "#{@noncommunicating_non_business.id}:#{@noncommunicating_non_business.name}"
       }
 
       should 'log the non-communicating -> delisted phase' do
@@ -76,13 +76,13 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
         @updater.update_all
       end
 
-      should 'log the newly delisted business' do
-        logger.expects(:info).with("Delisted #{@business}")
+      should 'log the newly delisted non-business' do
+        logger.expects(:info).with("Delisted #{@non_business}")
         @updater.update_all
       end
 
-      should 'log that the newly delisted business was emailed' do
-        logger.expects(:info).with("emailed delisted #{@business}")
+      should 'log that the newly delisted non-business was emailed' do
+        logger.expects(:info).with("emailed delisted #{@non_business}")
         @updater.update_all
       end
 
@@ -93,12 +93,12 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
   context 'emailing' do
 
     setup do
-      @updater = CopStatusUpdater.new(logger)
+      @updater = CoeStatusUpdater.new(logger)
     end
 
-    should 'send an email to delisted businesses' do
-      organization = create_business(@noncommunicating)
-      CopMailer.expects(:delisting_today)
+    should 'send an email to delisted non-businesses' do
+      organization = create_non_business(@noncommunicating)
+      CoeMailer.expects(:delisting_today)
         .with(organization)
         .returns(stub(:deliver))
         .once
@@ -110,8 +110,8 @@ class CopStatusUpdaterTest < ActiveSupport::TestCase
   private
 
   def mailer
-    @mailer ||= mock('cop_mailer').tap do |m|
-      m.responds_like_instance_of(CopMailer)
+    @mailer ||= mock('coe_mailer').tap do |m|
+      m.responds_like_instance_of(CoeMailer)
       m.stubs(delisting_today: stub(:deliver))
     end
   end
