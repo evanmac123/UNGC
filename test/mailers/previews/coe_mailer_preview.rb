cd0145a0ddadd7947dc9e4051353acab62730bb7
Benjamin Moss <drteeth@gmail.com>
2015-09-28 23:22:05 -0400

    Adds NRR and CP contacts for COP/COE mailers

--- test/mailers/previews/coe_mailer_preview.rb
+++ test/mailers/previews/coe_mailer_preview.rb
@@ -9,15 +9,33 @@ class CoeMailerPreview < ActionMailer::Preview
   private
 
   def organization
-    @organization ||= FixtureReplacement.create_organization
+    @organization ||= FixtureReplacement.create_organization(
+      country: country,
+      cop_due_on: Date.today - 5.years
+    ).tap do |org|
+      create_contact_point(org)
+    end
   end
 
-  def cop
-    @cop ||= FixtureReplacement.create_communication_on_progress(organization: organization)
+  def country
+    @country ||= begin
+      # create a local network and a report recipient
+      network = FixtureReplacement.create_local_network
+      create_report_recipient_for(network)
+
+      # create a country in that network
+      FixtureReplacement.create_country(local_network: network)
+    end
   end
 
-  def user
-    @user ||= FixtureReplacement.create_contact(organization: organization)
+  def create_contact_point(organization)
+    @contact_point_role ||= FixtureReplacement.create_role(name: Role::FILTERS[:contact_point])
+    FixtureReplacement.create_contact(organization: organization, roles: [@contact_point_role])
+  end
+
+  def create_report_recipient_for(local_network)
+    @network_report_recipient_role ||= FixtureReplacement.create_role(name: Role::FILTERS[:network_report_recipient])
+    FixtureReplacement.create_contact(local_network: local_network, roles: [@network_report_recipient_role])
   end
 
 end
--- test/mailers/previews/cop_mailer_preview.rb
+++ test/mailers/previews/cop_mailer_preview.rb
@@ -69,7 +69,12 @@ class CopMailerPreview < ActionMailer::Preview
   private
 
   def organization
-    @organization ||= FixtureReplacement.create_organization
+    @organization ||= FixtureReplacement.create_organization(
+      country: country,
+      cop_due_on: Date.today - 5.years
+    ).tap do |org|
+      create_contact_point(org)
+    end
   end
 
   def cop
@@ -78,6 +83,27 @@ class CopMailerPreview < ActionMailer::Preview
 
   def user
     @user ||= FixtureReplacement.create_contact(organization: organization)
+  end
+
+  def country
+    @country ||= begin
+      # create a local network and a report recipient
+      network = FixtureReplacement.create_local_network
+      create_report_recipient_for(network)
+
+      # create a country in that network
+      FixtureReplacement.create_country(local_network: network)
+    end
+  end
+
+  def create_contact_point(organization)
+    @contact_point_role ||= FixtureReplacement.create_role(name: Role::FILTERS[:contact_point])
+    FixtureReplacement.create_contact(organization: organization, roles: [@contact_point_role])
+  end
+
+  def create_report_recipient_for(local_network)
+    @network_report_recipient_role ||= FixtureReplacement.create_role(name: Role::FILTERS[:network_report_recipient])
+    FixtureReplacement.create_contact(local_network: local_network, roles: [@network_report_recipient_role])
   end
 
 end
