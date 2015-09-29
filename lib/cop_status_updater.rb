5a0ae9a8bf05b46c1252cd2a6a63580664419c27
Benjamin Moss <drteeth@gmail.com>
2015-09-28 23:40:00 -0400

    remove another reference to CommunicationMailer.

--- lib/cop_status_updater.rb
+++ lib/cop_status_updater.rb
@@ -58,8 +58,7 @@ class CopStatusUpdater
   end
 
   def notify_of_delisting(organization)
-    m = CommunicationMailer.new(organization: organization, mailer: mailer)
-    m.delisting_today(organization).deliver
+    CopMailer.delisting_today(organization).deliver
     logger.info "emailed delisted #{organization.id}:#{organization.name}"
   rescue => e
     logger.error "Could not email #{organization.id}:#{organization.name}", e
