be7680329eb506f3357ecb638de446425a22ddb4
Benjamin Moss <drteeth@gmail.com>
2016-02-10 14:41:27 -0500

    fix boolean logic and refactor

--- app/policies/contact/local_network_policy.rb
+++ app/policies/contact/local_network_policy.rb
@@ -10,35 +10,25 @@ class Contact::LocalNetworkPolicy
   end
 
   def can_create?(contact)
-    local_network_allowed_roles(contact)
+    in_allowed_role? && from_same_network_as?(contact)
   end
 
   def can_update?(contact)
-    local_network_allowed_roles(contact)
+    in_allowed_role? && from_same_network_as?(contact)
   end
 
   def can_destroy?(contact)
-    local_network_allowed_roles(contact)
+    in_allowed_role? && from_same_network_as?(contact)
   end
 
   private
-
-  def local_network_allowed_roles(contact)
-    contact_point_from_same_network_as?(contact) ||
-    contact_person_from_same_network_as?(contact) &&
-    from_same_network_as?(contact)
-  end
 
   def from_same_network_as?(contact)
     contact.belongs_to_network?(@local_network_contact.local_network)
   end
 
-  def contact_point_from_same_network_as?(contact)
-    @local_network_contact.is?(Role.contact_point)
-  end
-
-  def contact_person_from_same_network_as?(contact)
-    @local_network_contact.is?(Role.network_focal_point)
+  def in_allowed_role?
+    @local_network_contact.is?(Role.contact_point) || @local_network_contact.is?(Role.network_focal_point)
   end
 
 end
