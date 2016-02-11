17030f66d5afcbe1be3b44a79ac122c6fb1911c0
Benjamin Moss <drteeth@gmail.com>
2016-02-11 00:24:49 -0500

    Extract local network policy test into it's own test.
    Try to make an easy to maintain way to test policy permissions

---
+++ test/policies/contact/local_network_policy_test.rb
@@ -0,0 +1,81 @@
+require 'test_helper'
+
+class Contact::LocalNetworkPolicyTest < ActiveSupport::TestCase
+
+  setup do
+    create_roles
+  end
+
+  should 'reject non-network contacts' do
+    contact = new_contact
+    assert_raise do
+      Contact::LocalNetworkPolicy.new(contact)
+    end
+  end
+
+  should 'handle permissions related to local network contacts' do
+    canada = create_local_network(name: 'canada')
+    italy = create_local_network(name: 'italy')
+
+    assert_allows :network_report_recipient, from: canada, permission: :can_upload_image?, contact_from: canada
+    assert_allows :network_report_recipient, from: canada, permission: :can_create?, contact_from: canada
+    assert_allows :network_report_recipient, from: canada, permission: :can_update?, contact_from: canada
+    assert_allows :network_report_recipient, from: canada, permission: :can_destroy?, contact_from: canada
+
+    refute_allows :network_report_recipient, from: italy, permission: :can_upload_image?, contact_from: canada
+    refute_allows :network_report_recipient, from: italy, permission: :can_create?, contact_from: canada
+    refute_allows :network_report_recipient, from: italy, permission: :can_update?, contact_from: canada
+    refute_allows :network_report_recipient, from: italy, permission: :can_destroy?, contact_from: canada
+
+    assert_allows :network_focal_point, from: canada, permission: :can_upload_image?, contact_from: canada
+    assert_allows :network_focal_point, from: canada, permission: :can_create?, contact_from: canada
+    assert_allows :network_focal_point, from: canada, permission: :can_update?, contact_from: canada
+    assert_allows :network_focal_point, from: canada, permission: :can_destroy?, contact_from: canada
+
+    refute_allows :network_focal_point, from: italy, permission: :can_upload_image?, contact_from: canada
+    refute_allows :network_focal_point, from: italy, permission: :can_create?, contact_from: canada
+    refute_allows :network_focal_point, from: italy, permission: :can_update?, contact_from: canada
+    refute_allows :network_focal_point, from: italy, permission: :can_destroy?, contact_from: canada
+
+    assert_allows :contact_point, from: canada, permission: :can_upload_image?, contact_from: canada
+    refute_allows :contact_point, from: canada, permission: :can_create?, contact_from: canada
+    refute_allows :contact_point, from: canada, permission: :can_update?, contact_from: canada
+    refute_allows :contact_point, from: canada, permission: :can_destroy?, contact_from: canada
+  end
+
+  private
+
+  def assert_allows(role_name, from:, permission:, contact_from:)
+    allows?(true, role_name, from, permission, contact_from)
+  end
+
+  def refute_allows(role_name, from:, permission:, contact_from:)
+    allows?(false, role_name, from, permission, contact_from)
+  end
+
+  def allows?(expected, role_sym, source_network, method, target_network)
+    role = Role.find_by!(name: Role::FILTERS[role_sym])
+    current_contact = create_contact(roles: [role], local_network: source_network)
+    target_contact = create_contact(local_network: target_network)
+
+    policy = Contact::LocalNetworkPolicy.new(current_contact)
+    assert policy.respond_to?(method)
+
+    is_allowed = policy.public_send(method, target_contact)
+
+    if expected
+      expected_assertion = 'allow'
+      unexpected_assertion = 'denied'
+    else
+      expected_assertion = 'deny'
+      unexpected_assertion = 'allowed'
+    end
+
+    error_message = "expected to #{expected_assertion} #{role_sym} from #{source_network.name} #{method} for a contact from #{target_network.name}, but it was #{unexpected_assertion} "
+
+    unless expected == is_allowed
+      raise Minitest::Assertion, error_message
+    end
+  end
+
+end
