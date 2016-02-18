5ad406426c43b6eb5478e5ffbdcca024bf971693
Benjamin Moss <drteeth@gmail.com>
2016-02-18 15:18:38 -0500

    sdg pioneer forms - a few validation fixes

--- app/models/sdg_pioneer/other.rb
+++ app/models/sdg_pioneer/other.rb
@@ -10,7 +10,7 @@ class SdgPioneer::Other < ActiveRecord::Base
   validates :nominee_work_place, presence: true, length: {maximum: 255}
   validates :nominee_title, presence: true, length: {maximum: 255}
   validates :nominee_email, presence: true, length: {maximum: 255}
-  validates :nominee_phone, length: {maximum: 255}
+  validates :nominee_phone, length: {maximum: 32}
   validates :why_nominate, presence: true, length: {maximum: 2750}
   validates :accepts_tou, presence: true
 
