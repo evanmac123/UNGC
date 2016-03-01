fe768a01e819acfc0756b4c87fc16a4204b2dc44
Benjamin Moss <drteeth@gmail.com>
2016-02-29 19:14:39 -0500

    fix participant controller test.

--- test/controllers/participants_controller_test.rb
+++ test/controllers/participants_controller_test.rb
@@ -1,18 +1,27 @@
 require 'test_helper'
 
 class ParticipantsControllerTest < ActionController::TestCase
+
   setup do
     create_container path: '/what-is-gc/participants'
-
-    create_organization_and_user
-
-    @participant = Organization.last
   end
 
   test 'should get show' do
-    get :show, id: @participant
+    create_organization_and_user
+    @organization.update(participant: true)
+
+    get :show, id: @organization
 
     assert_response :success
     assert_not_nil assigns(:page)
   end
+
+  should 'not show non-participants' do
+    create_organization_and_user
+
+    get :show, id: @organization
+
+    assert_response :not_found
+  end
+
 end
