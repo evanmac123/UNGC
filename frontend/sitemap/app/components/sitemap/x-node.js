93b71731e1e5df77f49887047d8d3c7698f38dfb
Benjamin Moss <drteeth@gmail.com>
2016-03-11 16:38:36 -0500

    truncate long sitemap node paths

--- frontend/sitemap/app/components/sitemap/x-node.js
+++ frontend/sitemap/app/components/sitemap/x-node.js
@@ -11,8 +11,20 @@ export default Ember.Component.extend({
   hasDraft: Ember.computed('node.model.hasDraft', 'node.model.draftPayloadId', function() {
     return this.get('node.model.hasDraft') && !Ember.isEmpty(this.get('node.model.draftPayloadId'));
   }),
+
   isPublished: Ember.computed('hasDraft', 'node.model.publicPayloadId', function() {
     return this.get('node.model.publicPayloadId') && !this.get('hasDraft');
+  }),
+
+  shortenedPublicPath: Ember.computed('node.model.publicPath', function() {
+    var maxLength = 40;
+    var fullPath = this.get('node.model.publicPath');
+    var shortenedPath = fullPath;
+    if(fullPath.length > maxLength) {
+      var toTrim = fullPath.length - maxLength;
+      shortenedPath = "..." + fullPath.slice(toTrim);
+    }
+    return shortenedPath;
   }),
 
   classNameBindings: [
