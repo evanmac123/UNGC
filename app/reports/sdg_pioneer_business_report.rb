807f4a38a917549b3cf5c35f07cdd599d25a3613
Abin <abin.abraham4@gmail.com>
2016-03-11 11:17:41 -0500

    build query for sdg pioneer reports

--- app/reports/sdg_pioneer_business_report.rb
+++ app/reports/sdg_pioneer_business_report.rb
@@ -5,11 +5,40 @@ class SdgPioneerBusinessReport < SimpleReport
   end
 
   def header
-    []
+    ['Organization Name',
+     'Is Participant',
+     'Contact Person Name',
+     'Contact Person Title',
+     'Contact Person Email',
+     'Contact Person Phone',
+     'Website URL',
+     'Country',
+     'Local Network Status',
+     'Positive Outcomes',
+     'Matching SDGs'
+     'Other Relevant Information',
+     'Local Business Name'
+     'Is Nominated',
+     'Nominating Organization',
+     'Nominating Individual']
   end
 
   def row(business)
-    []
+    [ business.organization_name,
+      business.is_participant,
+      business.contact_person_name,
+      business.contact_person_title,
+      business.contact_person_email,
+      business.contact_person_phone,
+      business.website_url,
+      business.country_name,
+      business.local_network_status,
+      business.positive_outcomes,
+      business.matching_sdgs
+      business.other_relevant_info,
+      business.local_business_name,
+      business.is_nominated,
+      business.nominating_organization,
+      business.nominating_individual ]
   end
-
 end
--- app/reports/sdg_pioneer_individual_report.rb
+++ app/reports/sdg_pioneer_individual_report.rb
@@ -5,11 +5,42 @@ class SdgPioneerIndividualReport < SimpleReport
   end
 
   def header
-    []
+    ['Is Participant',
+     'Name',
+     'Email',
+     'Phone',
+     'Description of Individual'
+     'Other Relevant Information',
+     'Supporting Link',
+     'Matching SDGs'
+     'Local Business Nominating Name'
+     'Title'
+     'Website URL',
+     'Country',
+     'Local Network Status',
+     'Is Nominated',
+     'Nominating Organization',
+     'Nominating Individual']
   end
 
-  def row(business)
-    []
+  def row(individual)
+    [
+      individual.is_participant,
+      individual.name,
+      individual.email,
+      individual.phone,
+      individual.description_of_individual,
+      individual.other_relevant_info,
+      individual.supporting_link,
+      individual.matching_sdgs,
+      individual.local_business_nomination_name,
+      individual.title,
+      individual.website_url,
+      individual.country_name,
+      individual.local_network_status,
+      individual.is_nominated,
+      individual.nominating_organization,
+      individual.nominating_individual ]
   end
 
 end
