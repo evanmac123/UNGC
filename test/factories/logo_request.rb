16a7249ea668eeb2beb6c9488af040b98f863ee1
Benjamin Moss <drteeth@gmail.com>
2016-04-12 18:05:40 -0400

    truncate logo request purpose to 255

--- test/factories/logo_request.rb
+++ test/factories/logo_request.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :logo_request do
     organization
     publication { create(:logo_publication) }
-    purpose { Faker::Lorem.paragraph }
+    purpose { Faker::Lorem.paragraph[0,255] }
   end
 end
