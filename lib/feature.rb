50a134ee682ec45890284ada4adbf17d56dbf883
Benjamin Moss <drteeth@gmail.com>
2016-05-11 12:16:28 -0400

    use self. instead of singleton class

--- lib/feature.rb
+++ lib/feature.rb
@@ -1,14 +1,12 @@
 # Feature flags.
 # Add predicates here for enabling/disabling features
 class Feature
-  class << self
 
-    # The SDG Pioneer forms are only open for a short time every year.
-    def sdg_pioneer_form?
-      feature_enabled = false
-      feature_enabled ||
-        Rails.env.test? # enable the forms for tests
-    end
-
+  # The SDG Pioneer forms are only open for a short time every year.
+  def self.sdg_pioneer_form?
+    feature_enabled = false
+    feature_enabled ||
+      Rails.env.test? # enable the forms for tests
   end
+
 end
