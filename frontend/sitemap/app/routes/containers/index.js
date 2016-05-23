5693d29bf9eef8429c588d378e2a45fa5c9221c9
Benjamin Moss <drteeth@gmail.com>
2016-05-23 14:30:36 -0400

    cleanup sitemap deps

--- frontend/sitemap/app/routes/containers/index.js
+++ frontend/sitemap/app/routes/containers/index.js
@@ -1,5 +1,5 @@
 import Ember     from 'ember';
-import Container from 'sitemap/models/container';
+// import Container from 'sitemap/models/container';
 import Sitemap   from 'sitemap/lib/sitemap/tree';
 
 export default Ember.Route.extend({
