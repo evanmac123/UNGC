b3b40fef4829ce9ab8aecca3498f31d9171600af
Benjamin Moss <drteeth@gmail.com>
2016-07-05 17:49:05 -0400

    answer basic questions

--- test/integration/cops/advanced_form_test.rb
+++ test/integration/cops/advanced_form_test.rb
@@ -24,6 +24,16 @@ class CopSubmissionTest < ActionDispatch::IntegrationTest
       form.save_draft
     end
 
+    # Answer the basic questions
+    form.format = 'Stand alone document'
+    form.start_date 'June', '2016'
+    form.end_date 'June', '2017'
+    form.include_continued_support_statement = false
+    form.references :human_rights, true
+    form.references :labour, false
+    form.include_measurement = true
+    form.method_shared_with_stakeholders = 'COP is easily accessible to all interested parties (e.g. via its website)'
+
     # answers are not duplicated when a 2nd draft is saved
     assert_no_difference 'CopAnswer.count' do
       form.check 'mandatory option'
@@ -34,22 +44,47 @@ class CopSubmissionTest < ActionDispatch::IntegrationTest
     # Answers are remembered
     assert form.checked?('mandatory option')
     assert form.filled?('verification open', with: 'hello there')
+    assert_equal 'standalone', form.format
+    assert_equal Date.new(2016, 6, 1), form.starts_on
+    assert_equal Date.new(2017, 6, 1), form.ends_on
+    assert form.include_continued_support_statement? false
+    assert_equal true, form.references?(:human_rights)
+    assert_equal false, form.references?(:labour)
+    assert_nil form.references?(:environment)
+    assert_nil form.references?(:anti_corruption)
+    assert form.include_measurement?
+    assert_equal 'all_access', form.method_shared_with_stakeholders
 
     # validations are run on Submit
     form.submit
     assert form.has_validation_error? "Attachment can't be blank"
 
     # We can submit files
-    form.submit_sample_pdf language: 'English'
+    form.add_file language: 'English', filename: 'untitled.pdf'
+
+    # And links
+    form.add_link language: 'English', url: 'http://example.com'
 
     # After successful submit, we should be taken to the admin/show page
     detail_page = form.submit
     assert detail_page.has_published_notice?
 
+    # log_page
+
     # TODO
     # check the confirmation info
     # check the public page info
     # check the dashboard index for a new entry
+  end
+
+  private
+
+  def log_page(filename)
+    path = "./public/system/#{filename}"
+    File.open(path, 'w') do |f|
+      f.write(page.html.gsub('display: none;', ''))
+    end
+    system "open http://localhost:3000/system/#{filename}"
   end
 
 end
