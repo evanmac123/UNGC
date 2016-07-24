6321e16bc436b00c4e3897961ae6ed67afa753e9
Benjamin Moss <drteeth@gmail.com>
2016-07-24 16:13:18 -0400

    Add integration test stubs for remaining COP forms

---
+++ test/integration/cop/express_form_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+module COP
+  class ExpressFormTest < ActionDispatch::IntegrationTest
+
+    setup do
+      travel_to Date.new(2016, 7, 6)
+    end
+
+    teardown do
+      travel_back
+    end
+
+    test "handle COP submission lifecycle" do
+    end
+
+  end
+end
