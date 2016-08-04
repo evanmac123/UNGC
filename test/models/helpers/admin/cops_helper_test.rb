f3d6523df383467ccf75dc0a1680610f63d7192f
Benjamin Moss <drteeth@gmail.com>
2016-08-04 12:14:39 -0400

    add coverage for cops_path

--- test/models/helpers/admin/cops_helper_test.rb
+++ test/models/helpers/admin/cops_helper_test.rb
@@ -2,19 +2,39 @@ require 'test_helper'
 
 class Admin::CopsHelperTest < ActionView::TestCase
 
-  setup {
+  setup do
     create(:organization_type)
     create(:organization)
-  }
-
-  should "#show_cop_attributes" do
-    create_cop_and_answers_for_grouping('additional')
-    assert show_cop_attributes(@cop, @principle_area).present?
   end
 
-  should "#show_basic_cop_attributes" do
+  test "#show_cop_attributes" do
+    create_cop_and_answers_for_grouping('additional')
+    assert view.show_cop_attributes(@cop, @principle_area).present?
+  end
+
+  test "#show_basic_cop_attributes" do
     create_cop_and_answers_for_grouping('basic')
-    assert show_basic_cop_attributes(@cop, @principle_area).present?
+    assert view.show_basic_cop_attributes(@cop, @principle_area).present?
+  end
+
+  test "organization contacts are sent to the dashboard" do
+    # Given an organization and a contact
+    organization = create(:business)
+    contact = create(:contact, organization: organization)
+
+    # and the contact is currently logged in
+    view.stubs(current_contact: contact)
+
+    assert_equal dashboard_path(tab: :cops), view.cops_path(organization)
+  end
+
+  test "non-organization user ex. staff or local network, is sent to their organization detail page" do
+    organization = create(:business)
+    contact = create(:contact)
+
+    view.stubs(current_contact: contact)
+
+    assert_equal admin_organization_path(organization, tab: :cops), view.cops_path(organization)
   end
 
   private
