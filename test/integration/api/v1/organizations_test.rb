38ab8492adfc058db1708b938ac7b17d88f03c2b
Benjamin Moss <drteeth@gmail.com>
2016-08-11 13:52:50 -0400

    update test to reflect desired output

--- test/integration/api/v1/organizations_test.rb
+++ test/integration/api/v1/organizations_test.rb
@@ -4,42 +4,58 @@ class Api::V1::OrganizationsTest < ActionDispatch::IntegrationTest
 
   test "/api/v1/organizations" do
     local_network = create(:local_network)
-    country = create(:country, local_network: local_network)
-    sector = create(:sector)
-    o = create(:business,
+    country = create(:country, local_network: local_network, name: "France")
+    sector = create(:sector, name: "Animal Wellfare")
+
+    organization_type = create(:organization_type,
+                               name: "SME",
+                               type_property: OrganizationType::BUSINESS)
+
+    o = create(:organization,
+               name: "Lasers n' Sharks",
+               revenue: 4,
+               employees: 43,
+               cop_state: 'delisted',
                state: 'approved',
+               url: "https://lasers-n-sharks.info",
                participant: true,
                sector: sector,
-               country: country
+               country: country,
+               is_local_network_member: true,
+               organization_type: organization_type
               )
+
     get '/api/v1/organizations.json'
     assert_response :success
     json_response = JSON.parse(response.body)
-    ap json_response
+
     expected = [
       {
         "id" => o.id,
-        "name" => o.name,
-        "country_name" => o.country.name,
-        "revenue" => o.revenue,
-        "employees" => o.employees,
+        "name" => "Lasers n' Sharks",
+        "participant" => true,
+        "country_name" => "France",
+        "revenue" => "between USD 1 billion and USD 5 billion",
+        "employees" => 43,
+        "cop_state" => "delisted",
+        "url" => "https://lasers-n-sharks.info",
+        "sector_name" => "Animal Wellfare",
+        "profile_url" => "https://www.example.com/what-is-gc/participants/#{o.id}-Lasers-n-Sharks",
+        "is_local_network_member" => true,
+        "is_deleted" => false,
         "created_at" => o.created_at,
         "updated_at" => o.updated_at,
-        "cop_state" => o.cop_state,
-        "url" => o.url,
-        "sector_id" => o.sector_id,
-        "is_local_network_member" => o.is_local_network_member,
         "organization_type" => {
           "name" => "SME",
           "type" => "Business"
-        },
-        "local_network" => {
-          "name" => "Canada",
-          "url" => "blah.com"
         }
-
-      },
+      }
     ]
+
+    ap :Expected
+    ap expected
+    ap :Actual
+    ap json_response
 
     assert_equal expected, json_response
   end
