8b9f608db8c45dd14391cf0180ed6b41366c4b90
Benjamin Moss <drteeth@gmail.com>
2016-08-11 17:28:51 -0400

    make timezones match up

--- test/integration/api/v1/organizations_test.rb
+++ test/integration/api/v1/organizations_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class Api::V1::OrganizationsTest < ActionDispatch::IntegrationTest
 
+  setup     { travel_to Time.zone.local(2016, 8, 11, 12, 43, 3) }
+  teardown  { travel_back }
+
   test "/api/v1/organizations" do
     local_network = create(:local_network)
     country = create(:country, local_network: local_network, name: "France")
@@ -40,22 +43,17 @@ class Api::V1::OrganizationsTest < ActionDispatch::IntegrationTest
         "cop_state" => "delisted",
         "url" => "https://lasers-n-sharks.info",
         "sector_name" => "Animal Wellfare",
-        "profile_url" => "https://www.example.com/what-is-gc/participants/#{o.id}-Lasers-n-Sharks",
+        "profile_url" => "http://www.example.com/what-is-gc/participants/#{o.id}-Lasers-n-Sharks",
         "is_local_network_member" => true,
         "is_deleted" => false,
-        "created_at" => o.created_at,
-        "updated_at" => o.updated_at,
+        "created_at" => "2016-08-11T12:43:03.000Z",
+        "updated_at" => "2016-08-11T12:43:03.000Z",
         "organization_type" => {
           "name" => "SME",
           "type" => "Business"
         }
       }
     ]
-
-    ap :Expected
-    ap expected
-    ap :Actual
-    ap json_response
 
     assert_equal expected, json_response
   end
