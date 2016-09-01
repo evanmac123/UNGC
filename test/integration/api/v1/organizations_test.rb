1ce5bf25ffb004a7f2f08254187ac284bbf78c39
Benjamin Moss <drteeth@gmail.com>
2016-09-01 16:18:43 -0400

    Fix org api tests
    Finish initiative filter test

--- test/integration/api/v1/organizations_test.rb
+++ test/integration/api/v1/organizations_test.rb
@@ -32,40 +32,50 @@ class Api::V1::OrganizationsTest < ActionDispatch::IntegrationTest
     assert_response :success
     json_response = JSON.parse(response.body)
 
-    expected = [
-      {
-        "id" => o.id,
-        "name" => "Lasers n' Sharks",
-        "participant" => true,
-        "country_name" => "France",
-        "revenue" => "between USD 1 billion and USD 5 billion",
-        "employees" => 43,
-        "cop_state" => "delisted",
-        "url" => "https://lasers-n-sharks.info",
-        "sector_name" => "Animal Wellfare",
-        "profile_url" => "http://www.unglobalcompact.org/what-is-gc/participants/#{o.id}-Lasers-n-Sharks",
-        "is_local_network_member" => true,
-        "is_deleted" => false,
-        "created_at" => "2016-08-11T12:43:03.000Z",
-        "updated_at" => "2016-08-11T12:43:03.000Z",
-        "organization_type" => {
-          "name" => "SME",
-          "type" => "Business"
+    expected = {
+      'organizations' => [
+        {
+          "id" => o.id,
+          "name" => "Lasers n' Sharks",
+          "participant" => true,
+          "country_name" => "France",
+          "revenue" => "between USD 1 billion and USD 5 billion",
+          "employees" => 43,
+          "cop_state" => "delisted",
+          "url" => "https://lasers-n-sharks.info",
+          "sector_name" => "Animal Wellfare",
+          "profile_url" => "http://www.unglobalcompact.org/what-is-gc/participants/#{o.id}-Lasers-n-Sharks",
+          "is_local_network_member" => true,
+          "is_deleted" => false,
+          "created_at" => "2016-08-11T12:43:03.000Z",
+          "updated_at" => "2016-08-11T12:43:03.000Z",
+          "organization_type" => {
+            "name" => "SME",
+            "type" => "Business"
+          }
         }
-      }
-    ]
+      ]
+    }
 
     assert_equal expected, json_response
   end
 
   test "filter organization by climate initiative" do
-    organization = create(:organization)
+    # Given an organization which has signed the climate initiative
+    signed = create(:business, name: "We signed")
     climate_id = Initiative::FILTER_TYPES[:climate]
     climate = create(:initiative, id: climate_id)
-    s = create(:signing, organization: organization, initiative: climate)
+    create(:signing, organization: signed, initiative: climate)
 
+    # And one who hasn't
+    hasnt = create(:business, name: "We haven't")
 
+    get '/api/v1/organizations.json?initiative=climate'
 
+    json_response = JSON.parse(response.body)
+    organizations = json_response['organizations']
+    assert_equal 1, organizations.length
+    assert_equal organizations.first['name'], "We signed"
   end
 
 end
