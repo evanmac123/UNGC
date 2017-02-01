4a7673d95c2974cd978814d5bcbae163bf952aeb
Benjamin Moss <drteeth@gmail.com>
2017-02-01 00:33:12 -0500

    implement missing organization mailer previews

--- test/mailers/previews/organization_mailer_preview.rb
+++ test/mailers/previews/organization_mailer_preview.rb
@@ -1,18 +1,29 @@
 class OrganizationMailerPreview < ActionMailer::Preview
 
   def submission_received
+    create_contact_point
+
+    OrganizationMailer.submission_received(organization)
   end
 
   def submission_jci_referral_received
+    create_ceo
+
+    OrganizationMailer.submission_jci_referral_received(organization)
   end
 
   def in_review
+    create_contact_point
+    organization.comments << create_staff_comment
+
+    OrganizationMailer.in_review(organization)
   end
 
   def in_review_local_network
     create_ceo
     create_contact_point
     create_network_report_recipient
+    organization.comments << create_staff_comment
 
     OrganizationMailer.in_review_local_network(organization)
   end
@@ -32,6 +43,9 @@ class OrganizationMailerPreview < ActionMailer::Preview
   end
 
   def approved_nonbusiness
+    create_contact_point
+
+    OrganizationMailer.approved_nonbusiness(organization)
   end
 
   def approved_local_network
@@ -50,47 +64,53 @@ class OrganizationMailerPreview < ActionMailer::Preview
   end
 
   def reject_microenterprise
+    create_contact_point
+
+    OrganizationMailer.reject_microenterprise(organization)
   end
 
   def reject_microenterprise_network
+    create_ceo
+    create_contact_point
+
+    OrganizationMailer.reject_microenterprise_network(organization)
   end
 
   def foundation_invoice
+    create_contact_point
+
+    OrganizationMailer.foundation_invoice(organization)
   end
 
   def foundation_reminder
+    create_contact_point
+
+    OrganizationMailer.foundation_reminder(organization)
   end
 
   private
 
   def create_ceo
-    FactoryGirl.create(:contact, organization: organization, roles: [Role.ceo])
+    FactoryGirl.create(:contact, organization: organization,
+                       roles: [Role.ceo])
   end
 
   def create_contact_point
-    FactoryGirl.create(:contact, organization: organization, roles: [Role.contact_point])
+    FactoryGirl.create(:contact, organization: organization,
+                       roles: [Role.contact_point])
   end
 
   def create_network_report_recipient
-    FactoryGirl.create(:contact, local_network: local_network, roles: [Role.network_report_recipient])
+    FactoryGirl.create(:contact, local_network: local_network,
+                       roles: [Role.network_report_recipient])
   end
 
   def create_staff
     FactoryGirl.create(:contact, organization: ungc)
   end
 
-  def staff_user
-    @staff_user ||= create_staff
-  end
-
   def organization
-    @organization ||= FactoryGirl.create(:organization, country: country).tap do |org|
-      org.comments.create!(body: Faker::Lorem.sentence, contact: staff_user)
-    end
-  end
-
-  def business
-    @business ||= FactoryGirl.create(:business)
+    @organization ||= FactoryGirl.create(:organization, country: country)
   end
 
   def country
@@ -103,6 +123,10 @@ class OrganizationMailerPreview < ActionMailer::Preview
 
   def ungc
     @ungc ||= Organization.where(name: DEFAULTS[:ungc_organization_name]).first_or_create!
+  end
+
+  def create_staff_comment
+    FactoryGirl.create(:comment, body: Faker::Lorem.sentence, contact: create_staff)
   end
 
 end
