6690cb02d6dce8f884be0934dca7d6a73bbae3b3
Benjamin Moss <drteeth@gmail.com>
2017-02-15 13:32:38 -0500

    Ensure now purely guards against missing comments

--- app/mailers/logo_request_mailer.rb
+++ app/mailers/logo_request_mailer.rb
@@ -3,7 +3,10 @@ class LogoRequestMailer < ActionMailer::Base
           :bcc  => 'archive@unglobalcompact.org'
 
   def in_review(logo_request)
+    ensure_has_a_comment(logo_request)
+
     @logo_request = logo_request
+
     mail \
       :to => logo_request.contact.email_recipient,
       :subject => "Your Global Compact Logo Request has been updated"
@@ -11,16 +14,22 @@ class LogoRequestMailer < ActionMailer::Base
 
   def approved(logo_request)
     ensure_has_a_comment(logo_request)
+
+    @logo_request = logo_request
+
     mail \
       :to => logo_request.contact.email_recipient,
       :subject => "Your Global Compact Logo Request has been accepted"
   end
 
   def rejected(logo_request)
+    ensure_has_a_comment(logo_request)
+
     contact = logo_request.contact
     return if contact.nil?
 
-    ensure_has_a_comment(logo_request)
+    @logo_request = logo_request
+
     mail \
       :to => contact.email_recipient,
       :subject => "Global Compact Logo Request Status"
@@ -29,9 +38,7 @@ class LogoRequestMailer < ActionMailer::Base
   private
 
   def ensure_has_a_comment(logo_request)
-    if logo_request.logo_comments.present?
-      @logo_request = logo_request
-    else
+    if logo_request.logo_comments.empty?
       raise "LogoRequestMailer requires a logo request to have at least one comment"
     end
   end
