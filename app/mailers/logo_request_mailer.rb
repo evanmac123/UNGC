d97413903937dde15b52a466951e9c61f312dd58
Abin <abin.abraham4@gmail.com>
2017-02-15 13:28:00 -0500

    extract if else statement into method

--- app/mailers/logo_request_mailer.rb
+++ app/mailers/logo_request_mailer.rb
@@ -10,34 +10,30 @@ class LogoRequestMailer < ActionMailer::Base
   end
 
   def approved(logo_request)
-    if ensure_has_a_comment(logo_request)
-      @logo_request = logo_request
-      mail \
-        :to => logo_request.contact.email_recipient,
-        :subject => "Your Global Compact Logo Request has been accepted"
-      else
-        raise "LogoRequestMailer requires a logo request to have at least one comment"
-    end
+    ensure_has_a_comment(logo_request)
+    mail \
+      :to => logo_request.contact.email_recipient,
+      :subject => "Your Global Compact Logo Request has been accepted"
   end
 
   def rejected(logo_request)
     contact = logo_request.contact
     return if contact.nil?
 
-    if ensure_has_a_comment(logo_request)
-      @logo_request = logo_request
-      mail \
-        :to => contact.email_recipient,
-        :subject => "Global Compact Logo Request Status"
-    else
-      raise "LogoRequestMailer requires a logo request to have at least one comment"
-    end
+    ensure_has_a_comment(logo_request)
+    mail \
+      :to => contact.email_recipient,
+      :subject => "Global Compact Logo Request Status"
   end
 
   private
 
   def ensure_has_a_comment(logo_request)
-    logo_request.logo_comments.present?
+    if logo_request.logo_comments.present?
+      @logo_request = logo_request
+    else
+      raise "LogoRequestMailer requires a logo request to have at least one comment"
+    end
   end
 
 end
