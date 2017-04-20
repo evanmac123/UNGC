65094de7aa7ee57a9828d060a8e6a3690f562715
Abin <abin.abraham4@gmail.com>
2017-04-20 15:55:02 -0400

    queries

--- app/queries/igloo_contacts_query.rb
+++ app/queries/igloo_contacts_query.rb
@@ -6,7 +6,8 @@ class IglooContactsQuery
 
   def run
     result = Contact.where("updated_at >= ?", 5.minutes.ago).includes(:organization)
-    result + updated_organization
+    result + updated_action_platform_organizations + updated_action_platform_contacts
+    + action_platform_contacts + action_platform_organizations
   end
 
   def updated_organization
@@ -16,6 +17,32 @@ class IglooContactsQuery
     end
     contacts_that_may_have_new_organization_names
   end
+
+  def action_platform_contacts
+    subscriptions = ActionPlatform::Subscription.all
+    contacts = subscriptions.map do |subscription|
+      subscription.contact
+    end
+    contacts
+  end
+
+  def action_platform_organizations
+    subscriptions = ActionPlatform::Subscription.all
+    organizations = subscriptions.map do |subscription|
+      subscription.organization
+    end
+    organizations
+  end
+
+  def updated_action_platform_contacts
+    ActionPlatform::Subscription.joins(:contact).where("contacts.updated_at >= ?", 5.minutes.ago)
+  end
+
+  def updated_action_platform_organizations
+    ActionPlatform::Subscription.joins(:organization).where("organizations.updated_at >= ?", 5.minutes.ago)
+  end
+
+
 
 
 end
