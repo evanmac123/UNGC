f69cc3f1d9da491b0452c7981390be59a6b99cb5
Abin <abin.abraham4@gmail.com>
2017-05-24 17:06:11 -0400

    fix typo change organization.id to organization_id

--- app/reports/action_platform_contacts.rb
+++ app/reports/action_platform_contacts.rb
@@ -16,7 +16,7 @@ class ActionPlatformContacts < SimpleReport
   def row(record)
     [
       record.organization.name,
-      record.organization.id,
+      record.organization_id,
       record.platform.name,
       record.contact.first_name,
       record.contact.last_name
