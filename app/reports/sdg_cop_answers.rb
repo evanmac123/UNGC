31a61c603cec283f10d72c7a8d77c19f163cad3f
Abin <abin.abraham4@gmail.com>
2017-08-22 16:58:52 -0400

    initial run through to get query right

--- app/reports/sdg_cop_answers.rb
+++ app/reports/sdg_cop_answers.rb
@@ -5,7 +5,6 @@ class SdgCopAnswers < SimpleReport
                     cop_attribute: [:cop_question]).
     includes(communication_on_progress: [{ organization: [:sector, :country, :organization_type] }],
              cop_attribute: [:cop_question ]).
-    where("value = 1 or cop_answers.text <> ''").
     where("grouping = 'sdgs'").
     order(:cop_id).
     group_by do |answer|
@@ -31,6 +30,41 @@ class SdgCopAnswers < SimpleReport
       'sector',
       'employees'
     ]
+    [
+      'Organization ID',
+      'Organization Name',
+      'COP ID',
+      'COP Type',
+      'COP Published On',
+      'Organization Type',
+      'Country',
+      'Sector',
+      'Employees',
+      'Opportunities and responsibilities that one or more SDGs represent to our business',
+      'Where the company’s priorities lie with respect to one or more SDGs',
+      'Goals and indicators set by our company with respect to one or more',
+      'How one or more SDGs are integrated into the company’s business model',
+      'The (expected) outcomes and impact of your company’s activities related to the SDGs',
+      'If the companies activities related to the SDGs are undertaken in collaboration with other stakeholder',
+      'Other established or emerging best practices',
+      'SDG 1',
+      'SDG 2',
+      'SDG 3',
+      'SDG 4',
+      'SDG 5',
+      'SDG 6',
+      'SDG 7',
+      'SDG 8',
+      'SDG 9',
+      'SDG 10',
+      'SDG 11',
+      'SDG 12',
+      'SDG 13',
+      'SDG 14',
+      'SDG 15',
+      'SDG 16',
+      'SDG 17',
+    ]
   end
 
   def row(row)
@@ -41,12 +75,12 @@ class SdgCopAnswers < SimpleReport
       communication_on_progress.id,
       rename_cop_type(communication_on_progress),
       communication_on_progress.published_on,
-      combined_text(answers),
-      combined_answers(answers),
       communication_on_progress.organization.organization_type_name,
       communication_on_progress.organization.country_name,
       communication_on_progress.organization.sector_name,
-      communication_on_progress.organization.employees
+      communication_on_progress.organization.employees,
+      combined_text(answers),
+      combined_answers(answers),
     ]
   end
 
@@ -70,6 +104,17 @@ class SdgCopAnswers < SimpleReport
     answers.map do |question|
       question.cop_attribute.text
     end.compact.join(",\n")
+  end
+
+  def first_answer(answers)
+    answers.flat_map do |key, value|
+      value.map do |cop_answer|
+        if cop_answer.cop_attribute_id == 6511
+          puts cop_answer.cop_attribute.text
+          puts cop_answer.value
+        end
+      end
+    end
   end
 
 
