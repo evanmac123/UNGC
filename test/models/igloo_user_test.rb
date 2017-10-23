6d4a664fff3efd0e92f87259c00c86053028dae7
Benjamin Moss <drteeth@gmail.com>
2017-10-23 13:16:52 -0400

    Don't let IglooUser's FK get in the way of deleting contacts

--- test/models/igloo_user_test.rb
+++ test/models/igloo_user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class IglooUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "deleting a contact destroy's the igloo user" do
+    contact = create(:contact)
+    create(:igloo_user, contact: contact)
+
+    assert_difference -> { IglooUser.count }, -1 do
+      contact.destroy
+    end
+  end
+
 end
