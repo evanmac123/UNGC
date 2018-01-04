fbe9112f1c02891e430b84121530ca35cc95d4bc
bikeath1337 <bikeath1337@me.com>
2018-01-04 14:22:29 -0500

    Corrects .group() for IssueTree

--- app/models/issue_tree.rb
+++ app/models/issue_tree.rb
@@ -4,7 +4,7 @@ class IssueTree < Tree
     super(Issue.where.not(parent_id: nil)
       .includes(:parent)
       .select([:id, :parent_id, :name])
-      .group(:parent_id, :id)
+      .group(:parent_id, :id, :name)
       .group_by do |issue|
         issue.parent
       end)
