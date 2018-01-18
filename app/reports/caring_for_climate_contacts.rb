4a3e0477cb58c126a487841c0dbc82925a802338
Benjamin Moss <drteeth@gmail.com>
2018-01-17 23:53:03 -0500

    reports must accept options

--- app/reports/caring_for_climate_contacts.rb
+++ app/reports/caring_for_climate_contacts.rb
@@ -1,6 +1,6 @@
 class CaringForClimateContacts < InitiativesContactsBase
 
-  def initialize
+  def initialize(options = {})
     super(initiative_name: Initiative::FILTER_TYPES[:climate], role_id: Role.caring_for_climate.id,
           signed_on_header: 'signed_c4c_on')
   end
--- app/reports/water_mandate_contacts.rb
+++ app/reports/water_mandate_contacts.rb
@@ -1,6 +1,6 @@
 class WaterMandateContacts < InitiativesContactsBase
 
-  def initialize
+  def initialize(options = {})
     super(initiative_name: Initiative::FILTER_TYPES[:water_mandate], role_id: Role.ceo_water_mandate.id,
           signed_on_header: 'signed_mandate_on')
   end
