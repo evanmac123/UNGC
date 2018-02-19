ecdf65acc7fbf4909eb2c6bae3c63277fab9adc0
Bobby Wallace <bikeath1337@mac.com>
2018-02-19 17:44:56 -0500

    Fixes DonationSync bug

--- app/jobs/crm/adapters/donation.rb
+++ app/jobs/crm/adapters/donation.rb
@@ -4,19 +4,19 @@ module Crm
   module Adapters
     class Donation < Crm::Adapters::Base
 
+
       def to_crm_params(transform_action)
         # NB we need to parse the raw stripe response
         # so that we can serialize it all back down as a single
         # JSON field.
         {
-            IdField: model.id,
-            MetadataField: {
+            Crm::DonationSyncJob::IdField => model.id,
+            Crm::DonationSyncJob::MetadataField => {
                 ungc: model.metadata,
                 stripe_response: JSON.parse(model.full_response),
             }.to_json
         }
       end
-
     end
   end
 end
