451824272fa2da7a9be6cff07d293086a586a506
Bobby Wallace <bikeath1337@mac.com>
2018-02-27 13:51:01 -0500

    Always synchronizes Organization “precise revenue”

--- app/jobs/crm/adapters/organization.rb
+++ app/jobs/crm/adapters/organization.rb
@@ -54,7 +54,7 @@ module Crm
         column('Removal_Reason__c', :removal_reason) { |organization| organization.removal_reason&.description}
 
         column('Revenue__c', :revenue_range)
-        column('AnnualRevenue', :revenue) do |organization|
+        column('AnnualRevenue', nil, true) do |organization|
           case
             when organization.precise_revenue.present?
               organization.precise_revenue.dollars
