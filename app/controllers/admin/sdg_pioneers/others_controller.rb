e52023d70b47a3f370e5c76e17565c5e465db2f0
Abin <abin.abraham4@gmail.com>
2018-05-07 15:46:45 -0400

    Update views for SDG pioneer applications (#856)
    Only show applications from May 3,2018
    Update views to show submission date

--- app/controllers/admin/sdg_pioneers/others_controller.rb
+++ app/controllers/admin/sdg_pioneers/others_controller.rb
@@ -1,11 +1,17 @@
 class Admin::SdgPioneers::OthersController < AdminController
 
   def index
-    @others = SdgPioneer::Other.all
+    @others = SdgPioneer::Other.where(created_at: start_date...Date.today)
   end
 
   def show
     @other = SdgPioneer::Other.find(params.fetch(:id))
+  end
+
+  private
+
+  def start_date
+    Date.new(2018,5,3)
   end
 
 end
--- app/controllers/admin/sdg_pioneers/submissions_controller.rb
+++ app/controllers/admin/sdg_pioneers/submissions_controller.rb
@@ -1,13 +1,19 @@
 class Admin::SdgPioneers::SubmissionsController < AdminController
 
   def index
-    @submissions = SdgPioneer::Submission.all
+    @submissions = SdgPioneer::Submission.where(created_at: start_date...Date.today)
     @business_leaders = @submissions.business_leader
     @submission_types = @submissions.group_by { |t| t.pioneer_type  }
   end
 
   def show
     @submission = SdgPioneer::Submission.find(params.fetch(:id))
+  end
+
+  private
+
+  def start_date
+    Date.new(2018,5,3)
   end
 
 end
