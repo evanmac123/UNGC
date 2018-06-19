8404b746816eee33c3db646276ccfebcb94d3b22
Benjamin Moss <drteeth@gmail.com>
2018-06-19 14:07:27 -0400

    Update Ocean AP Igloo group name

--- lib/igloo/platform_subscription_sync.rb
+++ lib/igloo/platform_subscription_sync.rb
@@ -74,7 +74,7 @@ module Igloo
       "Water Security through Stewardship" => "Water Security through Stewardship",
       "Peace, Justice and Strong Institutions" => "Justice and Strong Institutions",
       "Business for Humanitarian Action" => "Business for Humanitarian Action",
-      "Business Action Platform for the Ocean" => "Business Action Platform for the Ocean"
+      "Business Action Platform for the Ocean" => "Sustainable Ocean Business Action Platform",
     }.freeze
 
   end
