a52bba07c488477c8d2c45296e451723d7522ae5
Benjamin Moss <drteeth@gmail.com>
2018-07-19 14:21:53 -0400

    Fix Academy highlight widget and CTA

--- app/pages/academy_page.rb
+++ app/pages/academy_page.rb
@@ -23,7 +23,9 @@ class AcademyPage < ContainerPage
   end
 
   def article_blocks
-    Array(@data[:article_blocks])
+    Array(@data[:article_blocks]).map do |article_block|
+      HighlightPage.prepare_block_for_display(article_block)
+    end
   end
 
   def logos_and_partners
--- app/pages/highlight_page.rb
+++ app/pages/highlight_page.rb
@@ -11,16 +11,7 @@ class HighlightPage < ContainerPage
   def main_content_sections
     article_blocks = @data[:article_blocks] || []
     article_blocks.map do |section|
-      data = {
-        title: section[:title],
-        content: section[:content],
-        classes: section_classes(section),
-        styles: section_styles(section)
-      }
-      cta = section[:call_to_action]
-      data[:call_to_action] = cta if cta && cta[:enabled]
-      data[:image] = section[:widget_image] if section[:widget_image]
-      data
+      self.class.prepare_block_for_display(section)
     end
   end
 
@@ -28,9 +19,22 @@ class HighlightPage < ContainerPage
     Components::Resources.new(@data).data
   end
 
+  def self.prepare_block_for_display(section)
+    data = {
+      title: section[:title],
+      content: section[:content],
+      classes: section_classes(section),
+      styles: section_styles(section)
+    }
+    cta = section[:call_to_action]
+    data[:call_to_action] = cta if cta && cta[:enabled]
+    data[:image] = section[:widget_image] if section[:widget_image]
+    data
+  end
+
   private
 
-  def section_classes(section)
+  def self.section_classes(section)
     classes = []
     classes << 'has-custom-bg' if section[:bg_image]
     classes << "align-#{section[:align]}" if section[:align]
@@ -38,7 +42,7 @@ class HighlightPage < ContainerPage
     classes.join(' ')
   end
 
-  def section_styles(section)
+  def self.section_styles(section)
     section[:bg_image] ? "background-image: url(#{section[:bg_image]})" : ''
   end
 end
