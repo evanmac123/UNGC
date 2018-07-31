921f203854747f0ab0a79c85e8e565815c9f28f1
Benjamin Moss <drteeth@gmail.com>
2018-07-31 17:32:31 -0400

    Update policy rules

--- test/lib/academy/sign_in_policy_test.rb
+++ test/lib/academy/sign_in_policy_test.rb
@@ -5,55 +5,116 @@ require 'test_helper'
 module Academy
   class SignInPolicyTest < ActiveSupport::TestCase
 
-    test "it denies a normal contact" do
+    test "it denies a contacts by default" do
       contact = create(:contact)
       policy = ::Academy::SignInPolicy.new
 
       refute policy.can_sign_in?(contact)
     end
 
-    test "it allows staff to sign in" do
-      staff = create(:staff_contact)
-      policy = ::Academy::SignInPolicy.new
+    context "Organizations" do
 
-      assert policy.can_sign_in?(staff)
+      should "allow contacts from active, participant level organizations" do
+        organization = create_organization(active: true, cop_state: "active",
+          participant: true, level_of_participation: :participant_level)
+        contact = create(:contact, organization: organization)
+        policy = ::Academy::SignInPolicy.new
+
+        assert policy.can_sign_in?(contact)
+      end
+
+      should "disallow contacts from inactive organizations" do
+        organization = create_organization(active: false)
+        contact = create(:contact, organization: organization)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(contact)
+      end
+
+      should "disallow contacts from delisted organizations" do
+        organization = create_organization(cop_state: Organization::COP_STATE_DELISTED)
+        contact = create(:contact, organization: organization)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(contact)
+      end
+
+      should "disallow contacts from signatory tier organizations" do
+        organization = create_organization(level_of_participation: :signatory_level)
+        contact = create(:contact, organization: organization)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(contact)
+      end
+
+      should "disallow contacts from undecided organizations" do
+        organization = create(:organization, level_of_participation: nil)
+        contact = create(:contact, organization: organization)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(contact)
+      end
+
+      should "disallow contacts from non-participant organizations" do
+        organization = create(:organization, participant: false)
+        contact = create(:contact, organization: organization)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(contact)
+      end
     end
 
-    test "it allows local network contacts to sign in" do
-      # Given an active Local Network
-      network = create(:local_network, state: :active)
+    context "Staff" do
 
-      # And a contact from that network that can login
-      contact = create(:contact, local_network: network,
-        username: "Foo", password: "Passw0rd")
+      should "allow staff contacts with the Academy Manager role" do
+        academy_manager = create(:role, name: Role::FILTERS[:academy_manager])
+        staff = create(:staff_contact, roles: [academy_manager])
+        policy = ::Academy::SignInPolicy.new
 
-      # Then the policy should allow them to sign in
-      policy = ::Academy::SignInPolicy.new
-      assert policy.can_sign_in?(contact)
+        assert policy.can_sign_in?(staff)
+      end
+
+      should "disallow regular taff contacts" do
+        staff = create(:staff_contact)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(staff)
+      end
+
     end
 
-    test "it allows contacts from participant tier organizations" do
-      organization = create(:organization, level_of_participation: :participant_level)
-      contact = create(:contact, organization: organization)
-      policy = ::Academy::SignInPolicy.new
+    context "Local Networks" do
 
-      assert policy.can_sign_in?(contact)
+      should "allow Local Network representatives to sign-in" do
+        academy_role = create(:role, name: Role::FILTERS[:academy_local_network_representative])
+        network = create(:local_network)
+        contact = create(:contact, roles: [academy_role], local_network: network)
+        policy = ::Academy::SignInPolicy.new
+
+        assert policy.can_sign_in?(contact)
+      end
+
+      should "disallow regular local network contacts" do
+        network = create(:local_network)
+        contact = create(:contact, local_network: network)
+        policy = ::Academy::SignInPolicy.new
+
+        refute policy.can_sign_in?(contact)
+      end
+
     end
 
-    test "it disallows contacts from signatory organizations" do
-      organization = create(:organization, level_of_participation: :signatory_level)
-      contact = create(:contact, organization: organization)
-      policy = ::Academy::SignInPolicy.new
+    private
 
-      refute policy.can_sign_in?(contact)
-    end
+    def create_organization(params = {})
+      defaults = {
+        active: true,
+        cop_state: Organization::COP_STATE_ACTIVE,
+        participant: true,
+        level_of_participation: :participant_level,
+      }
 
-    test "it disallows contacts from undecided organizations" do
-      organization = create(:organization, level_of_participation: nil)
-      contact = create(:contact, organization: organization)
-      policy = ::Academy::SignInPolicy.new
-
-      refute policy.can_sign_in?(contact)
+      create(:organization, params.reverse_merge(defaults))
     end
 
   end
