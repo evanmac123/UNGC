240725709d80bd2e0cf9c8c1bc98dad4506fde3a
Abin Abraham <abin.abraham4@gmail.com>
2018-08-09 13:13:07 -0400

    Update organization information from an excel sheet

---
+++ app/models/organization_excel.rb
@@ -0,0 +1,27 @@
+class OrganizationExcel
+  include ActiveModel::Model
+  require 'roo'
+
+  attr_accessor :file
+
+  def self.import(file)
+      spreadsheet = Roo::Spreadsheet.open(file.path)
+      header = spreadsheet.row(1)
+      (2..spreadsheet.last_row).each do |i|
+        row = Hash[[header, spreadsheet.row(i)].transpose]
+        organization = Organization.find_by(id: row["id"])
+        organization.attributes = row.to_hash
+        organization.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+      when ".csv" then Csv.new(file.path, nil, :ignore)
+      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then Roo::Excelx.new(file.path)
+      else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+end
