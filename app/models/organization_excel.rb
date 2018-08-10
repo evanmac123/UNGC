8defe7928a2304f32487f4b50929eb1814384de4
Abin Abraham <abin.abraham4@gmail.com>
2018-08-10 11:14:08 -0400

    remove url to page from main nav

--- app/models/organization_excel.rb
+++ app/models/organization_excel.rb
@@ -14,14 +14,4 @@ class OrganizationExcel
         organization.save!
     end
   end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-      when ".csv" then Csv.new(file.path, nil, :ignore)
-      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Roo::Excelx.new(file.path)
-      else raise "Unknown file type: #{file.original_filename}"
-    end
-  end
-
 end
