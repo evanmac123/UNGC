1b860fbe665e86188306743b0c9c7e2b124b4886
Benjamin Moss <drteeth@gmail.com>
2018-08-30 00:45:33 -0400

    ensure @template has been set

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -3,18 +3,12 @@
 class SamlIdpController < SamlIdp::IdpController
   layout 'application'
 
-  def new
-    saml_authenticator = Saml::Authenticator.create_for(@saml_request.issuer)
-    @template = saml_authenticator.issuer_token
-    super
-  end
+  before_action :assign_template
 
   protected
 
   def idp_authenticate(username, password)
-    saml_authenticator = Saml::Authenticator.create_for(@saml_request.issuer)
-    @template = saml_authenticator.issuer_token
-    saml_authenticator.authenticate(username, password)
+    @saml_authenticator.authenticate(username, password)
   end
 
   def idp_make_saml_response(user)
@@ -23,6 +17,13 @@ class SamlIdpController < SamlIdp::IdpController
 
   def idp_logout
     # No-op
+  end
+
+  def assign_template
+    if @saml_request&.issuer.present?
+      @saml_authenticator = Saml::Authenticator.create_for(@saml_request.issuer)
+      @template = @saml_authenticator.issuer_token
+    end
   end
 
 end
