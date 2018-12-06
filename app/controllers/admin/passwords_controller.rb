e231976fb535b077c16771823c82d37de17beb11
Benjamin Moss <drteeth@gmail.com>
2018-12-05 22:30:35 -0500

    Immediate academy signups (#932)
    * Academy contacts get redirected to the academy
    
    When they reset their password. Only applies to Contacts that can't
    login to the UNGC site.

--- app/controllers/admin/passwords_controller.rb
+++ app/controllers/admin/passwords_controller.rb
@@ -5,8 +5,28 @@ class Admin::PasswordsController < Devise::PasswordsController
   def create
     super
   rescue Errno::ECONNREFUSED => e
-    # TODO: Remove this rescue because it's weird.
     flash[:notice] = 'Sorry, we could not send the email due to a server error. Please try again.'
     redirect_to new_contact_session_url
   end
+
+  def update
+    super do |contact|
+      if academy_only_viewer?(contact)
+        # Academy viewers that can't login need to be redirected to
+        # the academy after resetting their password
+        url = "https://academy.unglobalcompact.org/learn"
+        respond_with contact, location: url
+        break # don't go any further
+      end
+    end
+  end
+
+  private
+
+  def academy_only_viewer?(contact)
+    Devise.sign_in_after_reset_password &&
+      !contact.active_for_authentication? &&
+      contact.is?(Role.academy_viewer)
+  end
+
 end
